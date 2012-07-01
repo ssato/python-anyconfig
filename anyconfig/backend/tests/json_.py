d23e42ee10ea2843c4735669785a77d3fdecca59
Satoru SATOH <satoru.satoh@gmail.com>
2012-07-01 22:41:49 +0900

    added test cases for JSON and YAML backends

---
+++ anyconfig/backend/tests/json_.py
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2012 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+import anyconfig.backend.json_ as J
+
+import os
+import tempfile
+import unittest
+
+
+CONF_0 = """{
+  "a": 0,
+  "b": "bbb",
+
+  "sect0": {
+    "c": ["x", "y", "z"]
+  }
+}
+"""
+
+
+class Test_JsonConfigParser(unittest.TestCase):
+
+    def setUp(self):
+        (_, conf) = tempfile.mkstemp(prefix="ac-test-")
+        open(conf, 'w').write(CONF_0)
+        self.config_path = conf
+
+    def tearDown(self):
+        os.remove(self.config_path)
+
+    def test_00_load(self):
+
+        c = J.JsonConfigParser.load(self.config_path)
+
+        self.assertEquals(c['a'], 0, str(c))
+        self.assertEquals(c['b'], "bbb", c)
+        self.assertEquals(c.a, 0)
+        self.assertEquals(c.b, "bbb")
+
+        self.assertEquals(c.sect0.c, ['x', 'y', 'z'])
+
+
+# vim:sw=4:ts=4:et:

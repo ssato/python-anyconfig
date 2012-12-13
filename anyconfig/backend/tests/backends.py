12878319e92b6c384c8763123a177eb59e9f910f
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-13 18:41:31 +0900

    added test cases for anyconfig.backend.backends

---
+++ anyconfig/backend/tests/backends.py
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2012 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+import anyconfig.backend.backends as T
+import unittest
+
+
+class Test_00_pure_functions(unittest.TestCase):
+
+    def test_10_find_by_file(self):
+        ini_cf = "/a/b/c.ini"
+        jsn_cf = "/a/b/c.jsn"
+        yml_cf = "/a/b/c.yml"
+        unknown_cf = "/a/b/c.xyz"
+
+        def instchk(cf, cls):
+            isinstance(T.find_by_file(cf), cls)
+
+        self.assertTrue(ini_cf, T.BINI.IniConfigParser)
+        self.assertTrue(jsn_cf, T.BJSON.JsonConfigParser)
+        self.assertTrue(yml_cf, T.BYAML.YamlConfigParser)
+        self.assertTrue(T.find_by_file(unknown_cf) is None)
+
+    def test_20_find_by_type(self):
+        ini_t = "ini"
+        jsn_t = "json"
+        yml_t = "yaml"
+        unknown_t = "unknown_type"
+
+        def instchk(cf, ctype):
+            isinstance(T.find_by_type(ctype), cls)
+
+        self.assertTrue(ini_t, T.BINI.IniConfigParser)
+        self.assertTrue(jsn_t, T.BJSON.JsonConfigParser)
+        self.assertTrue(yml_t, T.BYAML.YamlConfigParser)
+        self.assertTrue(T.find_by_type(unknown_t) is None)
+
+# vim:sw=4:ts=4:et:

72786e4ae8030aba8f2b9cf6f25d45c8f4bb65ff
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-13 18:49:14 +0900

    disabled coverage checks by default and made tests run for each py file

--- aux/runtest.sh
+++ aux/runtest.sh
@@ -4,16 +4,18 @@ set -e
 curdir=${0%/*}
 topdir=${curdir}/../
 
+#coverage_opts="--with-coverage --cover-tests --cover-inclusive"
+coverage_opts=""
+
 which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
 
 if test $# -gt 0; then
     test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
     PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg $@
 else
-    test $check_with_pep8 = 1 && (find anyconfig -name '*.py' | xargs pep8) || :
-    PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg \
-        --with-coverage \
-        --cover-tests \
-        --cover-inclusive \
-        -w $topdir/anyconfig
+    for f in $(find ${topdir}/anyconfig -name '*.py'); do
+        echo "[Info] Check $f..."
+        test $check_with_pep8 = 1 && pep8 $f || :
+        PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg ${coverage_opts} $f
+    done
 fi

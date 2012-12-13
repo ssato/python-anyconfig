aa757cc252bc3ea8b46082f43ef0be17f648f496
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-13 15:19:07 +0900

    moved into aux/

--- aux/runtest.sh
+++ aux/runtest.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 set -e
 
-
-topdir=${0%/*}
+curdir=${0%/*}
+topdir=${curdir}/../
 
 which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
 
 if test $# -gt 0; then
     test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
-    PYTHONPATH=$topdir nosetests -c $topdir/nose.cfg $@
+    PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg $@
 else
     test $check_with_pep8 = 1 && (find anyconfig -name '*.py' | xargs pep8) || :
-    PYTHONPATH=$topdir nosetests -c $topdir/nose.cfg \
+    PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg \
         --with-coverage \
         --cover-tests \
         --cover-inclusive \

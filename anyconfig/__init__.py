ef33cf982daec68dd8b270926653d71303d11f47
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-25 03:27:25 +0900

    added some shortcuts for functions in anyconfig.AnyConifg

---
+++ anyconfig/__init__.py
@@ -0,0 +1,34 @@
+"""Generic interface to loaders and parsers for various config file formats.
+
+Instead of
+
+    import json, yaml
+    jd = json.load(open("foo.json"))
+    yd = yaml.load(open("bar.yaml"))
+
+use
+
+    import anyconfig as ac
+    jd = ac.load("foo.json")
+    yd = ac.load("bar.yaml")
+
+The returned object is an anyconfig.Bunch object, dict-like object but its
+values are also accessible as attributes, by default.
+
+"""
+from .AnyConfig import load, loads, dump
+
+VERSION = "0.0.3"
+
+# If daily snapshot versioning mode:
+#importt datetime
+#VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
+
+__version__ = VERSION
+__all__ = [
+    'dump', 'load', 'loads',
+]
+
+__author__ = 'Satoru SATOH <ssat@redhat.com>'
+
+# vim:sw=4:ts=4:et:

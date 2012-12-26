ad3a03c89fae68e19211299e2c0fd0045ea27860
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-26 22:40:47 +0900

    added some more test cases for anyconfig.api.{loads,mload,mload_metaconf}

--- anyconfig/tests/api.py
+++ anyconfig/tests/api.py
@@ -11,6 +11,7 @@ import anyconfig.backend.xml_ as BXML
 import anyconfig.backend.yaml_ as BYAML
 import anyconfig.backend.properties_ as BPROP
 
+import os
 import os.path
 import unittest
 
@@ -55,6 +56,16 @@ class Test_10_pure_functions(unittest.TestCase):
             self.assertEquals(A.find_parser("dummy.properties"),
                               BPROP.PropertiesParser)
 
+    def test_30_dumps_and_loads(self):
+        a = B.Bunch(name="a", a=1, b=B.Bunch(b=[1, 2], c="C"))
+        a1 = A.loads(A.dumps(a, "json"), "json")
+
+        # FIXME: Too verbose
+        self.assertEquals(a.name, a1.name)
+        self.assertEquals(a.a, a1.a)
+        self.assertEquals(a.b.b, a1.b.b)
+        self.assertEquals(a.b.c, a1.b.c)
+
 
 class Test_20_effectful_functions(unittest.TestCase):
 
@@ -80,7 +91,7 @@ class Test_20_effectful_functions(unittest.TestCase):
         self.assertEquals(a.b.b, a1.b.b)
         self.assertEquals(a.b.c, a1.b.c)
 
-    def test_20_dump_and_loads(self):
+    def test_20_dump_and_mload(self):
         a = B.Bunch(name="a", a=1, b=B.Bunch(b=[1, 2], c="C"))
         b = B.Bunch(a=2, b=B.Bunch(b=[1, 2, 3, 4, 5], d="D"))
 
@@ -93,7 +104,7 @@ class Test_20_effectful_functions(unittest.TestCase):
         A.dump(b, b_path)
         self.assertTrue(os.path.exists(b_path))
 
-        x = A.loads([a_path, b_path])
+        x = A.mload([a_path, b_path])
 
         # FIXME: Too verbose
         self.assertEquals(a.name, x.name)
@@ -102,12 +113,40 @@ class Test_20_effectful_functions(unittest.TestCase):
         self.assertEquals(a.b.c, x.b.c)
         self.assertEquals(b.b.d, x.b.d)
 
-        x = A.loads([a_path, b_path], B.ST_MERGE_DICTS_AND_LISTS)
+        x = A.mload([a_path, b_path], update=B.ST_MERGE_DICTS_AND_LISTS)
 
         self.assertEquals(a.name, x.name)
         self.assertEquals(b.a, x.a)
         self.assertEquals([1, 2, 3, 4, 5], x.b.b)
         self.assertEquals(a.b.c, x.b.c)
         self.assertEquals(b.b.d, x.b.d)
+
+    def test_50_mload_metaconf__single_file(self):
+        m0_content = '{"topdir": "/tmp"}\n'
+        m0_path = os.path.join(self.workdir, "m0.json")
+
+        open(m0_path, 'w').write(m0_content)
+
+        mc = A.mload_metaconf(m0_path)
+        self.assertEquals(mc.topdir, "/tmp")
+
+    def test_52_mload_metaconf__dir(self):
+        m0_content = '{}\n'
+        m1_content = '{"topdir": "/tmp"\n'
+
+        subdir = "metaconf.d"
+        confdir = os.path.join(self.workdir, subdir)
+
+        os.makedirs(confdir)
+
+        m0_path = os.path.join(confdir, "m0.json")
+        m1_path = os.path.join(confdir, "m1.json")
+
+        open(m0_path, 'w').write(m0_content)
+        open(m1_path, 'w').write(m1_content)
+
+        mc = A.mload_metaconf(confdir, conf_exts=".json")
+        self.assertEquals(mc.topdir, self.workdir)
+
 
 # vim:sw=4:ts=4:et:

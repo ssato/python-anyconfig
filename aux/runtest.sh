97708b71fc3cdd163dfe21a2cee069c67a700259
Satoru SATOH <satoru.satoh@gmail.com>
2013-01-04 17:21:05 +0900

    made python package dir automatically found, and coverage option set from env. val 'WITH_COVERAGE'

--- aux/runtest.sh
+++ aux/runtest.sh
@@ -4,18 +4,29 @@ set -e
 curdir=${0%/*}
 topdir=${curdir}/../
 
-#coverage_opts="--with-coverage --cover-tests --cover-inclusive"
-coverage_opts=""
+if `env | grep -q 'WITH_COVERAGE' 2>/dev/null`; then
+    coverage_opts="--with-coverage --cover-tests --cover-inclusive"
+fi
 
 which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
 
 if test $# -gt 0; then
     test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
-    PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg $@
+    PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg ${coverage_opts} $@
 else
-    for f in $(find ${topdir}/anyconfig -name '*.py'); do
-        echo "[Info] Check $f..."
-        test $check_with_pep8 = 1 && pep8 $f || :
-        PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg ${coverage_opts} $f
+    # Find out python package dir and run tests for .py files under it.
+    for d in ${topdir}/*; do
+        if test -d $d -a -f $d/__init__.py; then
+            pypkgdir=$d
+
+            for f in $(find ${pypkgdir} -name '*.py'); do
+                echo "[Info] Check $f..."
+                test $check_with_pep8 = 1 && pep8 $f || :
+                PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg \
+                        ${coverage_opts} $f
+            done
+
+            break
+        fi
     done
 fi

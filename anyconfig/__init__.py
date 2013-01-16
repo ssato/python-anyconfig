f06edc78d24b3a2a05fbbdf117594fefa55c18eb
Satoru SATOH <satoru.satoh@gmail.com>
2013-01-16 11:25:40 +0900

    enhanced module's doc and added some usage examples

--- anyconfig/__init__.py
+++ anyconfig/__init__.py
@@ -8,12 +8,73 @@ Instead of
 
 use
 
-    import anyconfig as ac
-    jd = ac.load("foo.json")
-    yd = ac.load("bar.yaml")
+    import anyconfig
+    jd = anyconfig.load("foo.json")
+    yd = anyconfig.load("bar.yaml")
 
 The returned object is an anyconfig.Bunch object, dict-like object but its
 values are also accessible as attributes, by default.
+
+You can also load multiple config files at once w/ anyconfig.multi_load:
+
+    conf = anyconfig.multi_load(["foo.json", "bar.yaml"])
+
+or if path of config files can be specified w/ a glob pattern, you can use
+anyconfig.load instead such like:
+
+    conf = anyconfig.load("/etc/xyz/conf.d/*.conf", "yaml")
+
+On loading multiple config files, you can choose strategy to merge configs from
+the followings:
+
+* anyconfig.MS_REPLACE: Replace all configuration parameters provided in former
+  config files are simply replaced w/ the ones in later config files.
+
+  For example, if a.yml and b.yml are like followings:
+
+  a.yml::
+
+    a: 1
+    b: 
+       - c: 0
+       - c: 2
+    d:
+       e: "aaa"
+       f: 3
+
+  b.yml::
+
+    b: 
+       - c: 3
+    d:
+       e: "bbb"
+
+  then::
+  
+    load(["a.yml", "b.yml"], merge=anyconfig.MS_REPLACE)
+    
+  will give object such like::
+  
+    {'a': 1, 'b': [{'c': 3}], 'd': {'e': "bbb"}}
+
+* anyconfig.MS_DICTS: Merge dicts recursively. That is, the following
+
+  ::
+
+    load(["a.yml", "b.yml"], merge=anyconfig.MS_DICTS)
+
+  will give object such like::
+  
+    {'a': 1, 'b': [{'c': 3}], 'd': {'e': "bbb", 'f': 3}}
+
+* anyconfig.MS_DICTS_AND_LISTS: Merge dicts and lists recursively. That is, the
+  following::
+
+    load(["a.yml", "b.yml"], merge=anyconfig.MS_DICTS_AND_LISTS)
+
+  will give object such like::
+
+    {'a': 1, 'b': [{'c': 0}, {'c': 2}, {'c': 3}], 'd': {'e': "bbb", 'f': 3}}
 
 """
 from .api import single_load, multi_load, load, loads, dump, dumps, \

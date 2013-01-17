185cc42119a6cde73af5ceb0be6b16fe925edef7
Satoru SATOH <satoru.satoh@gmail.com>
2013-01-17 15:48:16 +0900

    added to replace anyconfig.Bunch in the near future

---
+++ anyconfig/mergeabledict.py
@@ -0,0 +1,72 @@
+#
+# Copyright (C) 2011, 2012 Satoru SATOH <ssato@redhat.com>
+#
+import anyconfig.utils as U
+import copy
+
+# TODO: Keep items' order:
+#from collections import OrderedDict as dict
+
+
+(ST_REPLACE, ST_MERGE_DICTS, ST_MERGE_DICTS_AND_LISTS) = (0, 1, 2)
+
+
+def is_MergeableDict_or_dict(x):
+    return isinstance(x, (MergeableDict, dict))
+
+
+class MergeableDict(dict):
+    """
+    Dict-like object supports 'merge' operation.
+    """
+
+    # TODO: Which strategy should be choosen for default?
+    strategy = ST_MERGE_DICTS_AND_LISTS
+
+    def get_strategy(self):
+        return self.strategy
+
+    def update(self, other, strategy=None):
+        """Update members recursively based on given strategy.
+        """
+        if strategy is None:
+            strategy = self.get_strategy()
+
+        if strategy == ST_REPLACE:
+            self.update_w_replace(other)
+        elif strategy == ST_MERGE_DICTS_AND_LISTS:
+            self.update_w_merge(other, merge_lists=True)
+        else:
+            self.update_w_merge(other, merge_lists=False)
+
+    def update_w_replace(self, other):
+        if is_MergeableDict_or_dict(other):
+            for k, v in other.iteritems():
+                self[k] = v
+        else:
+            self = copy.copy(other)
+
+    def update_w_merge(self, other, merge_lists=False):
+        """Merge members recursively.
+
+        :param merge_lists: Merge not only dicts but also lists. For example,
+
+            [1, 2, 3], [3, 4] ==> [1, 2, 3, 4]
+            [1, 2, 2], [2, 4] ==> [1, 2, 2, 4]
+        """
+        if is_MergeableDict_or_dict(other):
+            for k, v in other.iteritems():
+                if k in self and is_MergeableDict_or_dict(v) and \
+                        is_MergeableDict_or_dict(self[k]):  # update recursively.
+                    self[k].update_w_merge(v, merge_lists)
+                else:
+                    if merge_lists and U.is_iterable(v):
+                        v0 = self.get(k, None)
+                        if v0 is None:
+                            self[k] = [x for x in list(v)]
+                        else:
+                            self[k] += [x for x in list(v) if x not in v0]
+                    else:
+                        self[k] = v
+
+# vim:sw=4:ts=4:et:
---
+++ anyconfig/tests/mergeabledict.py
@@ -0,0 +1,61 @@
+#
+# Copyright (C) 2011, 2012 Satoru SATOH <satoru.satoh @ gmail.com>
+#
+import anyconfig.mergeabledict as T
+import anyconfig.tests.common as C
+
+import os.path
+import unittest
+
+
+class Test_MergeableDict(unittest.TestCase):
+
+    def test_update__w_merge_dicts(self):
+        a = T.MergeableDict(name="a", a=1,
+                            b=T.MergeableDict(b=[1, 2], c="C"),
+                            e=[3, 4])
+        b = T.MergeableDict(a=2, b=T.MergeableDict(b=[1, 2, 3], d="D"))
+
+        ref = T.MergeableDict(**a.copy())
+        ref['a'] = 2
+        ref['b'] = T.MergeableDict(b=[1, 2, 3], c="C", d="D")
+        ref['e'] = [3, 4]
+
+        a.update(b)
+
+        self.assertEquals(a, ref)
+
+    def test_update__w_merge_dicts_and_lists(self):
+        a = T.MergeableDict(name="a", a=1, b=T.MergeableDict(b=[1, 2], c="C"))
+        b = T.MergeableDict(a=2, b=T.MergeableDict(b=[3, 4, 5], d="D"))
+
+        ref = T.MergeableDict(**a.copy())
+        ref['a'] = 2
+        ref['b'] = T.MergeableDict(b=[1, 2, 3, 4, 5], c="C", d="D")
+
+        a.update(b, T.ST_MERGE_DICTS_AND_LISTS)
+
+        self.assertEquals(a, ref)
+
+    def test_update__w_replace(self):
+        a = T.MergeableDict(name="a", a=1, b=T.MergeableDict(b=[1, 2], c="C"))
+        b = T.MergeableDict(a=2, b=T.MergeableDict(b=[3, 4, 5], d="D"))
+
+        ref = T.MergeableDict(**a.copy())
+        ref['a'] = 2
+        ref['b'] = b['b']
+        ref['b']['c'] = a['b']['c']
+
+        a.update(b, T.ST_REPLACE)
+
+        self.assertEquals(a, ref)
+
+    def test_update__w_None(self):
+        a = T.MergeableDict(name="a", a=1, b=T.MergeableDict(b=[1, 2], c="C"))
+        ref = T.MergeableDict(**a.copy())
+
+        a.update(None)
+
+        self.assertEquals(a, ref)
+
+# vim:sw=4:ts=4:et:

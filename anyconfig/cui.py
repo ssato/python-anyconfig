fdb6861af899a938059120f03cfdc8561993bdff
Satoru SATOH <satoru.satoh@gmail.com>
2013-01-27 11:23:04 +0900

    added -A and --atype option to override config w/ -A option

--- anyconfig/cui.py
+++ anyconfig/cui.py
@@ -16,6 +16,9 @@ sys.stdout = codecs.getwriter(__enc)(sys.stdout)
 sys.stderr = codecs.getwriter(__enc)(sys.stderr)
 
 
+DEFAULTS = dict(debug=False, list=False, output=None, itype=None, otype=None,
+                atype=None)
+
 USAGE = """\
 %prog [Options...] CONF_PATH_OR_PATTERN_0 [CONF_PATH_OR_PATTERN_1 ..]
 
@@ -23,23 +26,21 @@ Examples:
   %prog --list
   %prog -I yaml /etc/xyz/conf.d/a.conf
   %prog -I yaml '/etc/xyz/conf.d/*.conf' -o xyz.conf --otype json
+  %prog '/etc/xyz/conf.d/*.json' -o xyz.yml \\
+    --atype json -A '{"obsoletes": "sysdata", "conflicts": "sysdata-old"}'
+  %prog '/etc/xyz/conf.d/*.json' -o xyz.yml \\
+    -A obsoletes:sysdata;conflicts:sysdata-old
   %prog /etc/foo.json /etc/foo/conf.d/x.json /etc/foo/conf.d/y.json
 """
 
 
-def main(argv=sys.argv):
-    defaults = {
-        "debug": False,
-        "list": False,
-        "output": None,
-        "itype": None,
-        "otype": None,
-    }
+def option_parser(defaults=DEFAULTS, usage=USAGE):
     ctypes = A.list_types()
+    ctypes_s = ", ".join(ctypes)
     type_help = "Explicitly select type of %s config files from " + \
-        ", ".join(ctypes) + " [Automatically detected by file path]"
+        ctypes_s + " [Automatically detected by file ext]"
 
-    p = optparse.OptionParser(USAGE)
+    p = optparse.OptionParser(usage)
     p.set_defaults(**defaults)
 
     p.add_option("-D", "--debug", action="store_true", help="Debug mode")
@@ -49,6 +50,15 @@ def main(argv=sys.argv):
     p.add_option("-I", "--itype", choices=ctypes, help=(type_help % "Input"))
     p.add_option("-O", "--otype", choices=ctypes, help=(type_help % "Output"))
 
+    p.add_option("-A", "--args", help="Argument configs to override")
+    p.add_option("", "--atype", choices=ctypes,
+        help="Explicitly select type of argument config from " + ctypes_s)
+
+    return p
+
+
+def main(argv=sys.argv):
+    p = option_parser()
     (options, args) = p.parse_args(argv[1:])
 
     llvl = logging.DEBUG if options.debug else logging.INFO
@@ -65,12 +75,17 @@ def main(argv=sys.argv):
 
     data = A.load(args, options.itype)
 
+    if options.args:
+        diff = A.loads(options.args, options.atype)
+        data.update(diff)
+
     if options.output:
         cp = A.find_parser(options.output, options.otype)
         cp.dump(data, options.output)
     else:
         assert options.otype is not None, \
             "Please specify Output type w/ -O/--otype option"
+
         cp = A.find_parser(None, options.otype)
         sys.stdout.write(cp.dumps(data))
 

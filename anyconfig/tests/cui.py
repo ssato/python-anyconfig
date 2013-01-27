5e66211a740e8e4a582b647aebee3c44fd63b345
Satoru SATOH <satoru.satoh@gmail.com>
2013-01-28 05:27:56 +0900

    added test cases for anyconfig.cui

---
+++ anyconfig/tests/cui.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2013 Satoru SATOH <ssato at redhat.com>
+#
+import anyconfig.cui as T
+import anyconfig.api as A
+import anyconfig.tests.common as C
+
+import os
+import os.path
+import unittest
+
+
+class Test_10_effectful_functions(unittest.TestCase):
+
+    def setUp(self):
+        self.workdir = C.setup_workdir()
+
+    def tearDown(self):
+        C.cleanup_workdir(self.workdir)
+
+    def test_10__no_args(self):
+        with self.assertRaises(SystemExit) as cm:
+            T.main(["dummy"])
+        self.assertNotEquals(cm.exception.code, 0)
+
+    def test_20__list(self):
+        with self.assertRaises(SystemExit) as cm:
+            T.main(["dummy", "--list"])
+        self.assertEqual(cm.exception.code, 0)
+
+    def test_30_single_input(self):
+        a = dict(name="a", a=1, b=dict(b=[1, 2], c="C"))
+
+        input = os.path.join(self.workdir, "a.json")
+        output = os.path.join(self.workdir, "b.json")
+
+        A.dump(a, input)
+        self.assertTrue(os.path.exists(input))
+
+        T.main(["dummy", "-o", output, input])
+        self.assertTrue(os.path.exists(output))
+
+# vim:sw=4:ts=4:et:

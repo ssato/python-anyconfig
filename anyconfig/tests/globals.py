a9c77651217b107c4992e73c612d8f9cf75b503f
Satoru SATOH <satoru.satoh@gmail.com>
2013-03-30 12:09:08 +0900

    add (empty) test cases for anyconfig.globals

---
+++ anyconfig/tests/globals.py
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2013 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+import anyconfig.globals as TT
+import anyconfig.tests.common as C
+
+import unittest
+
+
+class Test_00(unittest.TestCase):
+    pass
+
+# vim:sw=4:ts=4:et:

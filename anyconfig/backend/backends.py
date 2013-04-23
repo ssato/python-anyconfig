81607d2c19ef70bcd84df1adeb76408faaf41b12
Satoru SATOH <satoru.satoh@gmail.com>
2013-04-23 16:46:07 +0900

    prioritize config parsers and make highest ones returnes when find by file (extension) or config type

--- anyconfig/backend/backends.py
+++ anyconfig/backend/backends.py
@@ -1,17 +1,79 @@
 #
-# Copyright (C) 2011, 2012 Satoru SATOH <ssato @ redhat.com>
+# Copyright (C) 2011 - 2013 Satoru SATOH <ssato @ redhat.com>
 # License: MIT
 #
+from itertools import groupby
+from operator import methodcaller
+
 import anyconfig.backend.base as Base
 import anyconfig.backend.ini_ as BINI
 import anyconfig.backend.json_ as BJSON
 import anyconfig.backend.xml_ as BXML
 import anyconfig.backend.yaml_ as BYAML
 import anyconfig.backend.properties_ as BPROP
+import anyconfig.utils as U
+
 
 _CPs = [p for p in [BINI.IniConfigParser, BJSON.JsonConfigParser,
                     BYAML.YamlConfigParser, BXML.XmlConfigParser,
                     BPROP.PropertiesParser] if p.supports()]
+
+
+def cmp_cps(lhs, rhs):
+    """Compare config parsers by these priorities.
+    """
+    return cmp(lhs.priority(), rhs.priority())
+
+
+def fst(tpl):
+    """
+    >>> fst((0, 1))
+    0
+    """
+    return tpl[0]
+
+
+def snd(tpl):
+    """
+    >>> snd((0, 1))
+    1
+    """
+    return tpl[1]
+
+
+def groupby_key(xs, kf):
+    return groupby(sorted(xs, key=kf), key=kf)
+
+
+def uniq(iterable):
+    """
+    >>> uniq([1, 2, 3, 1, 2])
+    [1, 2, 3]
+    """
+    acc = []
+    for x in iterable:
+        if x not in acc:
+            acc.append(x)
+
+    return acc
+
+
+def list_parsers_by_type(cps=_CPs):
+    """
+    :return: List (generator) of (config_type, [config_parser])
+    """
+    return ((t, sorted(p, cmp=cmp_cps)) for t, p in
+            groupby_key(cps, methodcaller("type")))
+
+
+def list_parsers_by_extension(cps=_CPs):
+    """
+    :return: List (generator) of (config_ext, [config_parser])
+    """
+    cps_by_ext = U.concat(([(x, p) for x in p._extensions] for p in cps))
+
+    return ((x, sorted((snd(xp) for xp in xps), cmp=cmp_cps)) for x, xps in
+            groupby_key(cps_by_ext, fst))
 
 
 def find_by_file(config_file, cps=_CPs):
@@ -20,9 +82,10 @@ def find_by_file(config_file, cps=_CPs):
 
     :param config_file: Config file path
     """
-    for cp in cps:
-        if cp.supports(config_file):
-            return cp
+    ext = U.get_file_extension(config_file)
+    for x, ps in list_parsers_by_extension(cps):
+        if x == ext:
+            return ps[-1]
 
     return None
 
@@ -33,15 +96,16 @@ def find_by_type(cptype, cps=_CPs):
 
     :param cptype: Config file's type
     """
-    for cp in cps:
-        if cp.type() == cptype:
-            return cp
+    for t, ps in list_parsers_by_type(cps):
+        if t == cptype:
+            return ps[-1]
 
     return None
 
 
 def list_types(cps=_CPs):
-    """List available config types."""
-    return [cp.type() for cp in cps]
+    """List available config types.
+    """
+    return sorted(uniq(t for t, ps in list_parsers_by_type(cps)))
 
 # vim:sw=4:ts=4:et:

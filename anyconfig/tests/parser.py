533d14783239a207245843660478cf2ad3043d70
Satoru SATOH <satoru.satoh@gmail.com>
2014-08-12 14:25:44 +0900

    sorted out test cases

--- anyconfig/tests/parser.py
+++ anyconfig/tests/parser.py
@@ -2,42 +2,57 @@
 # Copyright (C) 2011, 2012 Satoru SATOH <ssato @ redhat.com>
 # License: MIT
 #
-import anyconfig.parser as P
+import anyconfig.parser as TT
 import unittest
 
 
-class Test_parser(unittest.TestCase):
+# format: input, expected_result
+CASES = dict(single_0=[("", "")],
+             single=[("0", 0),
+                     ("123", 123),
+                     ("True", True),
+                     ("a string", "a string"),
+                     ("0.1", "0.1"),
+                     ("    a string contains extra whitespaces     ",
+                      "a string contains extra whitespaces"),
+                     ],
+             list=[("a,b", ["a", "b"]),
+                   ("1,2", [1, 2]),
+                   ("a,b,", ["a", "b"]),
+                   ],
+             attrlist_0=[("requires:bash,zsh",
+                          [('requires', ['bash', 'zsh']), ]),
+                         ("obsoletes:sysdata;conflicts:sysdata-old",
+                          [('obsoletes', 'sysdata'),
+                           ('conflicts', 'sysdata-old')]),
+                         ],
+             )
+
+
+class Test_00_parse(unittest.TestCase):
+
+    testcases = CASES
+
+    def run_cases(self, category, tfunc):
+        for inp, exp in self.testcases.get(category, []):
+            self.assertEquals(tfunc(inp), exp)
 
     def test_00_parse_single(self):
-        self.assertEquals(P.parse_single(""), "")
-        self.assertEquals(P.parse_single("0"), 0)
-        self.assertEquals(P.parse_single("123"), 123)
-        self.assertEquals(P.parse_single("True"), True)
-        self.assertEquals(P.parse_single("a string"), "a string")
-        self.assertEquals(P.parse_single("0.1"), "0.1")
-        self.assertEquals(
-            P.parse_single("    a string contains extra whitespaces     "),
-            "a string contains extra whitespaces"
-        )
+        self.run_cases("single_0", TT.parse_single)
+        self.run_cases("single", TT.parse_single)
 
-    def test_01_parse_list(self):
-        self.assertEquals(P.parse_list(""), [])
-        self.assertEquals(P.parse_list("a,b"), ["a", "b"])
-        self.assertEquals(P.parse_list("1,2"), [1, 2])
-        self.assertEquals(P.parse_list("a,b,"), ["a", "b"])
-        self.assertEquals(P.parse_list("a|b|", "|"), ["a", "b"])
+    def test_10_parse_list(self):
+        self.run_cases("list", TT.parse_list)
 
-    def test_02_parse_attrlist_0(self):
-        self.assertEquals(
-            P.parse_attrlist_0("requires:bash,zsh"),
-            [('requires', ['bash', 'zsh'])]
-        )
-        self.assertEquals(
-            P.parse_attrlist_0("obsoletes:sysdata;conflicts:sysdata-old"),
-            [('obsoletes', 'sysdata'), ('conflicts', 'sysdata-old')]
-        )
+        # A few special cases:
+        self.assertEquals(TT.parse_list(""), [])
+        self.assertEquals(TT.parse_list("a|b|", "|"), ["a", "b"])
 
-    def test_03_parse(self):
-        pass
+    def test_20_parse_attrlist_0(self):
+        self.run_cases("attrlist_0", TT.parse_attrlist_0)
+
+    def test_30_parse(self):
+        self.run_cases("single", TT.parse)
+        self.run_cases("list", TT.parse)
 
 # vim:sw=4:ts=4:et:

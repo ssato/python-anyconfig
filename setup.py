f479da9763cfe922036fd12cb7874413c284b211
Satoru SATOH <satoru.satoh@gmail.com>
2015-04-17 17:33:04 +0900

    make use of pkg/rpmbuild-wrapper.sh to build [s]rpms

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, Command, find_packages
 import glob
 import os.path
 import os
+import subprocess
 
 
 curdir = os.getcwd()
@@ -26,14 +27,10 @@ data_files = [("share/man/man1", list_filepaths("docs/"))]
 class SrpmCommand(Command):
 
     user_options = []
-
     build_stage = "s"
-    cmd_fmt = """rpmbuild -b%(build_stage)s \
-        --define \"_topdir %(rpmdir)s\" \
-        --define \"_sourcedir %(rpmdir)s\" \
-        --define \"_buildroot %(BUILDROOT)s\" \
-        %(rpmspec)s
-    """
+
+    curdir = os.path.abspath(os.curdir)
+    rpmspec = os.path.join(curdir, "python-%s.spec" % PACKAGE)
 
     def initialize_options(self):
         pass
@@ -42,30 +39,21 @@ class SrpmCommand(Command):
         pass
 
     def run(self):
+        self.pre_sdist()
         self.run_command('sdist')
         self.build_rpm()
 
+    def pre_sdist(self):
+        c = open(self.rpmspec + ".in").read()
+        open(self.rpmspec, "w").write(c.replace("@VERSION@", VERSION))
+
     def build_rpm(self):
-        params = dict()
+        rpmbuild = os.path.join(self.curdir, "pkg/rpmbuild-wrapper.sh")
+        workdir = os.path.join(self.curdir, "dist")
 
-        params["build_stage"] = self.build_stage
-        rpmdir = params["rpmdir"] = os.path.join(
-            os.path.abspath(os.curdir), "dist"
-        )
-        rpmspec = params["rpmspec"] = os.path.join(
-            rpmdir, "../python-%s.spec" % PACKAGE
-        )
-
-        for subdir in ("SRPMS", "RPMS", "BUILD", "BUILDROOT"):
-            sdir = params[subdir] = os.path.join(rpmdir, subdir)
-
-            if not os.path.exists(sdir):
-                os.makedirs(sdir, 493)  # 493 = 0o755 (py3) or 0755 (py2)
-
-        c = open(rpmspec + ".in").read()
-        open(rpmspec, "w").write(c.replace("@VERSION@", VERSION))
-
-        os.system(self.cmd_fmt % params)
+        cmd_s = "%s -w %s -s %s %s" % (rpmbuild, workdir, self.build_stage,
+                                       self.rpmspec)
+        subprocess.check_call(cmd_s, shell=True)
 
 
 class RpmCommand(SrpmCommand):

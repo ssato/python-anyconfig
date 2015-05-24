cb5c350b648f78eec4ad4c2d6735378ef28cc67c
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-24 23:27:24 +0900

    fix for the pylint error, E402 module level import not at top of file

--- anyconfig/globals.py
+++ anyconfig/globals.py
@@ -4,10 +4,12 @@
 #
 """anyconfig globals.
 """
+import anyconfig.init
+
+
 AUTHOR = "Satoru SATOH <ssat@redhat.com>"
 VERSION = "0.0.7"
 
-import anyconfig.init
 LOGGER = anyconfig.init.LOGGER
 
 # vim:sw=4:ts=4:et:

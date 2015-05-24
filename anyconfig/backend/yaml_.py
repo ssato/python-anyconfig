900fde2c40f7e1a4dfd8dda054e6c681017313db
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-25 02:00:44 +0900

    remove a global, SUPPORTED and simplified

--- anyconfig/backend/yaml_.py
+++ anyconfig/backend/yaml_.py
@@ -4,53 +4,39 @@
 #
 """YAML files parser backend
 """
-import logging
+import yaml
 import anyconfig.backend.base as Base
 
 
-LOGGER = logging.getLogger(__name__)
-SUPPORTED = False
-try:
-    import yaml
-    SUPPORTED = True
-except ImportError:
-    LOGGER.warn("YAML module is not available. Disabled its support.")
+def filter_keys(keys, filter_key):
+    """
+    :param keys: Original keys list
+    :param filter_key: Key to filter out from `keys`
+    :return: A list of keys given `filter_key` is not contained
+    """
+    return [k for k in keys if k != filter_key]
 
 
-if SUPPORTED:
-    def filter_keys(keys, filter_key):
-        """
-        """
-        return [k for k in keys if k != filter_key]
+def yaml_load(fpath, **kwargs):
+    """
+    An wrapper of yaml.{safe_,}load
+    """
+    keys = filter_keys(kwargs.keys(), "safe")
+    if kwargs.get("safe", False):
+        return yaml.safe_load(fpath, **Base.mk_opt_args(keys, kwargs))
+    else:
+        return yaml.load(fpath, **kwargs)
 
-    def yaml_load(fp, **kwargs):
-        """
-        An wrapper of yaml.{safe_,}load
-        """
-        keys = filter_keys(kwargs.keys(), "safe")
-        if kwargs.get("safe", False):
-            return yaml.safe_load(fp, **Base.mk_opt_args(keys, kwargs))
-        else:
-            return yaml.load(fp, **kwargs)
 
-    def yaml_dump(data, fp, **kwargs):
-        """
-        An wrapper of yaml.{safe_,}dump
-        """
-        keys = filter_keys(kwargs.keys(), "safe")
-        if kwargs.get("safe", False):
-            return yaml.safe_dump(data, fp, **Base.mk_opt_args(keys, kwargs))
-        else:
-            return yaml.dump(data, fp, **kwargs)
-else:
-    def yaml_load(*args, **kwargs):
-        LOGGER.warn("Return {} as YAML module is not available: "
-                    "args=%s, kwargs=%s", ','.join(args), str(kwargs))
-        return {}
-
-    def yaml_dump(*args, **kwargs):
-        LOGGER.warn("Do nothing as YAML module is not available: "
-                    "args=%s, kwargs=%s", ','.join(args), str(kwargs))
+def yaml_dump(data, fpath, **kwargs):
+    """
+    An wrapper of yaml.{safe_,}dump
+    """
+    keys = filter_keys(kwargs.keys(), "safe")
+    if kwargs.get("safe", False):
+        return yaml.safe_dump(data, fpath, **Base.mk_opt_args(keys, kwargs))
+    else:
+        return yaml.dump(data, fpath, **kwargs)
 
 
 class YamlConfigParser(Base.ConfigParser):
@@ -60,20 +46,18 @@ class YamlConfigParser(Base.ConfigParser):
 
     _type = "yaml"
     _extensions = ("yaml", "yml")
-    _supported = SUPPORTED
-
     _load_opts = ["Loader", "safe"]
     _dump_opts = ["stream", "Dumper"]
 
     @classmethod
-    def load_impl(cls, config_fp, **kwargs):
+    def load_impl(cls, config_content, **kwargs):
         """
-        :param config_fp:  Config file content
+        :param config_content:  Config file content
         :param kwargs: backend-specific optional keyword parameters :: dict
 
         :return: dict object holding config parameters
         """
-        return yaml_load(config_fp, **kwargs)
+        return yaml_load(config_content, **kwargs)
 
     @classmethod
     def dumps_impl(cls, data, **kwargs):

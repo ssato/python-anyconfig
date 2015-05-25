803948607dbd6c815036e5b081a433b6800c3a0b
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-25 16:15:23 +0900

    make it using anyconfig.tests.common.{setup_workdir,cleanup_workdir} and fix some pylint warns

--- anyconfig/backend/tests/ini_.py
+++ anyconfig/backend/tests/ini_.py
@@ -3,11 +3,11 @@
 # License: MIT
 #
 # pylint: disable=missing-docstring
-import anyconfig.backend.ini_ as I
-
-import os
-import tempfile
+import os.path
 import unittest
+
+import anyconfig.backend.ini_ as TT
+import anyconfig.tests.common
 
 
 CONF_0 = """[DEFAULT]
@@ -19,63 +19,53 @@ c: x,y,z
 """
 
 
-class Test_IniConfigParser(unittest.TestCase):
+class Test(unittest.TestCase):
 
     def setUp(self):
-        (_, conf) = tempfile.mkstemp(prefix="ac-test-")
-        open(conf, 'w').write(CONF_0)
-        self.config_path = conf
+        self.workdir = anyconfig.tests.common.setup_workdir()
+        self.cpath = os.path.join(self.workdir, "conf0.ini")
+        open(self.cpath, 'w').write(CONF_0)
 
     def tearDown(self):
-        os.remove(self.config_path)
-
-    def test_00_supports(self):
-        self.assertTrue(I.IniConfigParser.supports("/a/b/c/d.ini"))
-        self.assertFalse(I.IniConfigParser.supports("/a/b/c/d.json"))
+        anyconfig.tests.common.cleanup_workdir(self.workdir)
 
     def test_10_loads(self):
-
-        c = I.IniConfigParser.loads(CONF_0)
-        self.assertEquals(c["DEFAULT"]['a'], 0, str(c))
-        self.assertEquals(c["DEFAULT"]['b'], "bbb", c)
-        self.assertEquals(c["sect0"]['c'], ['x', 'y', 'z'])
+        cfg = TT.IniConfigParser.loads(CONF_0)
+        self.assertEquals(cfg["DEFAULT"]['a'], 0, str(cfg))
+        self.assertEquals(cfg["DEFAULT"]['b'], "bbb", cfg)
+        self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
 
     def test_20_load(self):
+        cfg = TT.IniConfigParser.load(self.cpath)
+        self.assertEquals(cfg["DEFAULT"]['a'], 0, str(cfg))
+        self.assertEquals(cfg["DEFAULT"]['b'], "bbb", cfg)
+        self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
 
-        c = I.IniConfigParser.load(self.config_path)
-        self.assertEquals(c["DEFAULT"]['a'], 0, str(c))
-        self.assertEquals(c["DEFAULT"]['b'], "bbb", c)
-        self.assertEquals(c["sect0"]['c'], ['x', 'y', 'z'])
-
-    def test_should_show_error_on_invalid_ini(self):
+    def test_22_load__invalid_ini(self):
         invalid_ini = "key=name"
-        self.assertRaises(Exception, I.IniConfigParser.loads, invalid_ini)
+        self.assertRaises(Exception, TT.IniConfigParser.loads, invalid_ini)
 
     def test_20_load__w_options(self):
-
-        c = I.IniConfigParser.load(self.config_path, allow_no_value=False)
-        self.assertEquals(c["DEFAULT"]['a'], 0, str(c))
-        self.assertEquals(c["DEFAULT"]['b'], "bbb", c)
-        self.assertEquals(c["sect0"]['c'], ['x', 'y', 'z'])
+        cfg = TT.IniConfigParser.load(self.cpath, allow_no_value=False)
+        self.assertEquals(cfg["DEFAULT"]['a'], 0, str(cfg))
+        self.assertEquals(cfg["DEFAULT"]['b'], "bbb", cfg)
+        self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
 
     def test_30_dumps(self):
+        cfg = TT.IniConfigParser.loads(CONF_0)
+        cfg2 = TT.IniConfigParser.loads(TT.IniConfigParser.dumps(cfg))
 
-        c = I.IniConfigParser.loads(CONF_0)
-        d = I.IniConfigParser.dumps(c)
-        c2 = I.IniConfigParser.loads(d)
-
-        self.assertEquals(c2["DEFAULT"]['a'], c["DEFAULT"]['a'], str(c2))
-        self.assertEquals(c2["DEFAULT"]['b'], c["DEFAULT"]['b'], str(c2))
-        self.assertEquals(c2["sect0"]['c'], c["sect0"]['c'], str(c2))
+        self.assertEquals(cfg2["DEFAULT"]['a'], cfg["DEFAULT"]['a'], str(cfg2))
+        self.assertEquals(cfg2["DEFAULT"]['b'], cfg["DEFAULT"]['b'], str(cfg2))
+        self.assertEquals(cfg2["sect0"]['c'], cfg["sect0"]['c'], str(cfg2))
 
     def test_40_dump(self):
+        cfg = TT.IniConfigParser.loads(CONF_0)
+        TT.IniConfigParser.dump(cfg, self.cpath)
+        cfg2 = TT.IniConfigParser.load(self.cpath)
 
-        c = I.IniConfigParser.loads(CONF_0)
-        I.IniConfigParser.dump(c, self.config_path)
-        c2 = I.IniConfigParser.load(self.config_path)
-
-        self.assertEquals(c2["DEFAULT"]['a'], c["DEFAULT"]['a'], str(c2))
-        self.assertEquals(c2["DEFAULT"]['b'], c["DEFAULT"]['b'], str(c2))
-        self.assertEquals(c2["sect0"]['c'], c["sect0"]['c'], str(c2))
+        self.assertEquals(cfg2["DEFAULT"]['a'], cfg["DEFAULT"]['a'], str(cfg2))
+        self.assertEquals(cfg2["DEFAULT"]['b'], cfg["DEFAULT"]['b'], str(cfg2))
+        self.assertEquals(cfg2["sect0"]['c'], cfg["sect0"]['c'], str(cfg2))
 
 # vim:sw=4:ts=4:et:

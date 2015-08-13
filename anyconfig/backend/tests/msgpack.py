ff3a521616834e45970f40cd2b681289cffc0694
Satoru SATOH <satoru.satoh@gmail.com>
2015-08-13 14:48:03 +0900

    remove duplicated code and cleanup test cases

--- anyconfig/backend/tests/msgpack.py
+++ anyconfig/backend/tests/msgpack.py
@@ -3,6 +3,7 @@
 # License: MIT
 #
 # pylint: disable=missing-docstring
+import copy
 import os.path
 import unittest
 
@@ -10,78 +11,61 @@ import anyconfig.backend.msgpack as TT
 import anyconfig.tests.common
 import anyconfig.compat
 
-
-CONF_0 = {"a": 0.1,
-          "b": "bbb",
-          "sect0": {"c": ["x", "y", "z"]}}
+from anyconfig.tests.common import dicts_equal
 
 
-def _b(astr):
+def _bytes(astr):
     return bytes(astr, 'utf-8') if anyconfig.compat.IS_PYTHON_3 else astr
+
+
+CNF_0 = {_bytes("a"): 0.1,
+         _bytes("b"): _bytes("bbb"),
+         _bytes("sect0"): {_bytes("c"): [_bytes("x"), _bytes("y"),
+                                         _bytes("z")]}}
 
 
 class Test(unittest.TestCase):
 
     def setUp(self):
+        self.cnf = CNF_0
         self.workdir = anyconfig.tests.common.setup_workdir()
         self.cpath = os.path.join(self.workdir, "test0.msgpack")
-        self.packed = TT.msgpack.packb(CONF_0)
+        self.packed = TT.msgpack.packb(self.cnf)
         open(self.cpath, 'wb').write(self.packed)
 
     def tearDown(self):
         anyconfig.tests.common.cleanup_workdir(self.workdir)
 
     def test_10_loads(self):
-        cfg = TT.Parser.loads(self.packed)
-
-        self.assertEquals(cfg[_b('a')], 0.1, str(cfg))
-        self.assertEquals(cfg[_b('b')], _b("bbb"), cfg)
-        self.assertEquals(cfg[_b('sect0')][_b('c')],
-                          [_b('x'), _b('y'), _b('z')])
+        cnf = TT.Parser.loads(self.packed)
+        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
     def test_20_load(self):
-        cfg = TT.Parser.load(self.cpath)
-
-        self.assertEquals(cfg[_b('a')], 0.1, str(cfg))
-        self.assertEquals(cfg[_b('b')], _b("bbb"), cfg)
-        self.assertEquals(cfg[_b('sect0')][_b('c')],
-                          [_b('x'), _b('y'), _b('z')])
+        cnf = TT.Parser.load(self.cpath)
+        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
     def test_22_load__optional_kwargs(self):
-        cfg = TT.Parser.load(self.cpath, use_list=False)
-
-        self.assertEquals(cfg[_b('a')], 0.1, str(cfg))
-        self.assertEquals(cfg[_b('b')], _b("bbb"), cfg)
-        self.assertEquals(cfg[_b('sect0')][_b('c')],
-                          (_b('x'), _b('y'), _b('z')))
+        cnf = TT.Parser.load(self.cpath, use_list=False)
+        ref = copy.deepcopy(self.cnf)
+        ref[_bytes("sect0")][_bytes("c")] = (_bytes("x"), _bytes("y"),
+                                             _bytes("z"))
+        self.assertTrue(dicts_equal(cnf, ref), str(cnf))
 
     def test_30_dumps(self):
-        cfg = TT.Parser.loads(self.packed)
-        cfg2 = TT.Parser.loads(TT.Parser.dumps(cfg))
-
-        self.assertEquals(cfg2[_b('a')], 0.1, str(cfg))
-        self.assertEquals(cfg2[_b('b')], _b("bbb"), cfg)
-        self.assertEquals(cfg2[_b('sect0')][_b('c')],
-                          [_b('x'), _b('y'), _b('z')])
+        cnf = TT.Parser.loads(TT.Parser.dumps(self.cnf))
+        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
     def test_40_dump(self):
-        cfg = TT.Parser.loads(self.packed)
-        TT.Parser.dump(cfg, self.cpath)
-        cfg = TT.Parser.load(self.cpath)
-
-        self.assertEquals(cfg[_b('a')], 0.1, str(cfg))
-        self.assertEquals(cfg[_b('b')], _b("bbb"), cfg)
-        self.assertEquals(cfg[_b('sect0')][_b('c')],
-                          [_b('x'), _b('y'), _b('z')])
+        TT.Parser.dump(self.cnf, self.cpath)
+        cnf = TT.Parser.load(self.cpath)
+        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
     def test_42_dump_w_special_option(self):
-        cfg = TT.Parser.loads(self.packed)
-        TT.Parser.dump(cfg, self.cpath, use_single_float=True)
-        cfg = TT.Parser.load(self.cpath)
-
-        self.assertNotEquals(cfg[_b('a')], 0.1, str(cfg))
-        self.assertEquals(cfg[_b('b')], _b("bbb"), cfg)
-        self.assertEquals(cfg[_b('sect0')][_b('c')],
-                          [_b('x'), _b('y'), _b('z')])
+        TT.Parser.dump(self.cnf, self.cpath, use_single_float=True)
+        cnf = TT.Parser.load(self.cpath)
+        ref = copy.deepcopy(self.cnf)
+        ref[_bytes("a")] = cnf[_bytes("a")]  # single float value.
+        self.assertFalse(dicts_equal(cnf, self.cnf), str(cnf))
+        self.assertTrue(dicts_equal(cnf, ref), str(cnf))
 
 # vim:sw=4:ts=4:et:

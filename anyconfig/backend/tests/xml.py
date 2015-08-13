e73984f9308db1bd6de5e4e2b4df44b9869373ce
Satoru SATOH <satoru.satoh@gmail.com>
2015-08-13 15:35:36 +0900

    simplified comparison code in test cases

--- anyconfig/backend/tests/xml.py
+++ anyconfig/backend/tests/xml.py
@@ -10,9 +10,10 @@ import unittest
 
 import anyconfig.backend.xml as TT
 import anyconfig.tests.common
+from anyconfig.tests.common import dicts_equal
 
 
-CONF_0 = """<?xml version="1.0" encoding="UTF-8"?>
+CNF_0_S = """<?xml version="1.0" encoding="UTF-8"?>
 <config name='foo'>
   <a>0</a>
   <b id="b0">bbb</b>
@@ -22,13 +23,23 @@ CONF_0 = """<?xml version="1.0" encoding="UTF-8"?>
 </config>
 """
 
+CNF_0 = {'config': {'attrs': {'name': 'foo'},
+                    'children': [{'a': {'text': '0'}},
+                                 {'b': {'attrs': {'id': 'b0'}, 'text': 'bbb'}},
+                                 {'sect0': {
+                                     'children': [{'c': {'text': 'x, y, z'}}]
+                                 }}]}}
+
 
 class Test(unittest.TestCase):
+
+    cnf = CNF_0
+    cnf_s = CNF_0_S
 
     def setUp(self):
         self.workdir = anyconfig.tests.common.setup_workdir()
         self.cpath = os.path.join(self.workdir, "test0.xml")
-        open(self.cpath, 'w').write(CONF_0)
+        open(self.cpath, 'w').write(self.cnf_s)
 
     def tearDown(self):
         anyconfig.tests.common.cleanup_workdir(self.workdir)
@@ -38,34 +49,8 @@ class Test(unittest.TestCase):
         self.assertTrue(TT.Parser.supports("/a/b/c/d.xml"))
 
     def test_10_loads(self):
-        cfg = TT.Parser.loads(CONF_0)
-
-        self.assertTrue("config" in cfg)
-        self.assertTrue("attrs" in cfg["config"])
-        self.assertTrue("name" in cfg["config"]["attrs"])
-        self.assertEquals(cfg["config"]["attrs"].get("name", None), "foo")
-
-        self.assertTrue(isinstance(cfg["config"]["children"], list))
-        self.assertNotEquals(cfg["config"]["children"], [])
-
-        self.assertTrue('a' in cfg["config"]["children"][0])
-        self.assertTrue("text" in cfg["config"]["children"][0]['a'])
-        self.assertTrue("attrs" not in cfg["config"]["children"][0]['a'])
-        self.assertEquals(cfg["config"]["children"][0]['a']["text"], '0')
-
-        self.assertTrue('b' in cfg["config"]["children"][1])
-        self.assertTrue("text" in cfg["config"]["children"][1]['b'])
-        self.assertTrue("attrs" in cfg["config"]["children"][1]['b'])
-        self.assertEquals(cfg["config"]["children"][1]['b']["text"], "bbb")
-        self.assertTrue("id" in cfg["config"]["children"][1]['b']["attrs"])
-        self.assertEquals(cfg["config"]["children"][1]['b']["attrs"]["id"],
-                          "b0")
-
-        self.assertTrue('sect0' in cfg["config"]["children"][2])
-        self.assertTrue("text" not in cfg["config"]["children"][2]['sect0'])
-        self.assertTrue("attrs" not in cfg["config"]["children"][2]['sect0'])
-        self.assertTrue("children" in cfg["config"]["children"][2]['sect0'])
-        self.assertTrue(cfg["config"]["children"][2]['sect0']["children"])
+        cnf = TT.Parser.loads(self.cnf_s)
+        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
     def test_20_load(self):
         """FIXME: Implement test cases for Parser.load"""

3e44f30c99358c65d3ddbe61ef319e25dd7c5584
Satoru SATOH <satoru.satoh@gmail.com>
2015-08-13 14:56:06 +0900

    remove duplicated code and cleanup test cases

--- anyconfig/backend/tests/yaml.py
+++ anyconfig/backend/tests/yaml.py
@@ -14,9 +14,10 @@ except ImportError:
     TT = None
 
 import anyconfig.tests.common
+from anyconfig.tests.common import dicts_equal
 
 
-CONF_0 = """
+CNF_0_S = """
 a: 0
 b: bbb
 
@@ -24,15 +25,21 @@ sect0:
   c: ["x", "y", "z"]
 """
 
+CNF_0 = {'a': 0, 'b': 'bbb', 'sect0': {'c': ['x', 'y', 'z']}}
+
+
 if TT is not None:
     import yaml
 
     class Test(unittest.TestCase):
 
+        cnf = CNF_0
+        cnf_s = CNF_0_S
+
         def setUp(self):
             self.workdir = anyconfig.tests.common.setup_workdir()
             self.cpath = os.path.join(self.workdir, "test0.yml")
-            open(self.cpath, 'w').write(CONF_0)
+            open(self.cpath, 'w').write(self.cnf_s)
 
         def tearDown(self):
             anyconfig.tests.common.cleanup_workdir(self.workdir)
@@ -43,82 +50,46 @@ if TT is not None:
             self.assertTrue(TT.Parser.supports("/a/b/c/d.yml"))
 
         def test_10_loads(self):
-            cfg = TT.Parser.loads(CONF_0)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            cnf = TT.Parser.loads(self.cnf_s)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_12_loads__safe(self):
-            cfg = TT.Parser.loads(CONF_0, safe=True)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            cnf = TT.Parser.loads(self.cnf_s, safe=True)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_20_load(self):
-            cfg = TT.Parser.load(self.cpath)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            cnf = TT.Parser.load(self.cpath)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_22_load__safe(self):
-            cfg = TT.Parser.load(self.cpath, safe=True)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            cnf = TT.Parser.load(self.cpath, safe=True)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_20_load__w_options(self):
-            cfg = TT.Parser.load(self.cpath, Loader=yaml.loader.Loader)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            cnf = TT.Parser.load(self.cpath, Loader=yaml.loader.Loader)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_30_dumps(self):
-            cfg = TT.Parser.loads(CONF_0)
-            cfg = TT.Parser.loads(TT.Parser.dumps(cfg))
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            cnf = TT.Parser.loads(TT.Parser.dumps(self.cnf))
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_32_dumps__safe(self):
-            cfg = TT.Parser.loads(CONF_0)
-            res = TT.Parser.dumps(cfg, safe=True)
-            cfg = TT.Parser.loads(res)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            cnf = TT.Parser.loads(TT.Parser.dumps(self.cnf, safe=True))
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_40_dump(self):
-            cfg = TT.Parser.loads(CONF_0)
-            TT.Parser.dump(cfg, self.cpath)
-            cfg = TT.Parser.load(self.cpath)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            TT.Parser.dump(self.cnf, self.cpath)
+            cnf = TT.Parser.load(self.cpath)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_42_dump__safe(self):
-            cfg = TT.Parser.loads(CONF_0)
-            TT.Parser.dump(cfg, self.cpath, safe=True)
-            cfg = TT.Parser.load(self.cpath)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            TT.Parser.dump(self.cnf, self.cpath, safe=True)
+            cnf = TT.Parser.load(self.cpath)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
         def test_40_dump__w_options(self):
-            cfg = TT.Parser.loads(CONF_0)
-            TT.Parser.dump(cfg, self.cpath, Dumper=yaml.dumper.Dumper)
-            cfg = TT.Parser.load(self.cpath)
-
-            self.assertEquals(cfg['a'], 0, str(cfg))
-            self.assertEquals(cfg['b'], "bbb", cfg)
-            self.assertEquals(cfg["sect0"]['c'], ['x', 'y', 'z'])
+            TT.Parser.dump(self.cnf, self.cpath, Dumper=yaml.dumper.Dumper)
+            cnf = TT.Parser.load(self.cpath)
+            self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
 # vim:sw=4:ts=4:et:

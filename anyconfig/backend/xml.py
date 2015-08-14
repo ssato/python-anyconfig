d2b3e5584f1f645c798e1b0a304ea411000e906d
Satoru SATOH <satoru.satoh@gmail.com>
2015-08-14 11:46:24 +0900

    implement XML dump/dumps

--- anyconfig/backend/xml.py
+++ anyconfig/backend/xml.py
@@ -89,6 +89,50 @@ def etree_to_container(root, cls, attrs="@attrs", text="@text",
     return tree
 
 
+def container_to_etree(obj, cls, parent=None, attrs="@attrs", text="@text",
+                       children="@children"):
+    """
+    Convert a container object to XML ElementTree.
+
+    :param obj: Container instance to convert to
+    :param cls: Container class
+    :param parent: XML ElementTree parent node object or None
+    :param attrs: Key name for XML attributes
+    :param text: Key name for XML text elements
+    :param children: Key name for XML child elements
+
+    >>> obj = {'config': {'@attrs': {'name': 'foo'},
+    ...                   '@children': [{'a': {'@text': '0'}},
+    ...                                 {'b': {'@attrs': {'id': 'b0'},
+    ...                                        '@text': 'bbb'}}]}}
+    >>> tree = container_to_etree(obj, Parser.container())
+    >>> buf = anyconfig.compat.StringIO()
+    >>> tree.write(buf)
+    >>> buf.getvalue()
+    '<config name="foo"><a>0</a><b id="b0">bbb</b></config>'
+    >>>
+    """
+    if not isinstance(obj, (cls, dict)):
+        return  # All attributes and text should be set already.
+
+    for key, val in anyconfig.compat.iteritems(obj):
+        if key == attrs:
+            for attr, aval in anyconfig.compat.iteritems(val):
+                parent.set(attr, aval)
+        elif key == text:
+            parent.text = val
+        elif key == children:
+            for child in val:  # child should be a dict-like object.
+                for ckey, cval in anyconfig.compat.iteritems(child):
+                    celem = ET.Element(ckey)
+                    container_to_etree(cval, cls, celem, attrs, text, children)
+                    parent.append(celem)
+        else:
+            elem = ET.Element(key)
+            container_to_etree(val, cls, elem, attrs, text, children)
+            return ET.ElementTree(elem)
+
+
 class Parser(anyconfig.backend.base.Parser):
     """
     Parser for XML files.
@@ -119,13 +163,27 @@ class Parser(anyconfig.backend.base.Parser):
         return etree_to_container(root, cls.container())
 
     @classmethod
-    def dumps_impl(cls, data, **kwargs):
+    def dumps(cls, obj, **kwargs):
         """
-        :param data: Data to dump :: dict
+        :param obj: Data to dump :: dict
         :param kwargs: backend-specific optional keyword parameters :: dict
 
         :return: string represents the configuration
         """
-        raise NotImplementedError("XML dumper not implemented yet!")
+        root = container_to_etree(obj, cls.container())
+        buf = anyconfig.compat.StringIO()
+        root.write(buf, encoding='UTF-8', xml_declaration=True)
+        return buf.getvalue()
+
+    @classmethod
+    def dump_impl(cls, obj, config_path, **kwargs):
+        """
+        :param obj: Data to dump :: dict
+        :param config_path: Dump destination file path
+        :param kwargs: backend-specific optional keyword parameters :: dict
+        """
+        root = container_to_etree(obj, cls.container())
+        with open(config_path, cls._open_flags[1]) as out:
+            root.write(out, encoding='UTF-8', xml_declaration=True)
 
 # vim:sw=4:ts=4:et:

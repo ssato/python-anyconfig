5f8d8d89237158874b4b4a3e04d3a0f1992f235e
Satoru SATOH <satoru.satoh@gmail.com>
2015-08-31 09:17:10 +0900

    remove globals

--- anyconfig/backend/msgpack.py
+++ anyconfig/backend/msgpack.py
@@ -24,26 +24,20 @@ import msgpack
 import anyconfig.backend.json
 
 
-_LOAD_OPTS = ["read_size", "use_list", "object_pairs_hook", "list_hook",
-              "encoding", "unicode_errors", "max_buffer_size", "ext_hook",
-              "max_str_len", "max_bin_len", "max_array_len", "max_map_len",
-              "max_ext_len", "object_pairs_hook"]
-
-_DUMP_OPTS = ["default", "encoding", "unicode_errors", "use_single_float",
-              "autoreset", "use_bin_type"]
-
-
 class Parser(anyconfig.backend.json.Parser):
     """
     Loader/Dumper for MessagePack files.
     """
     _type = "msgpack"
     _extensions = []
-    _load_opts = _LOAD_OPTS
-    _dump_opts = _DUMP_OPTS
+    _load_opts = ["read_size", "use_list", "object_pairs_hook", "list_hook",
+                  "encoding", "unicode_errors", "max_buffer_size", "ext_hook",
+                  "max_str_len", "max_bin_len", "max_array_len", "max_map_len",
+                  "max_ext_len", "object_pairs_hook"]
+    _dump_opts = ["default", "encoding", "unicode_errors", "use_single_float",
+                  "autoreset", "use_bin_type"]
+    _open_flags = ('rb', 'wb')
     _funcs = dict(loads=msgpack.unpackb, load=msgpack.unpack,
                   dumps=msgpack.packb, dump=msgpack.pack)
-
-    _open_flags = ('rb', 'wb')
 
 # vim:sw=4:ts=4:et:

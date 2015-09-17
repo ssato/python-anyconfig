1e68e45c83f9021727226f2dcc49de24b0985e89
Satoru SATOH <satoru.satoh@gmail.com>
2015-09-17 13:20:58 +0900

    sort out test cases of anyconfig.mergeabledict

--- anyconfig/tests/mergeabledict.py
+++ anyconfig/tests/mergeabledict.py
@@ -6,7 +6,7 @@ import unittest
 import anyconfig.mergeabledict as TT
 
 
-class Test00(unittest.TestCase):
+class Test00Functions(unittest.TestCase):
 
     def test_create_from__convert_to(self):
         a = dict(name="a", a=1, b=dict(b=[1, 2], c="C"), e=[3, 4])
@@ -18,9 +18,53 @@ class Test00(unittest.TestCase):
         self.assertFalse(isinstance(c, TT.MergeableDict))
 
 
-class Test10(unittest.TestCase):
+class Test10MergeableDict(unittest.TestCase):
 
-    def test_update__w_merge_dicts(self):
+    mk_mdict = TT.MergeableDict.create
+
+    def test_20_update__w_replace(self):
+        a = self.mk_mdict(dict(name="a", a=1, b=dict(b=[1, 2], c="C")))
+        b = self.mk_mdict(dict(a=2, b=dict(b=[3, 4, 5], d="D")))
+
+        ref = TT.MergeableDict(**a.copy())
+        ref['a'] = 2
+        ref['b'] = b['b']
+        ref['b']['c'] = a['b']['c']
+
+        a.update(b, TT.MS_REPLACE)
+        self.assertEquals(a, ref)
+
+    def test_20_update__w_replace__not_a_dict(self):
+        a = TT.MergeableDict()
+        a.update(1, TT.MS_REPLACE)
+
+        # NOTE: It does not work.
+        try:
+            self.assertEquals(a, 1)
+        except AssertionError:
+            pass
+
+    def test_20_update__w_None(self):
+        a = TT.MergeableDict(name="a", a=1,
+                             b=TT.MergeableDict(b=[1, 2], c="C"))
+        ref = TT.MergeableDict(**a.copy())
+        a.update(None)
+
+        self.assertEquals(a, ref)
+
+    def test_30_update__wo_replace(self):
+        a = TT.MergeableDict(a=1, b=TT.MergeableDict(b=[1, 2], c="C"))
+        b = TT.MergeableDict(name="foo", a=2,
+                             b=TT.MergeableDict(b=[3, 4, 5], d="D"))
+
+        ref = TT.MergeableDict(**a.copy())
+        ref['name'] = b['name']
+
+        a.update(b, TT.MS_NO_REPLACE)
+
+        self.assertEquals(a, ref)
+
+    def test_40_update__w_merge_dicts(self):
         a = TT.MergeableDict(name="a", a=1,
                              b=TT.MergeableDict(b=[1, 2], c="C"),
                              e=[3, 4])
@@ -35,7 +79,7 @@ class Test10(unittest.TestCase):
 
         self.assertEquals(a, ref)
 
-    def test_update__w_merge_dicts_and_lists(self):
+    def test_40_update__w_merge_dicts_and_lists(self):
         a = TT.MergeableDict(name="a", a=1,
                              b=TT.MergeableDict(b=[1, 2], c="C"))
         b = TT.MergeableDict(a=2,
@@ -46,49 +90,6 @@ class Test10(unittest.TestCase):
         ref['b'] = TT.MergeableDict(b=[1, 2, 3, 4], c="C", d="D", e=[1, 2])
 
         a.update(b, TT.MS_DICTS_AND_LISTS)
-
-        self.assertEquals(a, ref)
-
-    def test_update__w_replace(self):
-        a = TT.MergeableDict(name="a", a=1,
-                             b=TT.MergeableDict(b=[1, 2], c="C"))
-        b = TT.MergeableDict(a=2, b=TT.MergeableDict(b=[3, 4, 5], d="D"))
-
-        ref = TT.MergeableDict(**a.copy())
-        ref['a'] = 2
-        ref['b'] = b['b']
-        ref['b']['c'] = a['b']['c']
-
-        a.update(b, TT.MS_REPLACE)
-        self.assertEquals(a, ref)
-
-    def test_update__w_replace__not_a_dict(self):
-        a = TT.MergeableDict()
-        a.update(1, TT.MS_REPLACE)
-
-        # NOTE: It does not work.
-        try:
-            self.assertEquals(a, 1)
-        except AssertionError:
-            pass
-
-    def test_update__wo_replace(self):
-        a = TT.MergeableDict(a=1, b=TT.MergeableDict(b=[1, 2], c="C"))
-        b = TT.MergeableDict(name="foo", a=2,
-                             b=TT.MergeableDict(b=[3, 4, 5], d="D"))
-
-        ref = TT.MergeableDict(**a.copy())
-        ref['name'] = b['name']
-
-        a.update(b, TT.MS_NO_REPLACE)
-
-        self.assertEquals(a, ref)
-
-    def test_update__w_None(self):
-        a = TT.MergeableDict(name="a", a=1,
-                             b=TT.MergeableDict(b=[1, 2], c="C"))
-        ref = TT.MergeableDict(**a.copy())
-        a.update(None)
 
         self.assertEquals(a, ref)
 

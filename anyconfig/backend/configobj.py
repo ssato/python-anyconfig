6deac8650e799fbd2855b148804668d115e8b23a
Satoru SATOH <satoru.satoh@gmail.com>
2015-12-24 23:51:41 +0900

    change: follow the internal API changes in ConfigObj backend
    - follow the internal API changes in anyconfig.backend.base, in
      ConfigObj backnend
    - try to find out loading options automatically from inspection result
    - make loading and dumping options undistinguished

--- anyconfig/backend/configobj.py
+++ anyconfig/backend/configobj.py
@@ -4,6 +4,11 @@
 #
 """configobj backend.
 
+.. versionchanged:: 0.4.99
+   Now loading and dumping options are detected automatically from inspection
+   result if possible. Also these became not distinguished because these will
+   be passed to configobj.Configuration anyway.
+
 - Format to support: configobj, http://goo.gl/JbP2Kp (readthedocs.org)
 - Requirements: configobj (https://pypi.python.org/pypi/configobj/)
 - Limitations: None obvious
@@ -17,7 +22,18 @@
 from __future__ import absolute_import
 
 import configobj
+import inspect
 import anyconfig.backend.base
+
+
+try:
+    _LOAD_OPTS = [a for a in inspect.getargspec(configobj.ConfigObj).args
+                  if a not in "self infile".split()]
+except (TypeError, AttributeError):
+    _LOAD_OPTS = ("options configspec encoding interpolation raise_errors"
+                  "list_values create_empty file_error stringify"
+                  "indent_type default_encoding unrepr write_empty_values"
+                  "_inspec").split()
 
 
 def make_configobj(cnf, **kwargs):
@@ -42,16 +58,19 @@ class Parser(anyconfig.backend.base.FromStreamLoader,
     """
     _type = "configobj"
     _priority = 10
-    _load_opts = ["cls", "configspec", "encoding", "interpolation",
-                  "raise_errors", "list_values", "create_empty", "file_error",
-                  "stringify", "indent_type", "default_encoding", "unrepr",
-                  "_inspec", ]
-    _dump_opts = ["cls", "encoding", "list_values", "indent_type",
-                  "default_encoding", "unrepr", "write_empty_values", ]
+    _load_opts = _LOAD_OPTS  # options on dump will be just ignored.
+    _dump_opts = _LOAD_OPTS  # Likewise.
     _open_flags = ('rb', 'wb')
 
-    load_from_path = anyconfig.backend.base.to_method(configobj.ConfigObj)
-    load_from_stream = anyconfig.backend.base.to_method(configobj.ConfigObj)
+    def __load(self, path_or_strm, **options):
+        """
+        :param path_or_strm: input config file path or file/file-like object
+        """
+        to_container = anyconfig.backend.base.to_container_fn(**options)
+        kwargs = self._load_options(**options)
+        return to_container(configobj.ConfigObj(path_or_strm, **kwargs))
+
+    load_from_path = load_from_stream = __load
 
     def dump_to_string(self, cnf, **kwargs):
         """

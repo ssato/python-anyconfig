82d9276bcd068c35a34515bddf8152de120aba2c
Satoru SATOH <satoru.satoh@gmail.com>
2016-01-04 01:03:41 +0900

    fix: add workaround for flake8 error (import-not-used error)

--- m9dicts/__init__.py
+++ m9dicts/__init__.py
@@ -2,15 +2,18 @@
 .. module:: m9dicts
    :synopsis: dict-like objects support recursive merge operations
 """
-from .globals import MS_REPLACE, MS_NO_REPLACE, MS_DICTS, MS_DICTS_AND_LISTS
+from .globals import (
+    MS_REPLACE, MS_NO_REPLACE, MS_DICTS, MS_DICTS_AND_LISTS
+)  # flake8: noqa
 from .dicts import (
     UpdateWithReplaceDict, UpdateWoReplaceDict, UpdateWithMergeDict,
     UpdateWithMergeListsDict, UpdateWithReplaceOrderedDict,
     UpdateWoReplaceOrderedDict, UpdateWithMergeOrderedDict,
     UpdateWithMergeListsOrderedDict
-)
-from .api import get, set_, make, convert_to
+)  # flake8: noqa
+from .api import get, set_, make, convert_to  # flake8: noqa
 
 __author__ = "Satoru SATOH <ssato@redhat.com>"
 __version__ = "0.1.0"
+
 # vim:sw=4:ts=4:et:

cba13ef42f2fbf248cff552e1dab34d70bf6e471
Satoru SATOH <satoru.satoh@gmail.com>
2016-01-08 19:50:14 +0900

    change: export MERGE_STRATEGIES and .utils.is_dict_like

--- m9dicts/__init__.py
+++ m9dicts/__init__.py
@@ -3,7 +3,7 @@
    :synopsis: dict-like objects support recursive merge operations
 """
 from .globals import (
-    MS_REPLACE, MS_NO_REPLACE, MS_DICTS, MS_DICTS_AND_LISTS
+    MS_REPLACE, MS_NO_REPLACE, MS_DICTS, MS_DICTS_AND_LISTS, MERGE_STRATEGIES
 )  # flake8: noqa
 from .dicts import (
     UpdateWithReplaceDict, UpdateWoReplaceDict, UpdateWithMergeDict,
@@ -12,6 +12,7 @@ from .dicts import (
     UpdateWithMergeListsOrderedDict
 )  # flake8: noqa
 from .api import get, set_, make, convert_to  # flake8: noqa
+from .utils import is_dict_like
 
 __author__ = "Satoru SATOH <ssato@redhat.com>"
 __version__ = "0.1.0"

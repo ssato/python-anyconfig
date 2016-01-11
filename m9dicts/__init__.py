edf2caa0699d533ac97300597071d31c03f18368
Satoru SATOH <satoru.satoh@gmail.com>
2016-01-11 23:54:27 +0900

    change: export is_namedtuple and is_list_like from m9dicts.utils

--- m9dicts/__init__.py
+++ m9dicts/__init__.py
@@ -12,7 +12,7 @@ from .dicts import (
     UpdateWithMergeListsOrderedDict
 )  # flake8: noqa
 from .api import get, set_, make, convert_to  # flake8: noqa
-from .utils import is_dict_like
+from .utils import is_dict_like, is_namedtuple, is_list_like
 
 __author__ = "Satoru SATOH <ssato@redhat.com>"
 __version__ = "0.1.0"

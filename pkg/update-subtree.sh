e8ea053580d4d7104a69a73819846445e0f23e1b
Satoru SATOH <satoru.satoh@gmail.com>
2016-01-15 00:55:06 +0900

    change: not pull m9dicts and squashed its git log

--- pkg/update-subtree.sh
+++ pkg/update-subtree.sh
@@ -5,8 +5,8 @@ set -ex
 commit=m9dicts/master
 subdir=m9dicts
 
-git pull ${subdir}
-git merge -s ours --no-commit ${commit:?}
+#git pull ${subdir}
+#git merge -s ours --no-commit ${commit:?}
 git rm -rf ${subdir:?}
 git read-tree --prefix=${subdir}/ -u ${commit}:${subdir}
 git commit

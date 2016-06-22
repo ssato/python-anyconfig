4b732efbff1f55fa5720ae954ba2059a25704dea
Satoru SATOH <satoru.satoh@gmail.com>
2016-06-23 03:46:06 +0900

    enhancement: add some test cases of anyconfig.gen_schema if newly added ac_schema_type was strict

--- anyconfig/tests/schema.py
+++ anyconfig/tests/schema.py
@@ -11,7 +11,7 @@ import anyconfig.schema as TT
 from anyconfig.tests.common import dicts_equal
 
 
-class Test(unittest.TestCase):
+class Test_00_Base(unittest.TestCase):
 
     obj = {'a': 1}
     schema = {"type": "object",
@@ -27,6 +27,26 @@ class Test(unittest.TestCase):
                                     'type': 'object'}},
                'type': 'object'}
 
+    opts = dict(ac_schema_typemap=TT._SIMPLETYPE_MAP)
+
+    def test_20_array_to_schema_node(self):
+        scm = TT.array_to_schema_node([1])
+        ref_scm = {'type': 'integer'}
+        self.assertTrue(dicts_equal(scm, ref_scm), scm)
+
+    def test_22_array_to_schema_node__empty_array(self):
+        scm = TT.array_to_schema_node([])
+        ref_scm = {'type': 'string'}
+        self.assertTrue(dicts_equal(scm, ref_scm), scm)
+
+    def test_30_object_to_schema_nodes_iter(self):
+        nscm = list(TT.object_to_schema_nodes_iter({'a': 1}))[0]
+        ref_nscm = ('a', {'type': 'integer'})
+        self.assertTrue(nscm, ref_nscm)
+
+
+class Test_10_Validation(Test_00_Base):
+
     def test_10_validate(self):
         (ret, msg) = TT.validate(self.obj, self.schema)
         self.assertFalse(msg)
@@ -37,21 +57,8 @@ class Test(unittest.TestCase):
         self.assertTrue(msg)
         self.assertFalse(ret)
 
-    def test_20_array_to_schema_node(self):
-        scm = TT.array_to_schema_node([1], TT._SIMPLETYPE_MAP)
-        ref_scm = {'type': 'integer'}
-        self.assertTrue(dicts_equal(scm, ref_scm), scm)
 
-    def test_22_array_to_schema_node__empty_array(self):
-        scm = TT.array_to_schema_node([], TT._SIMPLETYPE_MAP)
-        ref_scm = {'type': 'string'}
-        self.assertTrue(dicts_equal(scm, ref_scm), scm)
-
-    def test_30_object_to_schema_nodes_iter(self):
-        nscm = list(TT.object_to_schema_nodes_iter({'a': 1},
-                                                   TT._SIMPLETYPE_MAP))[0]
-        ref_nscm = ('a', {'type': 'integer'})
-        self.assertTrue(nscm, ref_nscm)
+class Test_20_GenSchema(Test_00_Base):
 
     def test_40_gen_schema__primitive_types(self):
         self.assertEqual(TT.gen_schema(None), {'type': 'null'})
@@ -76,6 +83,61 @@ class Test(unittest.TestCase):
 
     def test_46_gen_schema_and_validate__complex_types(self):
         scm = TT.gen_schema(self.obj2)
+        self.assertTrue(TT.validate(self.obj2, scm))
+
+
+def _gen_scm(val):
+    return TT.gen_schema(val, ac_schema_type="strict")
+
+
+class Test_30_GenStrictSchema(Test_00_Base):
+
+    schema = {"type": "object",
+              "properties": {"a": {"type": "integer"}},
+              "required": ["a"]}
+
+    ref_scm = {'properties': {'a': {'type': 'integer'},
+                              'b': {'items': {'type': 'integer'},
+                                    'type': 'array',
+                                    'minItems': 2, 'uniqueItems': True},
+                              'c': {'properties': {'d': {'type': 'string'},
+                                                   'e': {'type':
+                                                         'number'}},
+                                    'type': 'object',
+                                    'required': ['d', 'e']}},
+               'type': 'object',
+               'required': ['a', 'b', 'c']}
+
+    def test_40_gen_schema__primitive_types(self):
+        self.assertEqual(_gen_scm(None), {'type': 'null'})
+        self.assertEqual(_gen_scm(0), {'type': 'integer'})
+        self.assertEqual(_gen_scm("aaa"), {'type': 'string'})
+
+        scm = _gen_scm([1])
+        ref_scm = {'items': {'type': 'integer'}, 'type': 'array',
+                   'minItems': 1, 'uniqueItems': True}
+        self.assertTrue(dicts_equal(scm, ref_scm))
+
+        scm = _gen_scm(["aaa", "bbb", "aaa"])
+        ref_scm = {'items': {'type': 'string'}, 'type': 'array',
+                   'minItems': 3, 'uniqueItems': False}
+        self.assertTrue(dicts_equal(scm, ref_scm))
+
+        scm = _gen_scm({'a': 1})
+        ref_scm = {'properties': {'a': {'type': 'integer'}},
+                   'type': 'object', 'required': ['a']}
+        self.assertTrue(dicts_equal(scm, ref_scm))
+
+    def test_42_gen_schema_and_validate(self):
+        scm = _gen_scm(self.obj)
+        self.assertTrue(TT.validate(self.obj, scm))
+
+    def test_44_gen_schema__complex_types(self):
+        scm = _gen_scm(self.obj2)
+        self.assertTrue(dicts_equal(scm, self.ref_scm))
+
+    def test_46_gen_schema_and_validate__complex_types(self):
+        scm = _gen_scm(self.obj2)
         self.assertTrue(TT.validate(self.obj2, scm))
 
 # vim:sw=4:ts=4:et:

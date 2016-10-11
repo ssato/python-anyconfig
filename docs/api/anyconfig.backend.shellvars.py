0a817e35017772a4d717fe0d49f0abe3a4b0b7ac
Satoru SATOH <satoru.satoh@gmail.com>
2016-10-12 08:33:35 +0900

    enhancement: add the module doc of shellvars backend

---
+++ docs/api/anyconfig.backend.shellvars.py
@@ -0,0 +1,9 @@
+:mod:`anyconfig.backend.json`
+==============================
+
+.. automodule:: anyconfig.backend.shellvars
+    :members:
+    :special-members:
+    :private-members:
+    :undoc-members:
+    :show-inheritance:

12a7f40d42befde43088eda4d1da87d585c1967c
Satoru SATOH <satoru.satoh@gmail.com>
2016-11-27 22:36:54 +0900

    refactor: introduce a function to initialize logger for future use

--- anyconfig/init.py
+++ anyconfig/init.py
@@ -2,14 +2,22 @@
 # Copyright (C) 2015 Satoru SATOH <ssato @ redhat.com>
 # License: MIT
 #
+# suppress warning about the name of 'getLogger':
+# pylint: disable=invalid-name
 """anyconfig module initialization before any other things.
 """
 import logging
 import anyconfig.compat
 
-# See: "Configuring Logging for a Library" in python standard logging howto,
-# e.g. https://docs.python.org/2/howto/logging.html#library-config.
-LOGGER = logging.getLogger("anyconfig")
-LOGGER.addHandler(anyconfig.compat.NullHandler())
+
+def getLogger(name="anyconfig"):
+    """
+    See: "Configuring Logging for a Library" in python standard logging howto,
+    e.g. https://docs.python.org/2/howto/logging.html#library-config.
+    """
+    logger = logging.getLogger(name)
+    logger.addHandler(anyconfig.compat.NullHandler())
+
+    return logger
 
 # vim:sw=4:ts=4:et:

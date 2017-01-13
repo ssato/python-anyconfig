db69779434f3625f5740960d6a1f033f71b4d92c
Satoru SATOH <satoru.satoh@gmail.com>
2017-01-13 19:50:54 +0900

    change: suppress a warning about access to a protected member

--- anyconfig/tests/schema.py
+++ anyconfig/tests/schema.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2015 Satoru SATOH <ssato at redhat.com>
 # License: MIT
 #
-# pylint: disable=missing-docstring, invalid-name
+# pylint: disable=missing-docstring, invalid-name, protected-access
 from __future__ import absolute_import
 
 import unittest

558abb3ce238e9cfb3e6c99f1b64b03126e1f332
Satoru SATOH <satoru.satoh@gmail.com>
2017-02-11 04:13:26 +0900

    fix: update XML backend tests cases to follow its changes

--- anyconfig/backend/tests/xml.py
+++ anyconfig/backend/tests/xml.py
@@ -1,99 +1,99 @@
 #
-# Copyright (C) 2012 Satoru SATOH <ssato @ redhat.com>
+# Copyright (C) 2012 - 2017 Satoru SATOH <ssato @ redhat.com>
 # License: MIT
 #
 # pylint: disable=missing-docstring, invalid-name
 from __future__ import absolute_import
-
-import copy
-import os.path
 import unittest
 
 import anyconfig.backend.xml as TT
 import anyconfig.backend.tests.ini
 import anyconfig.tests.common
+import anyconfig.compat
 
-from anyconfig.tests.common import dicts_equal, to_bytes
-from anyconfig.mdicts import to_container
+from anyconfig.tests.common import dicts_equal
 
 
-CNF_0_S = """<?xml version="1.0" encoding="UTF-8"?>
-<config name='foo'>
-  <a>0</a>
-  <b id="b0">bbb</b>
-  <sect0>
-    <c>x, y, z</c>
-  </sect0>
-</config>
+XML_W_NS_S = """
+<a xmlns="http://example.com/ns/config"
+   xmlns:val="http://example.com/ns/config/val">
+   <b>1</b>
+   <val:c>C</val:c>
+</a>
 """
 
-CNF_0 = {'config': {'@attrs': {'name': 'foo'},
-                    '@children': [{'a': {'@text': '0'}},
-                                  {'b': {'@attrs': {'id': 'b0'},
-                                         '@text': 'bbb'}},
-                                  {'sect0': {
-                                      '@children': [{'c': {
-                                          '@text': 'x, y, z'
-                                      }}]}}]}}
+
+class Test_00(unittest.TestCase):
+
+    def test__namespaces_from_file(self):
+        ref = {"http://example.com/ns/config": '',
+               "http://example.com/ns/config/val": "val"}
+        xmlfile = anyconfig.compat.StringIO(XML_W_NS_S)
+        self.assertTrue(dicts_equal(TT._namespaces_from_file(xmlfile), ref))
 
 
-class Test00(unittest.TestCase):
+class Test_10(unittest.TestCase):
 
-    def setUp(self):
-        self.root = TT.ET.Element("config", attrib=dict(name="foo"))
-        celm0 = TT.ET.SubElement(self.root, "a")
-        celm0.text = '0'
-        celm1 = TT.ET.SubElement(self.root, "b", attrib=dict(id="b0", ))
-        celm1.text = "bbb"
-        self.tree = TT.ET.ElementTree(self.root)
+    def test_elem_to_container__None(self):
+        self.assertEqual(TT.elem_to_container(None, dict, {}), dict())
 
-        self.cnf = copy.deepcopy(CNF_0)
-        del self.cnf["config"]["@children"][-1]
+    def test_root_to_container__None(self):
+        self.assertEqual(TT.root_to_container(None, dict, {}), dict())
 
-        self.cnf_s = to_bytes('<config name="foo"><a>0</a>'
-                              '<b id="b0">bbb</b></config>')
+    def test_elem_to_container__attrs(self):
+        ref = dict(a={"@attrs": dict(x='1', y='y')})
+        root = TT.ET.XML("<a x='1' y='y'/>")
+        self.assertEqual(TT.elem_to_container(root, dict, {}), ref)
 
-    def test_10_etree_to_container__empty(self):
-        self.assertEqual(TT.etree_to_container(None, dict), {})
+    def test_elem_to_container__child(self):
+        ref = dict(a=dict(b="b"))
+        root = TT.ET.XML("<a><b>b</b></a>")
+        self.assertEqual(TT.elem_to_container(root, dict, {}), ref)
 
-    def test_12_etree_to_container(self):
-        cnf = TT.etree_to_container(self.root, to_container)
-        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
+    def test_elem_to_container__children(self):
+        ref = {'a': {'@children': [{'b': 'b'}, {'c': 'c'}]}}
+        root = TT.ET.XML("<a><b>b</b><c>c</c></a>")
+        self.assertEqual(TT.elem_to_container(root, dict, {}), ref)
 
-    def test_20_container_to_etree__empty(self):
-        # This should not happen but just in case.
-        self.assertTrue(TT.container_to_etree("aaa", dict) is None)
+    def test_elem_to_container__text(self):
+        root = TT.ET.XML("<a>A</a>")
+        self.assertEqual(TT.elem_to_container(root, dict, {}), {'a': 'A'})
 
-    def test_22_container_to_etree(self):
-        tree = TT.container_to_etree(self.cnf, to_container)
-        buf = TT.BytesIO()
-        tree.write(buf)
-        cnf_s = buf.getvalue()
-        self.assertEqual(cnf_s, self.cnf_s, cnf_s)
+    def test_elem_to_container__text_attrs(self):
+        ref = dict(a={"@attrs": {'x': 'X'}, "@text": "A"})
+        root = TT.ET.XML("<a x='X'>A</a>")
+        self.assertEqual(TT.elem_to_container(root, dict, {}), ref)
 
 
-class Test10(anyconfig.backend.tests.ini.Test10):
-
-    cnf = CNF_0
-    cnf_s = CNF_0_S
-    load_options = dump_options = dict(dummy="this will be ignored")
-
-    def setUp(self):
-        self.psr = TT.Parser()
+def tree_to_string(tree):
+    return TT.ET.tostring(tree.getroot())
 
 
-class Test20(anyconfig.backend.tests.ini.Test20):
+class Test_20(unittest.TestCase):
 
-    psr_cls = TT.Parser
-    cnf = CNF_0
-    cnf_s = CNF_0_S
-    cnf_fn = "conf0.xml"
+    def test_00_container_to_etree__None(self):
+        self.assertTrue(TT.container_to_etree(None) is None)
 
-    def setUp(self):
-        self.psr = TT.Parser()
-        self.workdir = anyconfig.tests.common.setup_workdir()
-        self.cpath = os.path.join(self.workdir, self.cnf_fn)
-        open(self.cpath, 'w').write(self.cnf_s)
+    def test_10_container_to_etree__text_attrs(self):
+        ref = '<a x="X" y="Y">A</a>'
+        obj = dict(a={"@attrs": {'x': 'X', 'y': 'Y'}, "@text": "A"})
+        res = TT.container_to_etree(obj)
+        self.assertEqual(tree_to_string(res), ref)
 
+    def test_20_container_to_etree__child(self):
+        return  # TODO
+
+        ref = "<a><b>b</b></a>"
+        obj = dict(a=dict(b="b"))
+        res = TT.container_to_etree(obj)
+        self.assertEqual(tree_to_string(res), ref)
+
+    def test_22_container_to_etree__children(self):
+        return  # TODO
+
+        ref = "<a><b>b</b><c>c</c></a>"
+        obj = {'a': {'@children': [{'b': 'b'}, {'c': 'c'}]}}
+        res = TT.container_to_etree(obj)
+        self.assertEqual(tree_to_string(res), ref)
 
 # vim:sw=4:ts=4:et:

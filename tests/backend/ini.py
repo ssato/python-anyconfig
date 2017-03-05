cfbc377e91db0f36011687215ce2082dc13813b5
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-05 17:20:37 +0900

    change: remove instance_check keyword option from .tests.backend.init.Test*._assert_dicts_equal

--- tests/backend/ini.py
+++ tests/backend/ini.py
@@ -46,33 +46,31 @@ class Test10(unittest.TestCase):
     def setUp(self):
         self.psr = TT.Parser()
 
-    def _assert_dicts_equal(self, cnf, ordered=False, instance_check=False,
-                            ccls=dict, ref=False):
+    def _assert_dicts_equal(self, cnf, ordered=False, ccls=dict, ref=False):
         if not ref:
             ref = self.cnf
         self.assertTrue(tests.common.dicts_equal(cnf, ref, ordered=ordered),
                         "\n %r\nvs.\n %r" % (cnf, ref))
-        if instance_check:
-            cls = ODict if self.is_order_kept and ordered else ccls
-            self.assertTrue(isinstance(cnf, cls))
+        cls = ODict if self.is_order_kept and ordered else ccls
+        self.assertTrue(isinstance(cnf, cls))
 
     def test_10_loads(self):
         cnf = self.psr.loads(self.cnf_s)
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_12_loads__w_options(self):
         cnf = self.psr.loads(self.cnf_s, **self.load_options)
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_20_dumps(self):
         cnf_s = self.psr.dumps(self.cnf)
         self.assertTrue(cnf_s)
         cnf = self.psr.loads(cnf_s)
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_22_dumps__w_options(self):
         cnf = self.psr.loads(self.psr.dumps(self.cnf, **self.dump_options))
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_30_loads_with_order_kept(self):
         cnf = self.psr.loads(self.cnf_s, ac_ordered=True)
@@ -85,37 +83,35 @@ class Test11(Test10):
 
     def test_10_loads(self):
         cnf = self.psr.loads(self.cnf_s, ac_parse_value=True)
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_12_loads__w_options(self):
         cnf = self.psr.loads(self.cnf_s, ac_parse_value=True,
                              **self.load_options)
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_14_loads__w_dict_factory(self):
         cnf = self.psr.loads(self.cnf_s, dict_type=MyDict)
-        self._assert_dicts_equal(cnf, instance_check=True, ccls=MyDict,
-                                 ref=CNF_0)
+        self._assert_dicts_equal(cnf, ccls=MyDict, ref=CNF_0)
 
-    def test_15_loads__w_dict_factory(self):
+    def test_15_loads__w_ac_dict_option(self):
         cnf = self.psr.loads(self.cnf_s, ac_dict=MyDict)
-        self._assert_dicts_equal(cnf, instance_check=True, ccls=MyDict,
-                                 ref=CNF_0)
+        self._assert_dicts_equal(cnf, ccls=MyDict, ref=CNF_0)
 
     def test_16_loads__w_dict_factory(self):
         if self.cnf:
             return  # FIXME.
         cnf = self.psr.loads(self.cnf_s, dict_type=MyDict, ac_parse_value=True)
-        self._assert_dicts_equal(cnf, instance_check=True, ccls=MyDict)
+        self._assert_dicts_equal(cnf, ccls=MyDict)
 
     def test_20_dumps(self):
         cnf = self.psr.loads(self.psr.dumps(self.cnf), ac_parse_value=True)
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_22_dumps__w_options(self):
         cnf = self.psr.loads(self.psr.dumps(self.cnf, **self.dump_options),
                              ac_parse_value=True)
-        self._assert_dicts_equal(cnf, instance_check=True)
+        self._assert_dicts_equal(cnf)
 
     def test_30_loads_with_order_kept(self):
         cnf = self.psr.loads(self.cnf_s, ac_parse_value=True, ac_ordered=True)

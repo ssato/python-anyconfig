f072793abb7ad54984fb24496e80ce8d1e231d17
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-07 12:58:16 +0900

    change: port some utility functions from m9dicts.utils

--- anyconfig/utils.py
+++ anyconfig/utils.py
@@ -6,6 +6,7 @@
 """
 from __future__ import absolute_import
 
+import collections
 import glob
 import os.path
 import types
@@ -222,5 +223,57 @@ def noop(val, *args, **kwargs):
     # It means nothing but can suppress 'Unused argument' pylint warns.
     # (val, args, kwargs)[0]
     return val
+
+
+_LIST_LIKE_TYPES = (collections.Iterable, collections.Sequence)
+
+
+def is_dict_like(obj):
+    """
+    :param obj: Any object behaves like a dict.
+
+    >>> is_dict_like("a string")
+    False
+    >>> is_dict_like({})
+    True
+    >>> is_dict_like(m9dicts.compat.OrderedDict((('a', 1), ('b', 2))))
+    True
+    """
+    return isinstance(obj, (dict, collections.Mapping))  # any others?
+
+
+def is_namedtuple(obj):
+    """
+    >>> p0 = collections.namedtuple("Point", "x y")(1, 2)
+    >>> is_namedtuple(p0)
+    True
+    >>> is_namedtuple(tuple(p0))
+    False
+    """
+    return isinstance(obj, tuple) and hasattr(obj, "_asdict")
+
+
+def is_list_like(obj):
+    """
+    >>> is_list_like([])
+    True
+    >>> is_list_like(())
+    True
+    >>> is_list_like([x for x in range(10)])
+    True
+    >>> is_list_like((1, 2, 3))
+    True
+    >>> g = (x for x in range(10))
+    >>> is_list_like(g)
+    True
+    >>> is_list_like("abc")
+    False
+    >>> is_list_like(0)
+    False
+    >>> is_list_like({})
+    False
+    """
+    return isinstance(obj, _LIST_LIKE_TYPES) and \
+        not (isinstance(obj, anyconfig.compat.STR_TYPES) or is_dict_like(obj))
 
 # vim:sw=4:ts=4:et:

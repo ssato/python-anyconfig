99dfda981f9d714cb3d448731257aeefef04f71d
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-08 15:28:29 +0900

    change: switch base test cases of json backend from tests.backend.ini to tests.backend.common

--- tests/backend/json.py
+++ tests/backend/json.py
@@ -1,14 +1,14 @@
 #
-# Copyright (C) 2012 - 2015 Satoru SATOH <ssato @ redhat.com>
+# Copyright (C) 2012 - 2017 Satoru SATOH <ssato @ redhat.com>
 # License: MIT
 #
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,invalid-name,too-few-public-methods
 from __future__ import absolute_import
-import anyconfig.backend.json as TT
-import tests.backend.ini
 
-from anyconfig.compat import OrderedDict as ODict, IS_PYTHON_2_6
-from tests.common import dicts_equal
+import anyconfig.backend.json as TT
+import tests.backend.common as TBC
+
+from anyconfig.compat import OrderedDict
 
 
 CNF_0_S = """{
@@ -22,37 +22,24 @@ CNF_0_S = """{
 }
 """
 
-CNF_0 = ODict((("a", 0), ("b", "bbb"), ("c", 5),
-               ("sect0", ODict((("d", ["x", "y", "z"]), )))))
+CNF_0 = OrderedDict((("a", 0), ("b", "bbb"), ("c", 5),
+                     ("sect0", OrderedDict((("d", ["x", "y", "z"]), )))))
 
 
-class Test10(tests.backend.ini.Test10):
+class HasParserTrait(TBC.HasParserTrait):
 
-    cnf = CNF_0
+    psr = TT.Parser()
     cnf_s = CNF_0_S
-    load_options = dump_options = dict(parse_int=None, indent=3)
-
-    if IS_PYTHON_2_6:
-        is_order_kept = False  # ..note:: object_pairs_hoo is not available.
-
-    def setUp(self):
-        self.psr = TT.Parser()
-
-
-class Test20(tests.backend.ini.Test20):
-
-    psr_cls = TT.Parser
     cnf = CNF_0
-    cnf_s = CNF_0_S
-    cnf_fn = "conf0.json"
 
-    def test_12_load__w_options(self):
-        cnf = self.psr.load(self.cpath, parse_int=None)
-        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
 
-    def test_22_dump__w_special_option(self):
-        self.psr.dump(self.cnf, self.cpath, parse_int=None, indent=3)
-        cnf = self.psr.load(self.cpath)
-        self.assertTrue(dicts_equal(cnf, self.cnf), str(cnf))
+class Test_10(TBC.Test_10_dumps_and_loads, HasParserTrait):
+
+    load_options = dump_options = dict(parse_int=None, indent=2)
+
+
+class Test_20(TBC.Test_20_dump_and_load, HasParserTrait):
+
+    pass
 
 # vim:sw=4:ts=4:et:

fe35b7eb53a39d90e6f09fc6852ca17848e2b5a3
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-08 15:51:12 +0900

    change: do not run test code of cbor backend if it's not available

--- tests/backend/cbor.py
+++ tests/backend/cbor.py
@@ -5,24 +5,27 @@
 # pylint: disable=missing-docstring,invalid-name,too-few-public-methods
 from __future__ import absolute_import
 
-import anyconfig.backend.cbor as TT
+try:
+    import anyconfig.backend.cbor as TT
+except ImportError:
+    TT = None
+
 import tests.backend.common as TBC
 
 
-class HasParserTrait(TBC.HasParserTrait):
+if TT is not None:
+    class HasParserTrait(TBC.HasParserTrait):
 
-    psr = TT.Parser()
-    cnf = dict(a=0, b="bbb", c=5, sect0=dict(d=["x", "y", "z"]))
-    cnf_s = TT.cbor.dumps(cnf)
+        psr = TT.Parser()
+        cnf = dict(a=0, b="bbb", c=5, sect0=dict(d=["x", "y", "z"]))
+        cnf_s = TT.cbor.dumps(cnf)
 
+    class Test_10(TBC.Test_10_dumps_and_loads, HasParserTrait):
 
-class Test_10(TBC.Test_10_dumps_and_loads, HasParserTrait):
+        load_options = dump_options = dict(sort_keys=False)
 
-    load_options = dump_options = dict(sort_keys=False)
+    class Test_20(TBC.Test_20_dump_and_load, HasParserTrait):
 
-
-class Test_20(TBC.Test_20_dump_and_load, HasParserTrait):
-
-    pass
+        pass
 
 # vim:sw=4:ts=4:et:

8c4759e2ade485469fbcad6159226382439d6025
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-08 15:49:19 +0900

    change: add a statement for debug as a comment

--- tests/backend/common.py
+++ tests/backend/common.py
@@ -80,6 +80,8 @@ class Test_10_dumps_and_loads(TestBase):
         if self.is_ready():
             cnf = self.psr.loads(self.cnf_s, ac_dict=MyDict)
             self._assert_dicts_equal(cnf, cls=MyDict)
+            # for debug:
+            # raise RuntimeError("psr=%r, cnf=%r" % (self.psr, self.cnf))
 
     def test_20_loads_with_dict_option(self):
         if self.is_ready():

ac1627b0b2192990bdbf84d233d9d6a8dff01d4d
Satoru SATOH <satoru.satoh@gmail.com>
2018-04-15 23:48:09 +0900

    fix: make it work with rst2html-3
    In fedora, rst2html is only provided in python2-docutils and if there is
    only python3-docutils installed, we have to run rst2html-3 instead.

--- pkg/gen-readme.sh
+++ pkg/gen-readme.sh
@@ -9,8 +9,9 @@ ${curdir}/header.rst
 ${docdir}/introduction.rst
 "
 # ${docdir}/hacking.rst
+rst2html=$(which rst2html 2> /dev/null > /dev/null && echo rst2html || echo rst2html-3)
 
 cat ${readme_files} > ${output}
-rst2html ${output} > /dev/null
+${rst2html} ${output} > /dev/null
 
 # vim:sw=2:ts=2:et:

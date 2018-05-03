ccbc6d5a88285b25cb06887a78f06324811ceb1c
Satoru SATOH <satoru.satoh@gmail.com>
2018-05-04 01:18:42 +0900

    enhancement: make test runner script works with python 3
    - introduce WITH_PYTHON_3 environment variable to switch python 2 and 3
    - add suffix (-3 if python 3 versions are available) to command names

--- pkg/runtest.sh
+++ pkg/runtest.sh
@@ -11,6 +11,8 @@ if `env | grep -q 'WITH_COVERAGE' 2>/dev/null`; then
     nprocs=0  # It seems that coverage does not like parallel tests.
 fi
 
+`env | grep -q 'WITH_PYTHON_3' 2>/dev/null` && sfx="-3" || sfx=""
+
 if test "x${nprocs}" != "x0" ; then
     if test -f /proc/cpuinfo; then
         nprocs=$(sed -n '/^processor.*/p' /proc/cpuinfo | wc -l)
@@ -20,13 +22,13 @@ if test "x${nprocs}" != "x0" ; then
     fi
 fi
 
-if `which pep8 2>&1 > /dev/null`; then
+if `which pycodestyle${sfx} 2>&1 > /dev/null`; then
     #pep8_opts="--statistics --benchmark"
-    if `which flake8 2>&1 > /dev/null`; then
+    if `which flake8${sfx} 2>&1 > /dev/null`; then
         pep8_opts="$pep8_opts --doctests"
-        function _pep8 () { flake8 $pep8_opts $@; }
+        function _pep8 () { flake8${sfx} $pep8_opts $@; }
     else
-        function _pep8 () { pep8 $pep8_opts $@; }
+        function _pep8 () { pep8${sfx} $pep8_opts $@; }
     fi
 else
     function _pep8 () { :; }
@@ -36,14 +38,14 @@ if `which pylint 2>&1 > /dev/null`; then
     pylint_opt="--disable=locally-disabled"
     test -f ${curdir}/pylintrc && \
         pylint_opt="$pylint_opt --rcfile=$curdir/pylintrc" || :
-    function _pylint () { pylint ${rcopt} $@ || :; }
+    function _pylint () { pylint${sfx} ${rcopt} $@ || :; }
 else
     function _pylint () { :; }
 fi
 
 if test $# -gt 0; then
     for x in $@; do _pep8 ${x%%:*}; _pylint ${x%%:*}; done
-    PYTHONPATH=$topdir nosetests ${nosetests_opts} $@
+    PYTHONPATH=$topdir nosetests${sfx} ${nosetests_opts} $@
 else
     cd ${topdir}
     # Find out python package dir and run tests for .py files under it.
@@ -59,7 +61,7 @@ else
             _pep8 $d
         fi
     done
-    PYTHONPATH=. nosetests ${nosetests_opts} --all-modules
+    PYTHONPATH=. nosetests${sfx} ${nosetests_opts} --all-modules
 fi
 
 # vim:sw=4:ts=4:et:

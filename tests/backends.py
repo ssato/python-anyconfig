2c570689836e7624df9a3ba7fe3bd449ec40ec82
Satoru SATOH <satoru.satoh@gmail.com>
2018-05-20 17:27:29 +0900

    enhancement: add some test cases exceptions raised in .backends.find_parser*

--- tests/backends.py
+++ tests/backends.py
@@ -8,6 +8,8 @@ from __future__ import absolute_import
 import unittest
 import anyconfig.backends as TT
 
+from anyconfig.globals import UnknownParserTypeError, UnknownFileTypeError
+
 
 class Test(unittest.TestCase):
 
@@ -16,5 +18,17 @@ class Test(unittest.TestCase):
 
         self.assertTrue(isinstance(types, list))
         self.assertTrue(bool(list))  # ensure it's not empty.
+
+    def test_20_find_parser_by_type__ng_cases(self):
+        self.assertRaises(ValueError, TT.find_parser_by_type, None)
+        self.assertRaises(UnknownParserTypeError, TT.find_parser_by_type,
+                          "_unkonw_type_")
+
+    def test_30_find_parser_ng_cases(self):
+        self.assertRaises(ValueError, TT.find_parser, None)
+        self.assertRaises(UnknownParserTypeError, TT.find_parser, None,
+                          "_unkonw_type_")
+        self.assertRaises(UnknownFileTypeError, TT.find_parser,
+                          "cnf.unknown_ext")
 
 # vim:sw=4:ts=4:et:

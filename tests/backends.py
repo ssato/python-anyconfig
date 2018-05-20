d1ea17521b29e5e140401d1f59777af85c3b8360
Satoru SATOH <satoru.satoh@gmail.com>
2018-05-20 17:36:33 +0900

    enhancement: add some test cases .backends.find_parser* can find parsers

--- tests/backends.py
+++ tests/backends.py
@@ -5,9 +5,12 @@
 # pylint: disable=missing-docstring
 from __future__ import absolute_import
 
+import os.path
 import unittest
+import anyconfig.backend.json
 import anyconfig.backends as TT
 
+from anyconfig.compat import pathlib
 from anyconfig.globals import UnknownParserTypeError, UnknownFileTypeError
 
 
@@ -24,11 +27,30 @@ class Test(unittest.TestCase):
         self.assertRaises(UnknownParserTypeError, TT.find_parser_by_type,
                           "_unkonw_type_")
 
+    def test_22_find_parser_by_type(self):
+        self.assertTrue(isinstance(TT.find_parser_by_type("json"),
+                                   anyconfig.backend.json.Parser))
+
     def test_30_find_parser_ng_cases(self):
         self.assertRaises(ValueError, TT.find_parser, None)
         self.assertRaises(UnknownParserTypeError, TT.find_parser, None,
                           "_unkonw_type_")
         self.assertRaises(UnknownFileTypeError, TT.find_parser,
                           "cnf.unknown_ext")
+
+    def test_32_find_parser_ng_cases(self):
+        pcls = anyconfig.backend.json.Parser
+        self.assertTrue(isinstance(TT.find_parser("x.conf",
+                                                  forced_type="json"),
+                                   pcls))
+        self.assertTrue(isinstance(TT.find_parser("x.json"), pcls))
+
+        cnf = os.path.join(os.path.dirname(__file__), "00-cnf.json")
+        with open(cnf) as inp:
+            self.assertTrue(isinstance(TT.find_parser(inp), pcls))
+
+        if pathlib is not None:
+            inp = pathlib.Path("x.json")
+            self.assertTrue(isinstance(TT.find_parser(inp), pcls))
 
 # vim:sw=4:ts=4:et:

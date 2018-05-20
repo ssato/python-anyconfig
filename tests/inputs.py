306731556b1ab13e02b74b7a48d21ef6d8ae4dea
Satoru SATOH <satoru.satoh@gmail.com>
2018-05-20 22:48:03 +0900

    change: add some test cases of .inputs.find_parser and utilize its test class also in test cases for .inputs.make

--- tests/inputs.py
+++ tests/inputs.py
@@ -23,9 +23,41 @@ IPATH_0 = os.path.join(os.path.dirname(__file__), "00-cnf.json")
 IPATH_0_FULL = anyconfig.utils.normpath(IPATH_0)
 
 
-class Test_60_make(unittest.TestCase):
+class Test_50_find_parser(unittest.TestCase):
     cpss = (CPS_BY_EXT, CPS_BY_TYPE)
     (ipath, ipath_full) = (IPATH_0, IPATH_0_FULL)
+
+    def __init__(self, *args, **kwargs):
+        super(Test_50_find_parser, self).__init__(*args, **kwargs)
+        self.fun = TT.find_parser
+
+    def __checks_helper(self, psr, pcls):
+        self.assertTrue(isinstance(psr, pcls))
+
+    def test_10__ng_cases(self):
+        with self.assertRaises(ValueError):
+            self.fun(None, CPS_BY_EXT, CPS_BY_TYPE)
+
+        with self.assertRaises(UnknownParserTypeError):
+            self.fun(None, CPS_BY_EXT, CPS_BY_TYPE,
+                     forced_type="type_not_exist")
+        with self.assertRaises(UnknownFileTypeError):
+            self.fun("cnf.unknown_ext", *self.cpss)
+
+    def test_20__forced_type(self):
+        res = self.fun(None, CPS_BY_EXT, CPS_BY_TYPE, forced_type="ini")
+        self.__checks_helper(res, anyconfig.backend.ini.Parser)
+
+    def test_30__by_fileext(self):
+        res = self.fun(self.ipath, *self.cpss)
+        self.__checks_helper(res, anyconfig.backend.json.Parser)
+
+
+class Test_60_make(Test_50_find_parser):
+
+    def __init__(self, *args, **kwargs):
+        super(Test_60_make, self).__init__(*args, **kwargs)
+        self.fun = TT.make
 
     def __checks_helper(self, inp, *args):
         self.assertEqual(inp.src, args[0])
@@ -34,23 +66,15 @@ class Test_60_make(unittest.TestCase):
         self.assertTrue(isinstance(inp.parser, args[3]))
         self.assertEqual(inp.opener, args[4])
 
-    def test_10__ng_cases(self):
-        self.assertRaises(ValueError, TT.make, None, *self.cpss)
-        self.assertRaises(UnknownParserTypeError,
-                          TT.make, None, CPS_BY_EXT, CPS_BY_TYPE,
-                          forced_type="type_not_exist")
-        self.assertRaises(UnknownFileTypeError,
-                          TT.make, "cnf.unknown_ext", *self.cpss)
-
     def test_20__forced_type(self):
-        inp = TT.make(None, CPS_BY_EXT, CPS_BY_TYPE, forced_type="ini")
-        self.__checks_helper(inp, None, None, None,
+        res = self.fun(None, CPS_BY_EXT, CPS_BY_TYPE, forced_type="ini")
+        self.__checks_helper(res, None, None, None,
                              anyconfig.backend.ini.Parser,
                              anyconfig.utils.noop)
 
     def test_30__by_fileext(self):
-        inp = TT.make(self.ipath, *self.cpss)
-        self.__checks_helper(inp, self.ipath, self.ipath_full, TT.PATH_STR,
+        res = self.fun(self.ipath, *self.cpss)
+        self.__checks_helper(res, self.ipath, self.ipath_full, TT.PATH_STR,
                              anyconfig.backend.json.Parser, open)
 
     def test_40__pathlib(self):
@@ -64,14 +88,14 @@ class Test_60_make(unittest.TestCase):
             itype = TT.PATH_STR
             opener = open
 
-        inp = TT.make(ipath, *self.cpss)
-        self.__checks_helper(inp, ipath, self.ipath_full, itype,
+        res = self.fun(ipath, *self.cpss)
+        self.__checks_helper(res, ipath, self.ipath_full, itype,
                              anyconfig.backend.json.Parser, opener)
 
     def test_50__stream(self):
         ifo = open(self.ipath)
-        inp = TT.make(ifo, *self.cpss)
-        self.__checks_helper(inp, ifo, self.ipath_full, TT.STREAM,
+        res = self.fun(ifo, *self.cpss)
+        self.__checks_helper(res, ifo, self.ipath_full, TT.STREAM,
                              anyconfig.backend.json.Parser,
                              anyconfig.utils.noop)
 

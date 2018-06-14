fdb44c9af3c2da7170072371043315f04232df57
Satoru SATOH <satoru.satoh@gmail.com>
2018-06-15 02:00:56 +0900

    enhancement: add a base class .processors.Processor
    add a base class anyconfig.processors.Processor will replace most of all
    functions in anyconfig.backend.base.Parser

--- anyconfig/processors.py
+++ anyconfig/processors.py
@@ -10,6 +10,38 @@ from __future__ import absolute_import
 import pkg_resources
 
 
+class Processor(object):
+    """
+    Abstract processor to provide basic implementation of some methods,
+    interfaces and members.
+
+    - _type: type indicates data types it can process
+    - _priority: Priority to select it if there are others of same type
+    - _extensions: File extensions of data type it can process
+    """
+    _type = None
+    _priority = 0   # 0 (lowest priority) .. 99  (highest priority)
+    _extensions = []
+
+    @classmethod
+    def type(cls):
+        """Processors' type
+        """
+        return cls._type
+
+    @classmethod
+    def priority(cls):
+        """Processors's priority
+        """
+        return cls._priority
+
+    @classmethod
+    def extensions(cls):
+        """File extensions which this process can process
+        """
+        return cls._extensions
+
+
 def _load_plugins_itr(pgroup, safe=True):
     """
     .. seealso:: the doc of :func:`load_plugins`

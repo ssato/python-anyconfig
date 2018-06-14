943e195e452170a93f4ab26db4a4bcc0a8d932f3
Satoru SATOH <satoru.satoh@gmail.com>
2018-06-15 02:50:10 +0900

    enhancement: port listing functions from .backends to .processors
    port some listing functions from anyconfig.backends such like
    _list_parsers_by_type and _list_parsers_by_extension to
    anyconfig.processors and abstract them.

--- anyconfig/processors.py
+++ anyconfig/processors.py
@@ -7,7 +7,10 @@ Abstract processor module will be base for parsers (loaders and dumpers), etc.
 """
 from __future__ import absolute_import
 
+import operator
 import pkg_resources
+
+import anyconfig.compat
 
 
 class Processor(object):
@@ -62,5 +65,42 @@ def load_plugins(pgroup, safe=True):
     :raises: ImportError
     """
     return list(_load_plugins_itr(pgroup, safe=safe))
+
+
+def list_processors_by_type(prs):
+    """
+    :param prs: A list of instances of :class:`Processor`
+    :return: List (generator) of (processor_type, [processor_cls])
+    """
+    return ((t, sorted(ps, key=operator.methodcaller("priority"))) for t, ps
+            in anyconfig.utils.groupby(prs, operator.methodcaller("type")))
+
+
+def _ext_proc_tpls_to_procs(xps):
+    """List processors by each priority.
+
+    :param xps: A list of (file_extension, processor_cls)
+    :return: List of [processor_cls]
+    """
+    return sorted((operator.itemgetter(1)(xp) for xp in xps),
+                  key=operator.methodcaller("priority"))
+
+
+def list_processors_by_ext(prs):
+    """
+    :param prs: A list of instances of :class:`Processor`
+    :return: List (generator) of (file_extension, [processor_cls])
+    """
+    ps_by_ext = anyconfig.utils.concat(([(x, p) for x in p.extensions()] for p
+                                        in prs))  # [(ext, proc_cls)]
+
+    return ((x, _ext_proc_tpls_to_procs(xps)) for x, xps
+            in anyconfig.utils.groupby(ps_by_ext, operator.itemgetter(0)))
+
+
+def list_types(prs):
+    """List types that any processors can process them are available.
+    """
+    return sorted(set(next(anyconfig.compat.izip(*prs))))
 
 # vim:sw=4:ts=4:et:

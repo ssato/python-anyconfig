6efec3e3f847117ced2c95815be1e96aaba2908d
Satoru SATOH <satoru.satoh@gmail.com>
2018-06-17 23:23:18 +0900

    enhancement: allow JSON parser loading primitive types' data
    allow JSON parser loading primitive types' data other than dict data
    such like lists, numbers and strings, and add a test case to check it.

--- anyconfig/backend/json.py
+++ anyconfig/backend/json.py
@@ -61,6 +61,7 @@ class Parser(anyconfig.backend.base.StringStreamFnParser):
     _load_opts = _LOAD_OPTS
     _dump_opts = _DUMP_OPTS
     _ordered = not anyconfig.compat.IS_PYTHON_2_6
+    _allow_primitives = True
     _dict_opts = _DICT_OPTS
 
     _load_from_string_fn = anyconfig.backend.base.to_method(json.loads)

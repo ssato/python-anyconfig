c429c62d00d24ddda60eee6e1de196db6fcd7fa6
Satoru SATOH <satoru.satoh@gmail.com>
2019-01-30 16:51:59 +0900

    fix: disable ruamel.yaml based backend if its lib is too old
    Disable ruamel.yaml based YAML backend if dependent ruamel.yaml library
    is too old and does not have the definition of ruamel.yaml.YAML class,
    necessary to make it works.
    
    This fix was based on the report from the pr#97 by Masatake YAMATO.

--- anyconfig/backend/yaml/ruamel_yaml.py
+++ anyconfig/backend/yaml/ruamel_yaml.py
@@ -44,6 +44,11 @@ import anyconfig.utils
 from . import pyyaml
 
 
+try:
+    ryaml.YAML  # flake8: noqa
+except AttributeError:
+    ImportError("ruamel.yaml may be too old to use!")
+
 _YAML_INIT_KWARGS = ["typ", "pure", "plug_ins"]  # kwargs for ruamel.yaml.YAML
 _YAML_INSTANCE_MEMBERS = ['allow_duplicate_keys', 'allow_unicode',
                           'block_seq_indent', 'canonical', 'composer',

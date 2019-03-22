c689f12754203add84174ac16570a551e8d458c0
Satoru SATOH <satoru.satoh@gmail.com>
2019-03-23 03:41:55 +0900

    refactor: export config test string from tests.backend.toml
    export config test string from tests.backend.toml to
    tests/res/20-00-cnf.toml.

--- tests/backend/toml.py
+++ tests/backend/toml.py
@@ -13,38 +13,6 @@ import tests.backend.common as TBC
 from anyconfig.compat import OrderedDict as ODict
 
 
-# Taken from https://github.com/toml-lang/toml:
-CNF_S = """title = "TOML Example"
-
-[owner]
-name = "Tom Preston-Werner"
-dob = 1979-05-27T07:32:00Z # First class dates
-
-[database]
-server = "192.168.1.1"
-ports = [ 8001, 8001, 8002 ]
-connection_max = 5000
-enabled = true
-
-[servers]
-
-  [servers.alpha]
-  ip = "10.0.0.1"
-  dc = "eqdc10"
-
-  [servers.beta]
-  ip = "10.0.0.2"
-  dc = "eqdc10"
-
-[clients]
-data = [ ["gamma", "delta"], [1, 2] ]
-
-hosts = [
-  "alpha",
-  "omega"
-]
-"""
-
 _DOB = TT.toml.loads("dob = 1979-05-27T07:32:00Z")['dob']
 
 CNF = ODict((('title', 'TOML Example'),
@@ -70,7 +38,7 @@ class HasParserTrait(TBC.HasParserTrait):
 
     psr = TT.Parser()
     cnf = CNF
-    cnf_s = CNF_S
+    cnf_s = TBC.read_from_res("20-00-cnf.toml")
 
 
 class Test_10(TBC.Test_10_dumps_and_loads, HasParserTrait):

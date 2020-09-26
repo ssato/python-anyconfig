52d088a442c24c3fd5d151e24d0bb7bcb9ae41d3
Satoru SATOH <satoru.satoh@gmail.com>
2020-09-26 17:07:56 +0900

    fix: catch cases if neigther xml.etree.{c,}ElementTree were missing correctly

--- src/anyconfig/backend/xml.py
+++ src/anyconfig/backend/xml.py
@@ -65,9 +65,10 @@ import re
 try:
     import xml.etree.cElementTree as ET
 except ImportError:
-    import xml.etree.ElementTree as ET
-except ImportError:
-    import elementtree.ElementTree as ET
+    try:
+        import xml.etree.ElementTree as ET
+    except ImportError:
+        import elementtree.ElementTree as ET
 
 import anyconfig.backend.base
 import anyconfig.compat

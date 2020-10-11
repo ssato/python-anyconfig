a2f4e0d0acfaa21f9880014ebbc3d290853702c9
Satoru SATOH <satoru.satoh@gmail.com>
2020-10-11 18:14:23 +0900

    change: add a TestCase base class setUp/tearDown an workdir

--- tests/common.py
+++ tests/common.py
@@ -3,8 +3,13 @@
 # Copyright (C) 2017 - 2018 Satoru SATOH <satoru.satoh at gmail.com>
 #
 # pylint: disable=missing-docstring
+r"""Common utilities for test cases.
+"""
 import os.path
+import pathlib
+import shutil
 import tempfile
+import unittest
 
 try:
     from unittest import SkipTest
@@ -73,5 +78,20 @@ def cleanup_workdir(workdir):
 
 def skip_test():
     raise SkipTest
+
+
+class TestCaseWithWorkdir(unittest.TestCase):
+
+    def setUp(self):
+        self.workdir = pathlib.Path(setup_workdir()).resolve()
+
+    def tearDown(self):
+        workdir = self.workdir
+        ng_dirs = ('/',
+                   str(self.workdir.home()),
+                   str(pathlib.Path('.').resolve()))
+
+        if str(workdir) not in ng_dirs:
+            shutil.rmtree(workdir)
 
 # vim:sw=4:ts=4:et:

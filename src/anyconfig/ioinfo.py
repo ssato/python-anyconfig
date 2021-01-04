a244af514133dec27f8b1e0ffed1151b16caac01
Satoru SATOH <satoru.satoh@gmail.com>
2021-01-04 18:33:12 +0900

    refactor: remove 'import ...' line from a doctest because it's imported already

--- src/anyconfig/ioinfo.py
+++ src/anyconfig/ioinfo.py
@@ -37,10 +37,7 @@ def guess_io_type(obj):
 
     >>> apath = "/path/to/a_conf.ext"
     >>> assert guess_io_type(apath) == IOI_PATH_STR
-
-    >>> import pathlib
-    >>> if pathlib is not None:
-    ...     assert guess_io_type(pathlib.Path(apath)) == IOI_PATH_OBJ
+    >>> assert guess_io_type(pathlib.Path(apath)) == IOI_PATH_OBJ
     >>> assert guess_io_type(open(__file__)) == IOI_STREAM
     >>> guess_io_type(1)  # doctest: +ELLIPSIS
     Traceback (most recent call last):

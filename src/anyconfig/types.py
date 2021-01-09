8f859b228655334fb3c238558b4d815048551d66
Satoru SATOH <satoru.satoh@gmail.com>
2021-01-09 10:18:03 +0900

    enhancement: add type aliases

---
+++ src/anyconfig/types.py
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 Satoru SATOH <satoru.satoh @ gmail.com>
+# SPDX-License-Identifier: MIT
+#
+# pylint: disable=invalid-name
+"""anyconfig type aliases.
+"""
+import pathlib
+import typing
+
+
+IterableT = typing.Iterable
+ListT = typing.List
+
+ConfigT = typing.Mapping
+SchemaT = typing.Mapping
+
+PathT = typing.Union[str, pathlib.Path]
+FileOrPathT = typing.Union[PathT, typing.IO]
+FileOrPathOrIOInfoT = typing.Union[FileOrPathT, typing.NamedTuple]
+
+ContextT = typing.Mapping
+
+MaybeDataT = typing.Optional[typing.Mapping]
+
+# vim:sw=4:ts=4:et:

a6765b10bbd5fae7320527370a098b96d269dedb
Satoru SATOH <satoru.satoh@gmail.com>
2021-05-31 02:04:17 +0900

    fix: correct broken test data and add one more test case for anyconfig.api.multi_load

--- tests/api/load/multi_load/common.py
+++ tests/api/load/multi_load/common.py
@@ -8,6 +8,6 @@ r"""Common utility functions.
 import pathlib
 
 
-RES_DIR = pathlib.Path(__file__).parent.parent.parent.resolve() / 'res'
+RES_DIR = pathlib.Path(__file__).parent / '../../../' / 'res'
 
 # vim:sw=4:ts=4:et:

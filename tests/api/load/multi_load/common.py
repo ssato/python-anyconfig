81e650717c0d77512755bb41d32e1926078b029d
Satoru SATOH <satoru.satoh@gmail.com>
2021-05-30 16:38:11 +0900

    enhancement: add test file data based test cases for anyconfig.api.multi_load

---
+++ tests/api/load/multi_load/common.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# SPDX-License-Identifier: MIT
+#
+# pylint: disable=missing-docstring
+r"""Common utility functions.
+"""
+import pathlib
+
+
+RES_DIR = pathlib.Path(__file__).parent.parent.parent.resolve() / 'res'
+
+# vim:sw=4:ts=4:et:

865d9e1d916f89a59ad3180a27f95b3d94aed98e
Satoru SATOH <satoru.satoh@gmail.com>
2021-05-30 11:52:31 +0900

    enhancement: add properties and shellvars test cases to tests.api.load.single_load

--- tests/api/load/single_load/test_other_formats.py
+++ tests/api/load/single_load/test_other_formats.py
@@ -27,13 +27,25 @@ def inp_exp_itr(ftype=None, fext=None):
 
 class TestCase(unittest.TestCase):
 
-    def helper(self, ftype):
-        for inp, exp_path in inp_exp_itr(ftype):
-            exp = TT.single_load(exp_path)
-            self.assertEqual(TT.single_load(inp), exp)
+    def helper(self, ftype, fext=None, ordred=True, force_type=False):
+        for inp, exp_path in inp_exp_itr(ftype=ftype, fext=fext):
+            exp = TT.single_load(exp_path, ac_ordered=ordred)
+
+            if force_type:
+                res = TT.single_load(inp, ac_parser=ftype)
+            else:
+                res = TT.single_load(inp)
+
+            self.assertEqual(res, exp)
 
     def test_single_load_ini(self):
         self.helper('ini')
+
+    def test_single_load_properties(self):
+        self.helper('properties')
+
+    def test_single_load_shellvars(self):
+        self.helper('shellvars', fext='sh', force_type=True)
 
     def test_single_load_xml(self):
         self.helper('xml')

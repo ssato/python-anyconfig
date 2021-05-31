0eb482ecafc7d326b238b60304a26183f903c3b9
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-01 05:42:38 +0900

    change: add some more test cases for anyconfig.api.multi_load
    Add some more test cases for anyconfig.api.multi_load, test cases to
    give inputs as an empty list, test case to load from (file) streams,
    test case to load with invalid merge strategies, test cast to load with
    ac_ignore_missing option.

--- tests/api/load/multi_load/test_basics.py
+++ tests/api/load/multi_load/test_basics.py
@@ -8,7 +8,7 @@ import unittest
 
 import anyconfig.api._load as TT
 
-from .common import RES_DIR
+from .common import RES_DIR, DIC_0
 
 
 def datasets_itr():
@@ -32,6 +32,9 @@ def gen_datasets():
 
 class TestCase(unittest.TestCase):
 
+    def test_multi_load_from_empty_path_list(self):
+        self.assertEqual(TT.multi_load([]), DIC_0)
+
     def test_multi_load_from_path_objects(self):
         for rdir, exp, opts in gen_datasets():
             res = TT.multi_load(sorted(rdir.glob('*.json')), **opts)
@@ -41,5 +44,35 @@ class TestCase(unittest.TestCase):
         for rdir, exp, opts in gen_datasets():
             res = TT.multi_load(str(rdir / '*.json'), **opts)
             self.assertEqual(res, exp, f'{rdir!s}')
+
+    def test_multi_load_from_streams(self):
+        for rdir, exp, opts in gen_datasets():
+            paths = sorted(rdir.glob('*.json'))
+            res = TT.multi_load((p.open() for p in paths), **opts)
+            self.assertEqual(res, exp, f'{rdir!s}')
+
+    def test_multi_load_with_wrong_merge_strategy(self):
+        data = gen_datasets()
+        (rdir, _exp, _opts) = data[0]
+
+        with self.assertRaises(ValueError):
+            TT.multi_load(
+                sorted(rdir.glob('*.json')), ac_merge='wrong_merge_strategy'
+            )
+            raise RuntimeError('Wrong merge strategy was not handled!')
+
+    def test_multi_load_with_ignore_missing_option(self):
+        paths = [
+            'file_not_exist_0.json',
+            'file_not_exist_1.json',
+            'file_not_exist_2.json',
+        ]
+        with self.assertRaises(FileNotFoundError):
+            TT.multi_load(paths)
+
+        self.assertEqual(
+            TT.multi_load(paths, ac_ignore_missing=True),
+            DIC_0
+        )
 
 # vim:sw=4:ts=4:et:

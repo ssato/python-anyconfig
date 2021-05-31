7389f632b93645614b726f09d066a685793b8956
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-01 05:42:38 +0900

    change: add some more test cases for anyconfig.api.multi_load
    Add some more test cases for anyconfig.api.multi_load, test cases to
    give inputs as an empty list, test case to load from (file) streams,
    test case to load with invalid merge strategies, test cast to load with
    ac_ignore_missing option.

--- tests/api/load/multi_load/common.py
+++ tests/api/load/multi_load/common.py
@@ -7,7 +7,10 @@ r"""Common utility functions.
 """
 import pathlib
 
+import anyconfig.dicts
+
 
 RES_DIR = pathlib.Path(__file__).parent / '../../../' / 'res'
+DIC_0 = anyconfig.dicts.convert_to(dict())
 
 # vim:sw=4:ts=4:et:

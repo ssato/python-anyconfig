070c1e0b4d3c15d79d074c162bafc315b579aeb5
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-09 02:16:14 +0900

    change: switch to use new test data collector in test cases for api.single_load
    Switch to utilize new test data collector, tests.base.TDataCollector, in
    test cases for anyconfig.api.single_load to simplify test code and
    increase the coverage of test cases in a consistent manner.
    
    - tests/api/single_load/common.py: switch to utilize new test data
      collector, tests.base.TDataCollector
    - tests/api/single_load/test_*.py: add or change to utilize the above

--- tests/api/single_load/test_basics.py
+++ tests/api/single_load/test_basics.py
@@ -3,11 +3,11 @@
 # License: MIT
 #
 # pylint: disable=missing-docstring
-import ast
 import collections
 import pathlib
 
 import anyconfig.api._load as TT
+import anyconfig.parsers
 
 from anyconfig.api import (
     UnknownFileTypeError, UnknownProcessorTypeError
@@ -17,90 +17,104 @@ from tests.base import NULL_CNTNR
 from .common import BaseTestCase
 
 
+JSON_PARSER = anyconfig.parsers.find(None, 'json')
+
+
 class MyDict(collections.OrderedDict):
     """My original dict class keep key orders."""
-    pass
 
 
-class TestCase(BaseTestCase):
+class Base(BaseTestCase):
 
     def test_single_load(self):
-        for inp, exp in self.ies:
-            self.assertEqual(TT.single_load(inp), exp)
+        for data in self.each_data():
+            self.assertEqual(
+                TT.single_load(data.inp_path, **data.opts),
+                data.exp,
+                f'{data.datadir!s}, {data.inp_path!s}'
+            )
 
-    def test_single_load_failiure_unknown_file_type(self):
-        with self.assertRaises(UnknownFileTypeError):
-            TT.single_load('dummy.ext_not_exist')
+    def test_single_load_intentional_failures(self):
+        for data in self.each_data():
+            with self.assertRaises(AssertionError):
+                self.assertEqual(
+                    TT.single_load(data.inp_path, **data.opts),
+                    None
+                )
 
-    def test_single_load_failiure_unknown_processor_type(self):
-        with self.assertRaises(UnknownProcessorTypeError):
-            TT.single_load('dummy.txt', ac_parser='proc_does_not_exist')
 
-    def test_single_load_failiure_invalid_object(self):
-        with self.assertRaises(ValueError):
-            TT.single_load('dummy.txt', ac_parser=object())
+class TestCase(Base):
 
-    def test_single_load_failiure_missing_file(self):
+    def test_single_load_from_stream(self):
+        for data in self.each_data():
+            self.assertEqual(
+                TT.single_load(data.inp_path.open(), **data.opts),
+                data.exp
+            )
+
+    def test_single_load_from_path_str(self):
+        for data in self.each_data():
+            self.assertEqual(
+                TT.single_load(str(data.inp_path), **data.opts),
+                data.exp
+            )
+
+    def test_single_load_with_ac_parser_by_instance(self):
+        for data in self.each_data():
+            self.assertEqual(
+                TT.single_load(data.inp_path, ac_parser=JSON_PARSER),
+                data.exp
+            )
+
+    def test_single_load_with_ac_parser_by_id(self):
+        for data in self.each_data():
+            self.assertEqual(
+                TT.single_load(data.inp_path, ac_parser=JSON_PARSER.cid()),
+                data.exp
+            )
+
+    def test_single_load_with_ac_ordered(self):
+        for data in self.each_data():
+            self.assertEqual(
+                TT.single_load(data.inp_path, ac_ordered=True),
+                collections.OrderedDict(data.exp)
+            )
+
+    def test_single_load_with_ac_dict(self):
+        for data in self.each_data():
+            res = TT.single_load(data.inp_path, ac_dict=MyDict)
+            self.assertTrue(isinstance(res, MyDict))
+            self.assertEqual(res, MyDict(**data.exp))
+
+    def test_single_load_missing_file_failures(self):
         with self.assertRaises(FileNotFoundError):
             TT.single_load('not_exist.json')
 
-    def test_single_load_from_stream(self):
-        for inp, exp in self.ies:
-            res = TT.single_load(open(inp), ac_parser='json')
-            self.assertEqual(res, exp)
+    def test_single_load_unknown_file_type_failures(self):
+        with self.assertRaises(UnknownFileTypeError):
+            TT.single_load('dummy.txt')
 
-    def test_single_load_from_path_str(self):
-        for inp, exp in self.ies:
-            res = TT.single_load(str(inp), ac_parser='json')
-            self.assertEqual(res, exp)
+    def test_single_load_invalid_parser_object_failures(self):
+        with self.assertRaises(ValueError):
+            TT.single_load('dummy.txt', ac_parser=object())
 
-    def test_single_load_with_ac_parser_by_str(self):
-        for inp, exp in self.ies:
-            self.assertEqual(TT.single_load(inp, ac_parser='json'), exp)
-
-    def test_single_load_with_ac_parser_by_instance(self):
-        for inp, exp in self.ies:
-            res = TT.single_load(inp, ac_parser=self.psr)
-            self.assertEqual(res, exp)
-
-    def test_single_load_with_ac_parser_by_id(self):
-        for inp, exp in self.ies:
-            res = TT.single_load(inp, ac_parser=self.psr.cid())
-            self.assertEqual(res, exp)
+    def test_single_load_unknown_processor_type_failures(self):
+        for data in self.each_data():
+            with self.assertRaises(UnknownProcessorTypeError):
+                TT.single_load(
+                    data.inp_path, ac_parser='proc_does_not_exist'
+                )
 
     def test_single_load_ignore_missing(self):
-        inp = pathlib.Path(__file__).parent / 'conf_file_not_exist.json'
+        inp = pathlib.Path() / 'conf_file_not_exist.json'
         assert not inp.exists()
 
         res = TT.single_load(inp, ac_parser='json', ac_ignore_missing=True)
         self.assertEqual(res, NULL_CNTNR)
 
-    def test_single_load_with_ac_ordered(self):
-        for inp, exp in self.ies:
-            res = TT.single_load(inp, ac_ordered=True)
-            self.assertEqual(res, collections.OrderedDict(exp))
 
-    def test_single_load_with_ac_dict(self):
-        for inp, exp in self.ies:
-            res = TT.single_load(inp, ac_dict=MyDict)
-            self.assertTrue(isinstance(res, MyDict))
-            self.assertEqual(res, exp)
-
-
-class PrimitivesTestCase(BaseTestCase):
-
-    kind = 'primitives'
-
-    def test_single_load_primitive_data(self):
-        ies = (
-            (inp,
-             # see: tests/res/json/basic/primitives/e/
-             inp.parent / 'e' / inp.name.replace('.json', '.txt')
-             )
-            for inp, _exp in self.ies
-        )
-        for inp, exp_path in ies:
-            exp = ast.literal_eval(exp_path.read_text().strip())
-            self.assertEqual(TT.single_load(inp), exp)
+class AcParserTestCase(Base):
+    kind = 'ac_parser'
+    pattern = '*.conf'
 
 # vim:sw=4:ts=4:et:

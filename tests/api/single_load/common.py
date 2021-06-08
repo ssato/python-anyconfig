c24e0611e5042d1afce4c2d9a09d63ae65d5ac1f
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-09 02:16:14 +0900

    change: switch to use new test data collector in test cases for api.single_load
    Switch to utilize new test data collector, tests.base.TDataCollector, in
    test cases for anyconfig.api.single_load to simplify test code and
    increase the coverage of test cases in a consistent manner.
    
    - tests/api/single_load/common.py: switch to utilize new test data
      collector, tests.base.TDataCollector
    - tests/api/single_load/test_*.py: add or change to utilize the above

--- tests/api/single_load/common.py
+++ tests/api/single_load/common.py
@@ -5,42 +5,10 @@
 # pylint: disable=missing-docstring
 import unittest
 
-import anyconfig.parsers
-
-from tests.base import (
-    DATA_00, list_resources
-)
+from ... import base
 
 
-class BaseTestCase(unittest.TestCase):
-
-    # see: tests/res/json/basic/
-    kind = 'basic'
-
-    @property
-    def ies(self):
-        path = f'json/{self.kind}/*.json'
-        _ies = sorted(
-            (inp, DATA_00.get(str(inp), None)) for inp in list_resources(path)
-        )
-        if not _ies:
-            raise RuntimeError(f'No data: {path}')
-
-        return _ies
-
-    @property
-    def psr(self):
-        return anyconfig.parsers.find(self.ies[0][0], 'json')
-
-
-class TestCaseWithExpctedData(BaseTestCase):
-
-    # see: tests/res/json/template/ for example.
-    @property
-    def ies(self):
-        return [
-            (inp, inp.parent / 'e' / inp.name)
-            for inp, _exp in super().ies
-        ]
+class BaseTestCase(base.TDataCollector, unittest.TestCase):
+    target = 'single_load'
 
 # vim:sw=4:ts=4:et:
---
+++ tests/api/single_load/test_multi_types.py
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# License: MIT
+#
+# pylint: disable=missing-docstring
+import anyconfig.api._load as TT
+
+from .common import BaseTestCase
+
+
+class TestCase(BaseTestCase):
+    kind = 'multi_types'
+    pattern = '*.*'
+
+    def test_single_load(self):
+        for data in self.each_data():
+            self.assertEqual(
+                TT.single_load(data.inp_path, **data.opts),
+                data.exp,
+                f'{data.datadir!s}, {data.inp_path!s}'
+            )
+
+    def test_single_load_intentional_failures(self):
+        for data in self.each_data():
+            with self.assertRaises(AssertionError):
+                self.assertEqual(
+                    TT.single_load(data.inp_path, **data.opts),
+                    None
+                )
+
+# vim:sw=4:ts=4:et:

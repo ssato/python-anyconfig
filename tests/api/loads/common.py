bd173134a92d26b4f5c4bb8e2e290fb9076762c3
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-19 03:02:08 +0900

    fix: correct the order of inheritance in tests.api.loads.common.TestCase
    Rename tests.api.loads.common.TestCase and correct the order of
    inheritance in it, and related changes.

--- tests/api/loads/common.py
+++ tests/api/loads/common.py
@@ -8,21 +8,10 @@ import unittest
 from ... import base
 
 
-def list_test_data(kind: str = 'basics'):
-    root = TESTS_DIR / 'res' / 'loads' / kind
-    _ies = sorted(
-        (ddir, sorted(
-            (inp, DATA_00.get(str(inp.resolve()), None))
-            for inp in ddir.glob('*.json') if inp.is_file()
-        ))
-        for ddir in root.glob('*') if ddir.is_dir()
-    )
-    if not _ies or any(not data for ddir, data in _ies):
-        raise RuntimeError(f'No data: {root!s}')
-
-
-class BaseTestCase(base.TDataCollector, unittest.TestCase):
-    target = 'loads'
+class TestCase(unittest.TestCase, base.TDataCollector):
     pattern = '*.txt'
+
+    def setUp(self):
+        self.init()
 
 # vim:sw=4:ts=4:et:

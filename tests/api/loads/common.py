6107cafaac6426df135ac71d6cd7e1cdf36d6a37
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-19 03:00:21 +0900

    enhancement: add file based test cases for anyconfig.api.loads with data files
    add file based test cases for anyconfig.api.loads with data files, and
    remove old ad-hoc code based test cases replace with them.

--- tests/api/loads/common.py
+++ tests/api/loads/common.py
@@ -8,6 +8,19 @@ import unittest
 from ... import base
 
 
+def list_test_data(kind: str = 'basics'):
+    root = TESTS_DIR / 'res' / 'loads' / kind
+    _ies = sorted(
+        (ddir, sorted(
+            (inp, DATA_00.get(str(inp.resolve()), None))
+            for inp in ddir.glob('*.json') if inp.is_file()
+        ))
+        for ddir in root.glob('*') if ddir.is_dir()
+    )
+    if not _ies or any(not data for ddir, data in _ies):
+        raise RuntimeError(f'No data: {root!s}')
+
+
 class BaseTestCase(base.TDataCollector, unittest.TestCase):
     target = 'loads'
     pattern = '*.txt'

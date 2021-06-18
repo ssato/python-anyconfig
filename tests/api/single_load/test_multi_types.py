ae502d9200c543b2f1b3053170b2afdc4962a5b8
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-18 10:11:39 +0900

    change: add base test case class for test cases of api.single_load
    Add base test case class for the test cases of api.single_load, and its
    test cases.
    
    - tests/api/single_load/common.py: add TestCase used as a base class for
      other actual test cases
    - tests/api/single_load/test_common.py: add test cases for the above
    - tests/api/single_load/test_*.py (other actual test cases): simplify
      test cases by inherits .common.TestCase and utilze its methods

--- tests/api/single_load/test_multi_types.py
+++ tests/api/single_load/test_multi_types.py
@@ -3,29 +3,11 @@
 # License: MIT
 #
 # pylint: disable=missing-docstring
-import anyconfig.api._load as TT
-
 from . import common
 
 
-class TestCase(common.BaseTestCase):
+class TestCase(common.TestCase):
     kind = 'multi_types'
     pattern = '*.*'
-
-    def test_single_load(self):
-        for data in self.each_data():
-            self.assertEqual(
-                TT.single_load(data.inp_path, **data.opts),
-                data.exp,
-                f'{data.datadir!s}, {data.inp_path!s}'
-            )
-
-    def test_single_load_intentional_failures(self):
-        for data in self.each_data():
-            with self.assertRaises(AssertionError):
-                self.assertEqual(
-                    TT.single_load(data.inp_path, **data.opts),
-                    None
-                )
 
 # vim:sw=4:ts=4:et:

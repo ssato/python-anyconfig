f26aa3b932294205024a20065a6254e76f41e7c3
Satoru SATOH <satoru.satoh@gmail.com>
2021-07-21 18:50:17 +0900

    fix: add missing type hint to tests.base.common.NULL_CNTNR

--- tests/base/common.py
+++ tests/base/common.py
@@ -6,12 +6,13 @@
 r"""Common utility functions.
 """
 import pathlib
+import typing
 
 
 TESTS_DIR = pathlib.Path(__file__).parent.parent.resolve()
 RES_DIR = TESTS_DIR / 'res'
 
-NULL_CNTNR = dict()
+NULL_CNTNR: typing.Dict[str, typing.Any] = dict()
 
 DATA = '3.149265'  # for test_utils.py
 

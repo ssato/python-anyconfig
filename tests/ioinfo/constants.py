29dba2bed55fff9ac1240cdb792d5acc70f3e65c
Satoru SATOH <satoru.satoh@gmail.com>
2021-07-24 04:13:59 +0900

    refactor/change: cleanup test cases of anyconfig.ioinfo.*
    Cleanup test cases of anyconfig.ioinfo.*
    
    - Split and re-organize test cases from tests/ioinfo.py to
      tests/ioinfo/*.py to make easier to maintain them
    - Add complte test cases for anyconfig.ioinfo.*

---
+++ tests/ioinfo/constants.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# License: MIT
+#
+# pylint: disable=missing-docstring
+import pathlib
+
+
+TEST_PY = pathlib.Path(__file__).resolve()
+
+# vim:sw=4:ts=4:et:
---
+++ tests/ioinfo/test_main.py
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2018 - 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# SPDX-License-Identifier: MIT
+#
+# pylint: disable=missing-docstring, invalid-name
+import unittest
+
+import anyconfig.ioinfo as TT
+
+from anyconfig.common import (
+    IOInfo, IOI_PATH_OBJ, IOI_STREAM
+)
+
+from .constants import TEST_PY
+
+
+TEST_IOI_PATH_OBJ = IOInfo(
+    src=TEST_PY, type=IOI_PATH_OBJ, path=str(TEST_PY), extension='py'
+)
+TEST_IOI_STREAM = IOInfo(
+    src=TEST_PY.open(), type=IOI_STREAM, path=str(TEST_PY), extension='py'
+)
+
+
+class TestCase(unittest.TestCase):
+
+    def test_make(self):
+        ies = (
+            (TEST_IOI_PATH_OBJ, TEST_IOI_PATH_OBJ),
+            (TEST_IOI_STREAM, TEST_IOI_STREAM),
+            (str(TEST_PY), TEST_IOI_PATH_OBJ),
+        )
+        for inp, exp in ies:
+            self.assertEqual(TT.make(inp), exp)
+
+    def test_make_failures(self):
+        inps = (None, )
+        for inp in inps:
+            with self.assertRaises(ValueError):
+                TT.make(inp)
+
+# vim:sw=4:ts=4:et:

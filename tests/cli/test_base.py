242740f7c1f85c83b3af88e47f5bcdc60439df49
Satoru SATOH <satoru.satoh@gmail.com>
2021-09-13 01:00:02 +0900

    change: add an utility class tests.cli.test_base.Expected
    Add an utility class tests.cli.test_base.Expected to simplify passing
    arguments to tests.cli.test_base.TestCase.run_main.

--- tests/cli/test_base.py
+++ tests/cli/test_base.py
@@ -19,15 +19,21 @@ def _run(*args: typing.Iterable[str]) -> None:
     TT.main(['dummy'] + [str(a) for a in args])
 
 
+class Expected(typing.NamedTuple):
+    """Keeps expected result's info.
+    """
+    exit_code: int = 0
+    exit_code_matches: bool = True
+    words_in_stdout: str = ''
+    words_in_stderr: str = ''
+    exception: BaseException = SystemExit
+
+
 class TestCase(unittest.TestCase):
 
     def run_main(self,
                  args: typing.Optional[typing.Iterable[str]] = None,
-                 ref_code: int = 0,
-                 expected: bool = True,
-                 words_in_stdout: typing.Optional[str] = None,
-                 words_in_stderr: typing.Optional[str] = None,
-                 exc_cls=SystemExit
+                 expected: Expected = Expected()
                  ) -> None:
         """
         Run anyconfig.cli.main and check if the exit code was expected one.
@@ -35,20 +41,25 @@ class TestCase(unittest.TestCase):
         if args is None:
             args = []
 
-        with self.assertRaises(exc_cls) as ctx:
+        with self.assertRaises(expected.exception) as ctx:
             with contextlib.redirect_stdout(io.StringIO()) as stdout:
                 with contextlib.redirect_stderr(io.StringIO()) as stderr:
                     _run(*args)
 
         exc = ctx.exception
-        self.assertTrue(isinstance(exc, exc_cls))
+        self.assertTrue(isinstance(exc, expected.exception))
         ecode = getattr(exc, 'error_code', getattr(exc, 'code', 1))
-        (self.assertEqual if expected else self.assertNotEqual)(
-            ecode, ref_code
-        )
-        if words_in_stdout:
-            self.assertTrue(words_in_stdout in stdout.getvalue())
-        if words_in_stderr:
-            self.assertTrue(words_in_stderr in stderr.getvalue())
+        if expected.exit_code_matches:
+            self.assertEqual(ecode, expected.exit_code)
+        else:
+            self.assertNotEqual(ecode, expected.exit_code)
+
+        if expected.words_in_stdout:
+            msg = stdout.getvalue()
+            self.assertTrue(expected.words_in_stdout in msg, msg)
+
+        if expected.words_in_stderr:
+            msg = stderr.getvalue()
+            self.assertTrue(expected.words_in_stderr in msg, msg)
 
 # vim:sw=4:ts=4:et:

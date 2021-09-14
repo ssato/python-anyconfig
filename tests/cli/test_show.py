2dd9372b7916ec088790fe1a5dec80b98f17e02b
Satoru SATOH <satoru.satoh@gmail.com>
2021-09-14 14:42:33 +0900

    change: replace some hand-crafted cli test cases with file based ones
    Replace some hand-crafted cli test cases in tests.cli.test_show_* with
    file based ones which collect test data under tests/res/cli/show/
    automatically.

---
+++ tests/cli/test_show.py
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2013 - 2021 Satoru SATOH <satoru.satoh @ gmail.com>
+# License: MIT
+#
+# pylint: disable=missing-docstring
+"""test cases of anyconfig.cli.main without arguments to show info.
+"""
+import anyconfig.api
+
+from . import datatypes, test_base
+
+
+class TestCase(test_base.TestCase):
+
+    kind = 'show'
+
+    def test_show_vesion(self):
+        ver = '.'.join(anyconfig.api.version())
+        self.run_main(
+            ['--version'], datatypes.Expected(words_in_stdout=ver)
+        )
+
+# vim:sw=4:ts=4:et:

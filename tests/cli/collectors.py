ab25ccfac7d9ee6dce5675320cec9af83890e4a5
Satoru SATOH <satoru.satoh@gmail.com>
2021-09-15 03:58:24 +0900

    change: follow changes of tests.base.collector and related modules

--- tests/cli/collectors.py
+++ tests/cli/collectors.py
@@ -4,6 +4,8 @@
 #
 """Provides base class to collect test data for cli test cases.
 """
+import pathlib
+
 from .. import base
 from . import datatypes
 
@@ -12,17 +14,21 @@ class Collector(base.TDataCollector):
     """Test data collector for test cases with no file outputs.
     """
 
-    def load_data(self, data):
+    def load_dataset(self, datadir: pathlib.Path, inp: pathlib.Path):
         """Load dataset and make an object keeps it.
+
+        .. seealso:: tests.base.collector.TDataCollector.load_dataset
         """
-        exp_data = base.load_data(data.exp)
-        exp = datatypes.Expected(**exp_data)
+        name = inp.stem
+        exp_data = base.load_data(
+            base.maybe_data_path(datadir / 'e', name, self.should_exist)
+        )
 
         return datatypes.TData(
-            data.datadir,
-            data.inp,
-            base.load_data(data.inp) or [],
-            exp
+            datadir,
+            inp,
+            base.load_data(inp) or [],
+            datatypes.Expected(**exp_data)
         )
 
 # vim:sw=4:ts=4:et:

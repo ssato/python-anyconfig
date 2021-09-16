40b50b798f5a1efa129056cec565d144a7eb4d52
Satoru SATOH <satoru.satoh@gmail.com>
2021-09-16 19:24:36 +0900

    change: add new file based test cases for the cli's schema validation support

---
+++ tests/cli/test_schema.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2013 - 2021 Satoru SATOH <satoru.satoh @ gmail.com>
+# License: MIT
+#
+# pylint: disable=missing-docstring
+"""test cases of anyconfig.cli.main with schema options.
+"""
+import unittest
+import warnings
+
+import anyconfig.schema
+
+from . import collectors, test_base
+
+
+class Collector(collectors.Collector):
+    kind = 'schema'
+
+
+@unittest.skipIf(not anyconfig.schema.SUPPORTED,
+                 'Library for JSON schema validation is not available')
+class TestCase(test_base.BaseTestCase):
+    collector = Collector()
+
+    def make_args(self, tdata):  # pylint: disable=no-self-use
+        """Make arguments to run cli.main.
+        """
+        return [
+            'anyconfig_cli', '--validate', '--schema', str(tdata.scm),
+            str(tdata.inp_path), *tdata.opts
+        ]
+
+    def _run_main(self, tdata):
+        """Override it to suppress some warnings.
+        """
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            super()._run_main(tdata)
+
+# vim:sw=4:ts=4:et:

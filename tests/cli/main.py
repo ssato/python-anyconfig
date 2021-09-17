9f73b35623d1bca4205abfed3ccbd94957207242
Satoru SATOH <satoru.satoh@gmail.com>
2021-09-17 11:29:04 +0900

    change: remove old test cases for the cli replaced with new file based ones

--- tests/cli/main.py
+++ tests/cli/main.py
@@ -63,60 +63,8 @@ class Test_20_Base(RunTestBase):
 
 class Test_30_single_input(Test_20_Base):
 
-    @unittest.skipIf(not anyconfig.schema.SUPPORTED,
-                     "json schema lib is not available")
-    def test_50_w_schema(self):
-        infile = CNF_0_PATH
-        scmfile = tests.common.respath('00-scm.yml')
-
-        output = self.workdir / "output.json"
-        self.run_and_check_exit_code(["--schema", scmfile, "--validate",
-                                      infile], 0)
-        self.run_and_check_exit_code(["--schema", scmfile, "-o", output,
-                                      infile], 0)
-
-        infile2 = self.workdir / "input.yml"
-        cnf = CNF_0.copy()
-        cnf["a"] = "aaa"  # Validation should fail.
-        anyconfig.api.dump(cnf, infile2)
-        self.run_and_check_exit_code(["--schema", scmfile, "--validate",
-                                      infile2], 1)
-
     def test_52_wo_schema(self):
         self.run_and_check_exit_code(["--validate", CNF_0_PATH], 1)
-
-    def test_54_gen_schema_and_validate_with_it(self):
-        cnf = dict(name="a", a=1, b=dict(b=[1, 2], c="C"))
-        infile = self.workdir / "cnf.json"
-        output = self.workdir / "out.json"
-        anyconfig.api.dump(cnf, infile)
-
-        self.run_and_check_exit_code(["--gen-schema", "-o", output, infile], 0)
-        self.assertTrue(output.exists())
-        self.run_and_check_exit_code(["--schema", output, "--validate",
-                                      infile], 0)
-
-    def test_60_w_arg_option(self):
-        a = dict(name="a", a=1, b=dict(b=[1, 2], c="C"), d=[1, 2])
-
-        infile = self.workdir / "a.json"
-        output = self.workdir / "b.json"
-
-        anyconfig.api.dump(a, infile)
-        self.assertTrue(infile.exists())
-
-        _run("-o", output, "-A", "a:10;name:x;d:3,4", infile)
-        self.assertTrue(output.exists())
-
-        x = anyconfig.api.load(output)
-
-        self.assertNotEqual(a["name"], x["name"])
-        self.assertNotEqual(a["a"], x["a"])
-        self.assertNotEqual(a["d"], x["d"])
-
-        self.assertEqual(x["name"], 'x')
-        self.assertEqual(x["a"], 10)
-        self.assertEqual(x["d"], [3, 4])
 
     def test_70_no_template(self):
         a = dict(name="a", a=1, b=dict(b=[1, 2], c="C"))
@@ -180,50 +128,5 @@ class Test_40_multi_inputs(Test_20_Base):
 
         _run("--template", "-o", output, infile)
         self.assertTrue(output.exists())
-
-
-class Test_50_others_w_input(Test_20_Base):
-
-    def setUp(self):
-        super().setUp()
-        dic = dict(name="a", a=1, b=dict(b=[1, 2], c="C"), d=[1, 2])
-        self.infile = self.workdir / "a.json"
-        anyconfig.api.dump(dic, self.infile)
-
-    def test_10_output_wo_output_option_w_otype(self):
-        self.run_and_check_exit_code(["--otype", "json", self.infile])
-
-    def test_12_output_wo_output_option_and_otype_w_itype(self):
-        self.run_and_check_exit_code(["--itype", "json", self.infile])
-
-    def test_20_no_out_dumper(self):
-        outfile = self.workdir / "out.conf"
-        self.run_and_check_exit_code(["-o", outfile, self.infile], 1)
-
-    def test_22_no_out_dumper_nor_itype(self):
-        infile = str(self.infile).replace(".json", ".conf")
-        outfile = self.workdir / "out.conf"
-        self.run_and_check_exit_code(["-o", outfile, infile], 1)
-
-    @unittest.skipIf(not getattr(anyconfig.query, "jmespath", False),
-                     "jmespath lib is not available")
-    def test_30_w_query_option(self):
-        self.run_and_check_exit_code(["-Q", "b.b[::-1]", self.infile], 0)
-
-
-class Test_50_others_wo_input(Test_20_Base):
-
-    def test_30_no_itype_and_otype(self):
-        outfile = self.workdir / "out.conf"
-        self.run_and_check_exit_code(["-o", outfile, "in.conf"], 1)
-
-    def test_40_no_inputs__w_env_option(self):
-        output = self.workdir / "out.json"
-        self.run_and_check_exit_code(["--env", "-o", output], 0)
-        data = anyconfig.api.load(output)
-
-        for env_var, env_val in os.environ.items():
-            self.assertTrue(env_var in data)
-            self.assertEqual(env_val, os.environ[env_var])
 
 # vim:sw=4:ts=4:et:

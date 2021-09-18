311cd86926d627a9a4c77bc102efbbb6e4b8593e
Satoru SATOH <satoru.satoh@gmail.com>
2021-09-18 11:02:39 +0900

    change: add new file based test cases for multiple inputs of the cli

---
+++ tests/cli/test_multi_inputs.py
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2013 - 2021 Satoru SATOH <satoru.satoh @ gmail.com>
+# License: MIT
+#
+# pylint: disable=missing-docstring
+"""test cases of anyconfig.cli.main with multiple inputs.
+"""
+from . import collectors, test_base
+
+
+class Collector(collectors.MultiDataCollector):
+    kind = 'multi_inputs'
+
+
+class TestCase(test_base.BaseTestCase):
+    collector = Collector()
+
+    def make_args(self, tdata):  # pylint: disable=no-self-use
+        """Make arguments to run cli.main.
+        """
+        return ['anyconfig_cli'] + tdata.opts + [str(p) for p in tdata.inputs]
+
+# vim:sw=4:ts=4:et:
---
+++ tests/res/cli/multi_inputs/20/README.md
@@ -0,0 +1,3 @@
+# Test cases for anyconfig\_cli
+
+- 10.json, 20.xml, 30.sh + o/10.json: multi type inputs without any options to load, and dump to a JSON file without any options

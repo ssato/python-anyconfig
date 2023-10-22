8b8a39597d3da7cb36a6c8cde0472e1b731e73b2
Satoru SATOH <satoru.satoh@gmail.com>
2023-10-23 00:37:05 +0900

    enhancement: add support of multiple toml load and dump libraries
    Add support of libraries to load and dump TOML files as follows.
    
    - tomllib and tomli-w: These seems the most popular library. tomllib
      came into the standard library in python 3.11.0.
    - tomli and tomli-w: for python < 3.11.0
    - tomlkit
    
    The correspondig test code were added at the same time.
    
    It may close #153

--- tests/backend/toml/common.py
+++ tests/backend/toml/common.py
@@ -1,27 +1,16 @@
 #
-# Copyright (C) 2015 - 2019 Satoru SATOH <satoru.satoh@gmail.com>
-# Copyright (C) 2017 Red Hat, Inc.
-# License: MIT
+# Copyright (C) 2023 Satoru SATOH <satoru.satoh @ gmail.com>
+# SPDX-License-Identifier: MIT
 #
 # pylint: disable=missing-docstring,invalid-name,too-few-public-methods
-# pylint: disable=ungrouped-imports
+# pylint: disable=raise-missing-from
 from collections import OrderedDict as ODict
 
-try:
-    import anyconfig.backend.toml as TT
-except ImportError:
-    import unittest
-    raise unittest.SkipTest
-
-import tests.backend.common as TBC
-
-
-_DOB = TT.toml.loads("dob = 1979-05-27T07:32:00Z")['dob']
 
 CNF = ODict((('title', 'TOML Example'),
              ('owner',
               ODict((('name', 'Tom Preston-Werner'),
-                     ('dob', _DOB)))),
+                     ('dob', '1979-05-27T07:32:00Z')))),
              ('database',
               ODict((('server', '192.168.1.1'),
                      ('ports', [8001, 8001, 8002]),
@@ -35,22 +24,3 @@ CNF = ODict((('title', 'TOML Example'),
              ('clients',
               ODict((('data', [['gamma', 'delta'], [1, 2]]),
                      ('hosts', ['alpha', 'omega']))))))
-
-
-class HasParserTrait(TBC.HasParserTrait):
-
-    psr = TT.Parser()
-    cnf = CNF
-    cnf_s = TBC.read_from_res("20-00-cnf.toml")
-
-
-class Test_10(TBC.Test_10_dumps_and_loads, HasParserTrait):
-
-    load_options = dump_options = dict(dummy="this_will_be_ignored")
-
-
-class Test_20(TBC.Test_20_dump_and_load, HasParserTrait):
-
-    pass
-
-# vim:sw=4:ts=4:et:

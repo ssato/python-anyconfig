b69989491c910556949573ad80457873504ce25f
Satoru SATOH <satoru.satoh@gmail.com>
2024-01-02 15:42:35 +0900

    change: remove the arg, file_extensions, from tests.common.*

--- tests/common/paths.py
+++ tests/common/paths.py
@@ -8,19 +8,13 @@ r"""Compute paths.
 import pathlib
 import typing
 
+from . import load
+
 
 TEST_DATA_MAJOR_VERSION: int = 1
 
 TESTDIR: pathlib.Path = pathlib.Path(__file__).parent.parent.resolve()
 RESOURCE_DIR: pathlib.Path = TESTDIR / "res" / str(TEST_DATA_MAJOR_VERSION)
-
-EXTS_TO_TRY: typing.Tuple[str, ...] = (
-    "txt",
-    "dat",
-    "py",
-    "json",
-    "pickle",
-)
 
 
 def get_resource_dir(
@@ -34,75 +28,43 @@ def get_resource_dir(
     ) / loader_or_dumper
 
 
-def get_aux_data_path(
-    ipath: pathlib.Path, subdir: str = "e",
-    file_ext: typing.Union[str, bool] = False,
-    exts_to_try: typing.Tuple[str, ...] = EXTS_TO_TRY,
-    **_kwargs,
-) -> typing.Optional[pathlib.Path]:
-    """Get a path to auxiliary data for input, `ipath`."""
-    if file_ext:
-        candidate = ipath.parent / subdir / f"{ipath.name}.{file_ext}"
-        if candidate.exists():
-            return candidate
-
-    for ext in exts_to_try:
-        candidate = ipath.parent / subdir / f"{ipath.name}.{ext}"
-        if candidate.exists():
-            return candidate
-
-    return None
-
-
 def get_aux_data_paths(
-    ipath: pathlib.Path,
-    file_extensions: typing.Tuple[str, ...] = (),
-    **_kwargs,
+    ipath: pathlib.Path, **_kwargs
 ) -> typing.Dict[str, pathlib.Path]:
     """Get a map of subdirs and paths to auxiliary data for input, `ipath`.
 
     It expects that aux data is in `ipath.parent`/*/.
     """
     name = ipath.name[:-len(ipath.suffix)]  # /a/b/c.json -> c
-    paths_g = ipath.parent.glob(f"*/{name}.*")
 
-    if file_extensions:
-        suffixes = [f".{x}" for x in file_extensions]
-        paths_g = (p for p in paths_g if p.suffix in suffixes)
-
-    return {p.parent.name: p for p in paths_g}
+    return {p.parent.name: p for p in ipath.parent.glob(f"*/{name}.*")}
 
 
 def get_data(
     topdir: typing.Optional[pathlib.Path],
-    file_extensions: typing.Tuple[str, ...] = (),
 ) -> typing.List[
     typing.Tuple[pathlib.Path, typing.Dict[str, pathlib.Path]]
 ]:
     return sorted(
-        (ipath, get_aux_data_paths(ipath, file_extensions))
+        (ipath, get_aux_data_paths(ipath))
         for ipath in topdir.glob("*.*") if ipath.is_file()
     )
 
 
-def get_data_paths(
-    loader_or_dumper: str, is_loader: bool = True,
-    topdir: typing.Optional[pathlib.Path] = None,
+def load_data(
+    topdir: typing.Optional[pathlib.Path],
+    keep_order: bool = False,
     **kwargs
 ) -> typing.List[
-    typing.Tuple[pathlib.Path, typing.Dict[str, pathlib.Path]]
+    typing.Tuple[pathlib.Path, typing.Dict[str, typing.Any]]
 ]:
-    """Get a set of data paths."""
-    resdir = get_resource_dir(
-        loader_or_dumper, is_loader=is_loader, topdir=topdir
-    )
-    return sorted(
+    return [
         (
             ipath,
             {
-                d.name: get_aux_data_path(ipath, d.name, *kwargs)
-                for d in ipath.parent.glob("*") if d.is_dir()
-            },
+                subdir: load.load_data(a, keep_order, **kwargs)
+                for subdir, a in adata.items()
+            }
         )
-        for ipath in resdir.glob("*/*.*") if ipath.is_file()
-    )
+        for ipath, adata in get_data(topdir)
+    ]
--- tests/common/test_paths.py
+++ tests/common/test_paths.py
@@ -42,45 +42,15 @@ def test_get_resource_dir(
 
 
 @pytest.mark.parametrize(
-    ("ipath", "subdir", "file_ext", "exts_to_try", "exp"),
-    ((TT.RESOURCE_DIR / "loaders/json.json/10/100_null.json",
-      "e", False, TT.EXTS_TO_TRY,
-      TT.RESOURCE_DIR / "loaders/json.json/10/e/100_null.json.py"),
-     (TT.RESOURCE_DIR / "loaders/json.json/10/100_null.json",
-      "e", "ext_not_exists", TT.EXTS_TO_TRY,
-      TT.RESOURCE_DIR / "loaders/json.json/10/e/100_null.json.py"),
-     (pathlib.Path("not/exist/dir/data.json"),
-      "e", False, TT.EXTS_TO_TRY, None),
-     (TT.RESOURCE_DIR / "loaders/json.json/10/100_null.json",
-      "e", False, (), None),
-     (TT.RESOURCE_DIR / "999_file_does_not_exist.json",
-      "e", False, (), None),
-     )
-)
-def test_get_aux_data_path(
-    ipath: pathlib.Path,
-    subdir: str,
-    file_ext: typing.Union[str, bool],
-    exts_to_try: typing.Tuple[str, ...],
-    exp: typing.Optional[pathlib.Path]
-):
-    assert TT.get_aux_data_path(
-        ipath, subdir=subdir, file_ext=file_ext, exts_to_try=exts_to_try
-    ) == exp
-
-
-@pytest.mark.parametrize(
-    ("ipath", "aux_paths", "file_extensions"),
-    (("no_aux_data.json", (), ()),
-     ("an_aux_data.yml", ("e/an_aux_data.json", ), ()),
+    ("ipath", "aux_paths"),
+    (("no_aux_data.json", ()),
+     ("an_aux_data.yml", ("e/an_aux_data.json", )),
      ("some_aux_data.yml",
-      ("e/some_aux_data.json", "o/some_aux_data.yml"),
-      ()),
+      ("e/some_aux_data.json", "o/some_aux_data.yml")),
      )
 )
 def test_get_aux_data_paths(
     ipath: str, aux_paths: typing.Tuple[str, ...],
-    file_extensions: typing.Tuple[str, ...],
     tmp_path: pathlib.Path
 ):
     (tmp_path / ipath).touch()
@@ -94,32 +64,26 @@ def test_get_aux_data_paths(
 
             (adir / "file_to_be_ignored_012.json").touch()
 
-    if file_extensions:
-        suffixes = [f".{x}" for x in file_extensions]
-        paths = [p for p in paths if p.suffix in suffixes]
-
     expected = {p.parent.name: p for p in paths}
 
-    res = TT.get_aux_data_paths(tmp_path / ipath, file_extensions)
+    res = TT.get_aux_data_paths(tmp_path / ipath)
     assert res == expected
 
 
 @pytest.mark.parametrize(
-    ("ipaths", "aux_paths", "file_extensions"),
-    ((("no_aux_data.json", ), (), ()),
-     (("10_no_aux_data.json", "20_no_aux_data.json"), (), ()),
-     (("an_aux_data.yml", ), ("e/an_aux_data.json", ), ()),
+    ("ipaths", "aux_paths"),
+    ((("no_aux_data.json", ), ()),
+     (("10_no_aux_data.json", "20_no_aux_data.json"), ()),
+     (("an_aux_data.yml", ), ("e/an_aux_data.json", )),
      (("10_an_aux_data.yml", "50_an_aux_data.yml"),
-      ("e/10_an_aux_data.json", "e/50_an_aux_data.json"), ()),
+      ("e/10_an_aux_data.json", "e/50_an_aux_data.json")),
      (("10_some_aux_data.yml", "99_some_aux_data.yml"),
       ("e/10_some_aux_data.json", "o/10_some_aux_data.yml",
-       "e/99_some_aux_data.json", "o/99_some_aux_data.yml",),
-      ()),
+       "e/99_some_aux_data.json", "o/99_some_aux_data.yml",)),
      )
 )
 def test_get_data(
     ipaths: typing.Tuple[str], aux_paths: typing.Tuple[str, ...],
-    file_extensions: typing.Tuple[str, ...],
     tmp_path: pathlib.Path
 ):
     for ipath in ipaths:
@@ -140,50 +104,5 @@ def test_get_data(
         for ipath in ipaths
     )
 
-    res = TT.get_data(tmp_path, file_extensions)
+    res = TT.get_data(tmp_path)
     assert res == expected
-
-
-@pytest.mark.parametrize(
-    ("loader_or_dumper", "is_loader", "rel_epaths"),
-    (
-        ("json.json", True, [("00/00_10.json", {})]),
-        ("json.json", True, [("00/00_10.json", {}), ("00/00_20.json", {})]),
-        ("json.json", True, [("00/00_10.json", {"e": "00/e/00_10.json.py"})]),
-        ("json.json", True,
-         [("00/00_10.json", {"e": "00/e/00_10.json.py"}),
-          ("00/00_20.json", {"e": "00/e/00_20.json.py"})]),
-    ),
-)
-def test_get_data_paths(
-    loader_or_dumper: str, is_loader: bool,
-    rel_epaths: typing.List[
-        typing.Tuple[str, typing.Dict[str, pathlib.Path]]
-    ],
-    tmp_path: pathlib.Path
-):
-    resdir = TT.get_resource_dir(
-        loader_or_dumper, is_loader=is_loader, topdir=tmp_path
-    )
-    exp = [
-        (resdir / ipath, {d: resdir / a for d, a in d_apaths.items()})
-        for ipath, d_apaths in rel_epaths
-    ]
-
-    assert TT.get_data_paths(
-        loader_or_dumper, is_loader=is_loader, topdir=tmp_path
-    ) == []
-
-    for ipath, apaths in exp:
-        ipath.parent.mkdir(parents=True, exist_ok=True)
-        ipath.touch()
-
-        for apath in apaths.values():
-            adir = apath.parent
-            if not adir.exists():
-                adir.mkdir()
-            apath.touch()
-
-    assert TT.get_data_paths(
-        loader_or_dumper, is_loader=is_loader, topdir=tmp_path
-    ) == exp

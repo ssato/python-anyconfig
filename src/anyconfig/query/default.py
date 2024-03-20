fc4654a80a3bc3497712050ddb759933966e84de
Satoru SATOH <satoru.satoh@gmail.com>
2024-03-20 22:47:36 +0900

    fix: corrections of type check errors

--- src/anyconfig/query/default.py
+++ src/anyconfig/query/default.py
@@ -1,15 +1,20 @@
 #
-# Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# Copyright (C) 2021 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
 # pylint: disable=unused-argument
 """Provide dummy implementation of anyconfig.query.*."""
-from ..common import InDataExT
-from .datatypes import MaybeJexp
+from __future__ import annotations
+
+import typing
+
+if typing.TYPE_CHECKING:
+    from ..common import InDataExT
+    from .datatypes import MaybeJexp
 
 
-def try_query(data: InDataExT, jexp: MaybeJexp = None, **options) -> InDataExT:
+def try_query(
+    data: InDataExT, jexp: MaybeJexp = None, **options
+) -> InDataExT:
     """Provide a dummy implementation of :func:`anyconfig.query.try_query`."""
     return data
-
-# vim:sw=4:ts=4:et:

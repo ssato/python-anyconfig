15a8a5a64138d3b1e8f89e0c7448f6a2946edb91
Satoru SATOH <satoru.satoh@gmail.com>
2024-03-20 22:47:36 +0900

    fix: corrections of type check errors

--- src/anyconfig/schema/default.py
+++ src/anyconfig/schema/default.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 - 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# Copyright (C) 2015 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
 # pylint: disable=unused-argument

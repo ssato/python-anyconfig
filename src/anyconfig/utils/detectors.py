b6be44af3ec7d2ebf348cbdac8f91cc2f7c6c063
Satoru SATOH <satoru.satoh@gmail.com>
2024-03-24 20:36:22 +0900

    fix: add TypeGuard to narrow data types

--- src/anyconfig/utils/detectors.py
+++ src/anyconfig/utils/detectors.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2012 - 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# Copyright (C) 2012 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
 """Functions to detect something."""
@@ -9,15 +9,21 @@ import collections.abc
 import types
 import typing
 
+if typing.TYPE_CHECKING:
+    try:
+        from typing import TypeGuard
+    except ImportError:
+        from typing_extensions import TypeGuard
 
-def is_iterable(obj: typing.Any) -> bool:
+
+def is_iterable(obj: typing.Any) -> TypeGuard[typing.Iterable]:
     """Test if given object is an iterable object."""
     return (isinstance(obj, (list, tuple, types.GeneratorType))
             or (not isinstance(obj, (int, str, dict))
                 and bool(getattr(obj, "next", False))))
 
 
-def is_dict_like(obj: typing.Any) -> bool:
+def is_dict_like(obj: typing.Any) -> TypeGuard[typing.Dict]:
     """Test if given object ``obj`` is an dict."""
     return isinstance(obj, (dict, collections.abc.Mapping))  # any others?
 
@@ -25,9 +31,7 @@ def is_dict_like(obj: typing.Any) -> bool:
 _LIST_LIKE_TYPES = (collections.abc.Iterable, collections.abc.Sequence)
 
 
-def is_list_like(obj: typing.Any) -> bool:
+def is_list_like(obj: typing.Any) -> TypeGuard[typing.Iterable]:
     """Test if given object ``obj`` is a list or -like one."""
     return isinstance(obj, _LIST_LIKE_TYPES) and \
         not (isinstance(obj, str) or is_dict_like(obj))
-
-# vim:sw=4:ts=4:et:

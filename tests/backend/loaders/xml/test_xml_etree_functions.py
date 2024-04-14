66e65b54266a452c55b0f91e2f2e5416df81ddf3
Satoru SATOH <satoru.satoh@gmail.com>
2024-04-14 15:11:23 +0900

    change: migration from unittest to pytest with pytest.mark.parametrize

--- tests/backend/loaders/xml/test_xml_etree_functions.py
+++ tests/backend/loaders/xml/test_xml_etree_functions.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
 #
-# Copyright (C) 2012 - 2024 Satoru SATOH <satoru.satoh @ gmail.com>
+# Copyright (C) 2012 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
 # pylint: disable=missing-docstring,invalid-name,too-few-public-methods
 # pylint: disable=ungrouped-imports,protected-access
+# pylint: disable=too-many-arguments
+#
+import collections
 
-import io
-import unittest
+import pytest
 
 import anyconfig.backend.xml.etree as TT
 
@@ -38,211 +39,201 @@ def to_bytes(astr):
     return bytes(astr, 'utf-8')
 
 
-def to_xml_elem(astr):
+def to_xml_elem(astr: str) -> TT.ElementTree.Element:
     """Convert a string to XML element object."""
-    return TT.ElementTree.XML(astr)
+    return TT.ElementTree.fromstring(astr)
 
 
-class Test_00(unittest.TestCase):
-
-    def test_10__namespaces_from_file(self):
-        ref = {"http://example.com/ns/config": '',
-               "http://example.com/ns/config/val": "val"}
-        xmlfile = io.StringIO(XML_WITH_NS_0)
-        self.assertEqual(TT._namespaces_from_file(xmlfile), ref)
-
-    def test_20__process_elem_text__whitespaces(self):
-        (elem, dic, subdic) = (to_xml_elem("<a> </a>"), {}, {})
-        TT._process_elem_text(elem, dic, subdic)
-        self.assertTrue(not dic)
-        self.assertTrue(not subdic)
-
-    def test_22__process_elem_text__wo_attrs_and_children(self):
-        (elem, dic, subdic) = (to_xml_elem("<a>A</a>"), {}, {})
-        TT._process_elem_text(elem, dic, subdic, text="#text")
-        self.assertEqual(dic, {"a": 'A'})
-        self.assertTrue(not subdic)
-
-    def test_22__process_elem_text__wo_attrs_and_children_parse(self):
-        (elem, dic, subdic) = (to_xml_elem("<a>A</a>"), {}, {})
-        TT._process_elem_text(elem, dic, subdic, text="#text",
-                              ac_parse_value=True)
-        self.assertEqual(dic, {"a": 'A'})
-        self.assertTrue(not subdic)
-
-        (elem, dic, subdic) = (to_xml_elem("<a>1</a>"), {}, {})
-        TT._process_elem_text(elem, dic, subdic, text="#text",
-                              ac_parse_value=True)
-        self.assertEqual(dic, {"a": 1})
-        self.assertTrue(not subdic)
-
-    def test_24__process_elem_text__w_attrs(self):
-        (elem, dic, subdic) = (to_xml_elem("<a id='1'>A</a>"), {}, {})
-        TT._process_elem_text(elem, dic, subdic, text="#text")
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, {"#text": 'A'})
-
-    def test_24__process_elem_text__w_children(self):
-        (elem, dic, subdic) = (to_xml_elem("<a>A<b/></a>"), {}, {})
-        TT._process_elem_text(elem, dic, subdic, text="#text")
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, {"#text": 'A'})
-
-    def test_30__process_elem_attrs__wo_text_and_children(self):
-        (elem, dic, subdic) = (to_xml_elem("<a id='A'/>"), {}, {})
-        TT._process_elem_attrs(elem, dic, subdic)
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, {"@attrs": {"id": 'A'}})
-
-    def test_32__process_elem_attrs__w_text(self):
-        (elem, dic, subdic) = (to_xml_elem("<a id='A'>AAA</a>"), {}, {})
-        TT._process_elem_attrs(elem, dic, subdic)
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, {"@attrs": {"id": 'A'}})
-
-    def test_34__process_elem_attrs__merge_attrs(self):
-        (elem, dic, subdic) = (to_xml_elem("<a id='A'/>"), {}, {})
-        TT._process_elem_attrs(elem, dic, subdic, merge_attrs=True)
-        self.assertEqual(dic, {"a": {"id": 'A'}})
-        self.assertTrue(not subdic)
-
-    def test_36__process_elem_attrs__wo_text_and_children_parse(self):
-        (elem, dic, subdic) = (to_xml_elem("<a id='1'/>"), {}, {})
-        TT._process_elem_attrs(elem, dic, subdic, ac_parse_value=True)
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, {"@attrs": {"id": 1}})
-
-        (elem, dic, subdic) = (to_xml_elem("<a id='A'/>"), {}, {})
-        TT._process_elem_attrs(elem, dic, subdic, ac_parse_value=True)
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, {"@attrs": {"id": 'A'}})
-
-        (elem, dic, subdic) = (to_xml_elem("<a id='true'/>"), {}, {})
-        TT._process_elem_attrs(elem, dic, subdic, ac_parse_value=True)
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, {"@attrs": {"id": True}})
-
-    def test_40__process_children_elems__root(self):
-        (elem, dic, subdic) = (
-            to_xml_elem("<list><i>A</i><i>B</i></list>"), {},
-            {}
-        )
-        TT._process_children_elems(elem, dic, subdic)
-        self.assertEqual(dic, {"list": [{"i": "A"}, {"i": "B"}]})
-        self.assertTrue(not subdic)
-
-    def test_42__process_children_elems__w_attr(self):
-        (elem, dic) = (
-            to_xml_elem("<list id='xyz'><i>A</i><i>B</i></list>"),
-            {}
-        )
-        subdic = {"id": "xyz"}
-        ref = subdic.copy()
-        ref.update({"#children": [{"i": "A"}, {"i": "B"}]})
-
-        TT._process_children_elems(elem, dic, subdic, children="#children")
-        self.assertTrue(not dic)
-        self.assertEqual(subdic, ref, subdic)
-
-    def test_44__process_children_elems__w_children_have_unique_keys(self):
-        (elem, dic, subdic) = (to_xml_elem("<a><x>X</x><y>Y</y></a>"), {}, {})
-        TT._process_children_elems(elem, dic, subdic)
-        self.assertEqual(dic, {"a": {"x": "X", "y": "Y"}})
-        self.assertTrue(not subdic)
-
-    def test_46__process_children_elems__w_merge_attrs(self):
-        elem = to_xml_elem("<a z='Z'><x>X</x><y>Y</y></a>")
-        dic = {"a": {"@attrs": {"z": "Z"}}}
-        subdic = dic["a"]["@attrs"]
-        TT._process_children_elems(elem, dic, subdic, merge_attrs=True)
-        self.assertEqual(dic, {"a": {"x": "X", "y": "Y", "z": "Z"}}, dic)
+@pytest.mark.parametrize(
+    ("path", "exp"),
+    (("tests/res/1/loaders/xml.etree/10/100.xml", {}),
+     ("tests/res/1/loaders/xml.etree/10/200.xml",
+      {"http://example.com/ns/config": "",
+       "http://example.com/ns/config/val": "val"}),
+     ),
+)
+def test__namespaces_from_file(path: str, exp):
+    assert TT._namespaces_from_file(path) == exp
 
 
-class Test_00_1(unittest.TestCase):
-
-    def _assert_eq_dic_from_snippet(self, snippet, ref, **opts):
-        self.assertEqual(
-            TT.elem_to_container(to_xml_elem(snippet), **opts),
-            ref
-        )
-
-    def test_10_elem_to_container__None(self):
-        self.assertEqual(TT.elem_to_container(None), {})
-
-    def test_10_root_to_container__None(self):
-        self.assertEqual(TT.root_to_container(None), {})
-
-    def test_12_elem_to_container__empty(self):
-        self._assert_eq_dic_from_snippet("<a/>", dict(a=None))
-
-    def test_20_elem_to_container__attrs(self):
-        ref = dict(a={"@attrs": dict(x='1', y='y')})
-        self._assert_eq_dic_from_snippet("<a x='1' y='y'/>", ref)
-
-    def test_30_elem_to_container__child(self):
-        ref = dict(a=dict(b="b"))
-        self._assert_eq_dic_from_snippet("<a><b>b</b></a>", ref)
-
-    def test_32_elem_to_container__children__same_keys(self):
-        ref = {'a': [{'b': '1'}, {'b': '2'}]}
-        self._assert_eq_dic_from_snippet("<a><b>1</b><b>2</b></a>", ref)
-
-    def test_34_elem_to_container__children(self):
-        ref = {'a': {'b': 'b', 'c': 'c'}}
-        self._assert_eq_dic_from_snippet("<a><b>b</b><c>c</c></a>", ref)
-
-    def test_36_elem_to_container__children__same_keys_w_text(self):
-        ref = {'a': {'@text': 'aaa', '@children': [{'b': '1'}, {'b': '2'}]}}
-        self._assert_eq_dic_from_snippet("<a>aaa<b>1</b><b>2</b></a>", ref)
-
-    def test_40_elem_to_container__text(self):
-        self._assert_eq_dic_from_snippet("<a>A</a>", {'a': 'A'})
-
-    def test_42_elem_to_container__text_attrs(self):
-        ref = dict(a={"@attrs": {'x': 'X'}, "@text": "A"})
-        self._assert_eq_dic_from_snippet("<a x='X'>A</a>", ref)
-
-    def test_50_root_to_container__text_attrs_tags(self):
-        ref = dict(a={"_attrs": {'x': 'X'}, "_text": "A"})
-        tags = dict(attrs="_attrs", text="_text")
-        self.assertEqual(
-            TT.root_to_container(
-                to_xml_elem("<a x='X'>A</a>"), dict, {}, tags=tags
-            ),
-            ref
-        )
+@pytest.mark.parametrize(
+    ("tag", "nspaces", "exp"),
+    (("a", {}, "a"),
+     ("a", {"http://example.com/ns/val/": "val"}, "a"),
+     ("{http://example.com/ns/val/}a",
+      {"http://example.com/ns/val/": "val"},
+      "val:a"),
+     ),
+)
+def test__tweak_ns(tag, nspaces, exp):
+    assert TT._tweak_ns(tag, nspaces=nspaces) == exp
 
 
-def tree_to_string(tree):
-    return TT.ElementTree.tostring(tree.getroot())
+@pytest.mark.parametrize(
+    ("dics", "exp"),
+    ((({}, {"a": 1}, {"a": 2}), False),
+     (({"a": 1}, {"b": 2}, {"b": 3, "c": 0}), False),
+     (({}, {}), True),
+     (({"(": 1}, {"b": 2}, {"c": 0}), True),
+     ),
+)
+def test__dicts_have_unique_keys(dics, exp):
+    assert TT._dicts_have_unique_keys(dics) == exp
 
 
-class Test_00_2(unittest.TestCase):
+@pytest.mark.parametrize(
+    ("val", "opts", "exp"),
+    (("1", {}, "1"),
+     ("1", {"ac_parse_value": True}, 1),
+     ),
+)
+def test__parse_text_parse_text(val, opts, exp):
+    assert TT._parse_text(val, **opts) == exp
 
-    def test_00_container_to_etree__None(self):
-        self.assertTrue(TT.container_to_etree(None) is None)
 
-    def test_10_container_to_etree__text_attrs(self):
-        ref = to_bytes('<a x="X" y="Y">A</a>')
-        obj = dict(a={"@attrs": {'x': 'X', 'y': 'Y'}, "@text": "A"})
-        res = TT.container_to_etree(obj)
-        self.assertEqual(tree_to_string(res), ref)
+@pytest.mark.parametrize(
+    ("elem_s", "opts", "exp_elem_text", "exp_dic", "exp_subdic"),
+    (("<a> </a>", {}, "", {}, {}),
+     ("<a> </a>", {"text": "#text"}, "", {}, {}),
+     ("<p:a xmlns:p='http://example.com'> </p:a>", {}, "", {}, {}),
+     ("<a>1</a>", {}, "1", {"a": "1"}, {}),
+     ("<a>1</a>", {"ac_parse_value": True}, "1", {"a": 1}, {}),
+     ("<a id='1'>1</a>", {"text": "#text"}, "1", {}, {"#text": "1"}),
+     ("<a id='1'>1</a>", {"text": "#text", "ac_parse_value": True},
+      "1", {}, {"#text": 1}),
+     ("<a>1<b/></a>", {}, "1", {}, {"@text": "1"}),
+     ("<a id='A' />", {}, None, {}, {}),
+     ),
+)
+def test__process_elem_text(elem_s, opts, exp_elem_text, exp_dic, exp_subdic):
+    (elem, dic, subdic) = (to_xml_elem(elem_s), {}, {})
+    TT._process_elem_text(elem, dic, subdic, **opts)
 
-    def test_12_container_to_etree__text_attrs_tags(self):
-        ref = to_bytes('<a x="X" y="Y">A</a>')
-        obj = dict(a={"_attrs": {'x': 'X', 'y': 'Y'}, "_text": "A"})
-        tags = dict(attrs="_attrs", text="_text")
-        res = TT.container_to_etree(obj, tags=tags)
-        self.assertEqual(tree_to_string(res), ref)
+    assert elem.text == exp_elem_text
+    assert dic == exp_dic
+    assert subdic == exp_subdic
 
-    def test_20_container_to_etree__child(self):
-        ref = to_bytes("<a><b>b</b></a>")
-        obj = dict(a=dict(b="b"))
-        res = TT.container_to_etree(obj)
-        self.assertEqual(tree_to_string(res), ref)
 
-    def test_22_container_to_etree__children(self):
-        ref = to_bytes("<a><b>b</b><c>c</c></a>")
-        obj = {'a': {'@children': [{'b': 'b'}, {'c': 'c'}]}}
-        res = TT.container_to_etree(obj)
-        self.assertEqual(tree_to_string(res), ref)
+@pytest.mark.parametrize(
+    ("elem_s", "opts", "exp_dic", "exp_subdic"),
+    (("<a id='A'/>", {}, {}, {"@attrs": {"id": "A"}}),
+     ("<a id='A'>AAA</a>", {}, {}, {"@attrs": {"id": "A"}}),
+     ("<a id='A'/>", {"merge_attrs": True}, {"a": {"id": "A"}}, {}),
+     ("<a id='1'/>", {"ac_parse_value": True}, {}, {"@attrs": {"id": 1}}),
+     ("<a id='A'/>", {"ac_parse_value": True}, {}, {"@attrs": {"id": "A"}}),
+     ("<a id='true'/>", {"ac_parse_value": True}, {},
+      {"@attrs": {"id": True}}),
+     ),
+)
+def test__process_elem_attrs(elem_s, opts, exp_dic, exp_subdic):
+    (elem, dic, subdic) = (to_xml_elem(elem_s), {}, {})
+    TT._process_elem_attrs(elem, dic, subdic, **opts)
+
+    assert dic == exp_dic
+    assert subdic == exp_subdic
+
+
+@pytest.mark.parametrize(
+    ("elem_s", "opts", "exp_dic", "exp_subdic"),
+    (("<a><x>X</x><y>Y</y></a>", {}, {"a": {"x": "X", "y": "Y"}}, {}),
+     ("<list><i>A</i><i>B</i></list>", {},
+      {"list": [{"i": "A"}, {"i": "B"}]}, {}),
+     ("<list id='xyz'><i>A</i><i>B</i></list>", {"children": "#children"},
+      {"list": [{"i": "A"}, {"i": "B"}]}, {}),
+     ("<a z='Z'><x>X</x><y>Y</y></a>", {"merge_attrs": True},
+      {"a": {"x": "X", "y": "Y", "z": "Z"}}, {}),
+     ),
+)
+def test_process_children_elems(
+    elem_s, opts, exp_dic, exp_subdic
+):
+    (elem, dic, subdic) = (to_xml_elem(elem_s), {}, {})
+    TT._process_children_elems(elem, dic, subdic, **opts)
+
+    assert dic == exp_dic
+    assert subdic == exp_subdic
+
+
+def test_elem_to_container__none():
+    assert TT.elem_to_container(None) == {}
+    assert TT.elem_to_container(
+        None, container=collections.OrderedDict
+    ) == collections.OrderedDict()
+
+
+_E2C_DATASETS = _R2C_DATASETS = (
+    ("<a/>", {"a": None}),
+    ("<a>A</a>", {"a": "A"}),
+    ("<a x='X'>A</a>",
+     {"a": {"@attrs": {"x": "X"}, "@text": "A"}}),
+    ("<a><b>b</b></a>", {"a": {"b": "b"}}),
+    ("<a><b>1</b><b>2</b></a>",
+     {"a": [{"b": "1"}, {"b": "2"}]}),
+    ("<a><b>b</b><c>c</c></a>",
+     {'a': {'b': 'b', 'c': 'c'}}),
+    ("<a x='1' y='y'/>",
+     {"a": {"@attrs": {"x": "1", "y": "y"}}}),
+    ("<a>aaa<b>1</b><b>2</b></a>",
+     {"a": {"@text": "aaa", "@children": [{"b": "1"}, {"b": "2"}]}}),
+)
+
+
+@pytest.mark.parametrize(("elem_s", "exp"), _E2C_DATASETS)
+def test_elem_to_container(elem_s, exp):
+    assert TT.elem_to_container(
+        to_xml_elem(elem_s)
+    ) == exp
+
+
+def test_root_to_container__none():
+    assert TT.root_to_container(None) == {}
+    assert TT.root_to_container(
+        None, container=collections.OrderedDict
+    ) == collections.OrderedDict()
+
+
+@pytest.mark.parametrize(("root_s", "exp"), _R2C_DATASETS)
+def test_root_to_container(root_s: str, exp):
+    assert TT.root_to_container(
+        to_xml_elem(root_s)
+    ) == exp
+
+
+@pytest.mark.parametrize(
+    ("obj", "parent"),
+    ((None, None),
+     ({}, None),
+     ),
+)
+def test_container_to_etree__errors(obj, parent):
+    with pytest.raises(ValueError):
+        assert TT.container_to_etree(obj, parent=parent)
+
+
+@pytest.mark.parametrize(
+    ("obj", "exp_s"),
+    (({"a": {"@attrs": {'x': 'X', 'y': 'Y'}, "@text": "A"}},
+      '<a x="X" y="Y">A</a>'),
+     ({"a": {"b": "b"}},
+      "<a><b>b</b></a>"),
+     ({'a': {'@children': [{'b': 'b'}, {'c': 'c'}]}},
+      "<a><b>b</b><c>c</c></a>"),
+     ),
+)
+def test_container_to_etree(obj, exp_s):
+    assert TT.ElementTree.tostring(
+        TT.container_to_etree(obj)
+    ) == to_bytes(exp_s)
+
+
+@pytest.mark.parametrize(
+    ("obj", "tags", "exp_s"),
+    (({"a": {"_attrs": {'x': 'X', 'y': 'Y'}, "_text": "A"}},
+      {"attrs": "_attrs", "text": "_text"},
+      '<a x="X" y="Y">A</a>'),
+     ),
+)
+def test_container_to_etree_with_tags(obj, tags, exp_s):
+    assert TT.ElementTree.tostring(
+        TT.container_to_etree(obj, tags=tags)
+    ) == to_bytes(exp_s)

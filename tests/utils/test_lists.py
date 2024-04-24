e3db551619ebab0f612b868e6712a7b731f6d3d0
Satoru SATOH <satoru.satoh@gmail.com>
2024-04-25 00:36:26 +0900

    fix/change: migrate test cases for anyconfig.utils from unittest to pytest

--- tests/utils/test_lists.py
+++ tests/utils/test_lists.py
@@ -1,36 +1,35 @@
 #
-# Copyright (C) 2012 - 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# Copyright (C) 2012 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
-# pylint: disable=missing-docstring,invalid-name
-r"""test cases for anyconfig.utils.lists.
-"""
+# pylint: disable=missing-docstring
+r"""test cases for anyconfig.utils.lists."""
+from __future__ import annotations
+
 import operator
-import unittest
+
+import pytest
 
 import anyconfig.utils.lists as TT
 
 
-class TestCase(unittest.TestCase):
+def test_groupby() -> None:
+    items = (("a", 1), ("b", -1), ("c", 1))
+    res = TT.groupby(items, operator.itemgetter(1))
+    assert [
+        (key, tuple(grp)) for key, grp in res
+    ] == [(-1, (("b", -1),)), (1, (("a", 1), ("c", 1)))]
 
-    def test_groupby(self):
-        items = (("a", 1), ("b", -1), ("c", 1))
-        res = TT.groupby(items, operator.itemgetter(1))
-        self.assertEqual(
-            [(key, tuple(grp)) for key, grp in res],
-            [(-1, (("b", -1),)), (1, (("a", 1), ("c", 1)))]
-        )
 
-    def test_concat(self):
-        ies = (
-            ([[]], []),
-            ((()), []),
-            ([[1, 2, 3], [4, 5]], [1, 2, 3, 4, 5]),
-            ([[1, 2, 3], [4, 5, [6, 7]]], [1, 2, 3, 4, 5, [6, 7]]),
-            (((1, 2, 3), (4, 5, (6, 7))), [1, 2, 3, 4, 5, (6, 7)]),
-            (((i, i * 2) for i in range(3)), [0, 0, 1, 2, 2, 4]),
-        )
-        for inp, exp in ies:
-            self.assertEqual(TT.concat(inp), exp)
-
-# vim:sw=4:ts=4:et:
+@pytest.mark.parametrize(
+    ("xss", "exp"),
+    (([[]], []),
+     ((()), []),
+     ([[1, 2, 3], [4, 5]], [1, 2, 3, 4, 5]),
+     ([[1, 2, 3], [4, 5, [6, 7]]], [1, 2, 3, 4, 5, [6, 7]]),
+     (((1, 2, 3), (4, 5, (6, 7))), [1, 2, 3, 4, 5, (6, 7)]),
+     (((i, i * 2) for i in range(3)), [0, 0, 1, 2, 2, 4])
+     ),
+)
+def test_concat(xss, exp):
+    assert TT.concat(xss) == exp

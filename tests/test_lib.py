8654cb3db9035cf724044b39616b6b5e31c96828
Satoru SATOH <satoru.satoh@gmail.com>
2024-04-27 02:46:26 +0900

    fix/change: migrate test cases, tests/test_lib.py, from unittest to pytest

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,16 +1,13 @@
 #
-# Copyright (C) 2015 - 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# Copyright (C) 2015 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
 # pylint: disable=missing-docstring
 import pathlib
 import subprocess
-import tempfile
-import unittest
 
 
-SCRIPT_TO_USE_ANYCONFIG = """\
-#! /usr/bin/env python
+SCRIPT_TO_USE_ANYCONFIG = """#! /usr/bin/env python
 import anyconfig
 
 c = anyconfig.load("/") or {}
@@ -23,19 +20,15 @@ if not pathlib.Path(NULL_DEV).exists():
 
 
 def check_output(cmd):
-    devnull = open(NULL_DEV, "w")
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=devnull)
-    return proc.communicate()[0]
+    with open(NULL_DEV, mode="w", encoding="utf-8") as devnull:
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                              stderr=devnull) as proc:
+            return proc.communicate()[0]
 
 
-class TestCase(unittest.TestCase):
+def test_run_script(tmp_path: pathlib.Path):
+    script = tmp_path / "a.py"
+    script.write_text(SCRIPT_TO_USE_ANYCONFIG)
+    out = check_output(["python", str(script)])
 
-    def test_00_run_script(self):
-        with tempfile.TemporaryDirectory(prefix="anyconfig-tests-") as tmpdir:
-            script = pathlib.Path(tmpdir) / "a.py"
-            script.write_text(SCRIPT_TO_USE_ANYCONFIG)
-
-            out = check_output(["python", str(script)])
-            self.assertTrue(out in (b"", ""))
-
-# vim:sw=4:ts=4:et:
+    assert out in (b"", "")

b15003e60433512ef9ff792ddefe066d8d93b29c
Satoru SATOH <satoru.satoh@gmail.com>
2024-05-07 03:47:48 +0900

    fix/change: migrate to pytest with use of new test data loader
    Migrate from unittest.Testcase to pytest test cases with
    pytest.mark.parametrize, and switch to new test data loader to simplify
    test cases.
    
    - tests/api/multi_load/common.py: Obsoleted and deleted old
      test data collector based on unittest.TestCase
    - tests/api/multi_load/*_{basics,multi_types,query,schema,template}.py:
      switch to use new test data collector
    - other files in tests/api/multi_load/: Obsoleted and removed
    - tests/res/1/api/multi_load/**/*.*: Added/moved test data for
      anyconfig.api.multi_load

--- tests/api/multi_load/test_multi_types.py
+++ tests/api/multi_load/test_multi_types.py
@@ -1,13 +1,35 @@
 #
-# Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# Copyright (C) 2021 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
 # pylint: disable=missing-docstring
-from . import common
+"""Test cases for anyconfig.api.multi_load with multi type inputs."""
+from __future__ import annotations
+
+import typing
+
+import pytest
+
+import anyconfig.api._load as TT
+
+from .common import (
+    NAMES, load_data_for_testfile, get_test_ids
+)
+
+if typing.TYPE_CHECKING:
+    import pathlib
 
 
-class TestCase(common.TestCase):
-    kind = 'multi_types'
-    pattern = '*.*'
+DATA = load_data_for_testfile(__file__)
+DATA_IDS: list[str] = get_test_ids(DATA)
 
-# vim:sw=4:ts=4:et:
+
+def test_data() -> None:
+    assert DATA
+
+
+@pytest.mark.parametrize(NAMES, DATA, ids=DATA_IDS)
+def test_multi_load(
+    inputs: list[pathlib.Path], opts: dict, exp
+) -> None:
+    assert TT.multi_load(inputs, **opts) == exp

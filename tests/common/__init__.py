a7ef11cbb50ca6828a0ea705ee1e8724797affec
Satoru SATOH <satoru.satoh@gmail.com>
2024-05-20 23:45:12 +0900

    change: export tests.common.paths.load_data

--- tests/common/__init__.py
+++ tests/common/__init__.py
@@ -4,11 +4,13 @@
 #
 "tests.common - common global variables and functions."""
 from .globals_ import TESTDIR, RESOURCE_DIR
+from .paths import load_data
 from .tdc import (
     get_test_ids, load_data_for_testfile
 )
 
 __all__ = [
     "TESTDIR", "RESOURCE_DIR",
+    "load_data",
     "get_test_ids", "load_data_for_testfile",
 ]

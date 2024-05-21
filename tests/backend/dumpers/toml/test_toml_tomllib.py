eb65b3a8b99ee2a53f5c104958bfbf32915cc55f
Satoru SATOH <satoru.satoh@gmail.com>
2024-05-21 21:49:36 +0900

    change: switch to new test data loader

--- tests/backend/dumpers/toml/test_toml_tomllib.py
+++ tests/backend/dumpers/toml/test_toml_tomllib.py
@@ -2,18 +2,13 @@
 # Copyright (C) 2012 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
-# pylint: disable=missing-docstring,invalid-name,too-few-public-methods
-# pylint: disable=ungrouped-imports
-"""Test cases for the loader.
-"""
-import pathlib
+# pylint: disable=missing-docstring
+"""Test cases for the dumper."""
+from __future__ import annotations
+
 import typing
 
 import pytest
-
-import tests.common.tdi_base
-import tests.common.dumper
-
 
 from ... import common
 
@@ -63,43 +58,5 @@ def test_dump(
     assert opath.exists()
     assert psr.load(ioi, **opts) == idata
 
-    content = psr.ropen(str(opath)).read().decode("utf-8")
+    # content = psr.ropen(str(opath)).read().decode("utf-8")
     # assert content == exp  # This may fail.
-
-
-class TDI(tests.common.tdi_base.TDI):
-    _cid = tests.common.tdi_base.name_from_path(__file__)
-    _is_loader = False
-
-
-(TT, DATA, DATA_IDS) = TDI().get_all()
-
-if TT is None:
-    pytest.skip(
-        f"skipping tests: {TDI().cid()} as it's not available.",
-        allow_module_level=True
-    )
-
-assert DATA
-
-
-class TestCase(tests.common.dumper.TestCase):
-    psr_cls = TT.Parser
-    exact_match = False
-
-    @pytest.mark.parametrize(
-        ("ipath", "aux"), DATA, ids=DATA_IDS,
-    )
-    def test_dumps(
-        self, ipath: pathlib.Path, aux: dict[str, typing.Any],
-    ):
-        self._assert_dumps(ipath, aux)
-
-    @pytest.mark.parametrize(
-        ("ipath", "aux"), DATA, ids=DATA_IDS,
-    )
-    def test_dump(
-        self, ipath: pathlib.Path, aux: dict[str, typing.Any],
-        tmp_path: pathlib.Path
-    ):
-        self._assert_dump(ipath, aux, tmp_path)

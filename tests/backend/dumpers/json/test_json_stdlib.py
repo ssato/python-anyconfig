2f045878bdb56e6604c02f96fefdc52cee99414b
Satoru SATOH <satoru.satoh@gmail.com>
2024-05-21 21:46:10 +0900

    change: add test cases with using new test data loader

--- tests/backend/dumpers/json/test_json_stdlib.py
+++ tests/backend/dumpers/json/test_json_stdlib.py
@@ -2,21 +2,18 @@
 # Copyright (C) 2012 - 2024 Satoru SATOH <satoru.satoh gmail.com>
 # SPDX-License-Identifier: MIT
 #
-# pylint: disable=missing-docstring,invalid-name,too-few-public-methods
-# pylint: disable=ungrouped-imports
-"""Test cases for the loader.
-"""
+# pylint: disable=missing-docstring
+"""Test cases for the dumpers."""
 from __future__ import annotations
 
-import pathlib
 import typing
 
 import pytest
 
-import tests.common.tdi_base
-import tests.common.dumper
-
 from ... import common
+
+if typing.TYPE_CHECKING:
+    import pathlib
 
 
 try:
@@ -59,40 +56,3 @@ def test_dump(
 
     content = psr.ropen(str(opath)).read()
     assert content == exp
-
-
-class TDI(tests.common.tdi_base.TDI):
-    _cid = tests.common.tdi_base.name_from_path(__file__)
-    _is_loader = False
-
-
-(TT, DATA, DATA_IDS) = TDI().get_all()
-
-if TT is None:
-    pytest.skip(
-        f"skipping tests: {TDI().cid()} as it's not available.",
-        allow_module_level=True
-    )
-
-assert DATA
-
-
-class TestCase(tests.common.dumper.TestCase):
-    psr_cls = TT.Parser
-
-    @pytest.mark.parametrize(
-        ("ipath", "aux"), DATA, ids=DATA_IDS,
-    )
-    def test_dumps(
-        self, ipath: pathlib.Path, aux: dict[str, typing.Any],
-    ):
-        self._assert_dumps(ipath, aux)
-
-    @pytest.mark.parametrize(
-        ("ipath", "aux"), DATA, ids=DATA_IDS,
-    )
-    def test_dump(
-        self, ipath: pathlib.Path, aux: dict[str, typing.Any],
-        tmp_path: pathlib.Path
-    ):
-        self._assert_dump(ipath, aux, tmp_path)

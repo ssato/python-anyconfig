9c708d03fbfdd2f768bf395a6d0a65e33ba3be8e
Satoru SATOH <satoru.satoh@gmail.com>
2024-06-10 02:39:05 +0900

    change: find test file for tests.backend.test_common dynamically if possible

--- tests/backend/constants.py
+++ tests/backend/constants.py
@@ -13,10 +13,22 @@ from . import common as TT
 
 CURDIR: pathlib.Path = pathlib.Path(__file__).parent
 
-MOD_TYPE: str = "json"
-MOD_BACKEND: str = "stdlib"
+TEST_FILES: list[pathlib.Path] = list(
+    (CURDIR / "loaders").glob("*/test_*.py")
+)
+
+assert TEST_FILES
+
+MOD_TYPE_DEFAULT: str = "json"
+
+if any(f for f in TEST_FILES if f.parent.name == MOD_TYPE_DEFAULT):
+    MOD_TYPE = MOD_TYPE_DEFAULT
+    MOD_BACKEND: str = "stdlib"
+else:
+    MOD_TYPE: str = TEST_FILES[0].parent.name
+    MOD_BACKEND: str = TEST_FILES[0].stem.split("_")[-1]
 
 TEST_FILE = (
     CURDIR / "loaders" / MOD_TYPE / f"test_{MOD_TYPE}_{MOD_BACKEND}.py"
 )
-TEST_DATADIR = TT.common.RESOURCE_DIR / "loaders" / "json.stdlib"
+TEST_DATADIR = TT.common.RESOURCE_DIR / "loaders" / f"{MOD_TYPE}.{MOD_BACKEND}"

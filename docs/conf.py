7829fc1512acc23bbcdde7abbda46f3e930f4112
Satoru Sato <satoru.satoh@gmail.com>
2026-02-11 23:53:12 +0900

    fix: fix several ruff errors in docs/conf.py
    fix or add workarouds for several ruff errors like the followings in
    docs/conf.py.
    
    - A001: builtin-variable-shadowing
    - I001: unsorted-imports
    - Q000: bad-quotes-inline-string
    - UP025: unicode-kind-prefix

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,24 @@
-# -*- coding: utf-8 -*-
 #
 # pylint:disable=invalid-name
 """conf.py for sphinx."""
-import sys
 import pathlib
+import sys
 
-sys.path.insert(0, str(pathlib.Path(__file__).parent.resolve() / 'src'))
+sys.path.insert(0, str(pathlib.Path(__file__).parent.resolve() / "src"))
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx_autodoc_typehints'
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
 ]
-source_suffix = '.rst'
-master_doc = 'index'
+source_suffix = ".rst"
+master_doc = "index"
 
-project = u'python-anyconfig'
-copyright = u'2021, Satoru SATOH <satoru.satoh@gmail.com>'
-version = '0.10.0'
+project = "python-anyconfig"
+version = "3.13.11"
 release = version
 
 exclude_patterns = []
 
-html_theme = 'default'
+html_theme = "default"
 
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"

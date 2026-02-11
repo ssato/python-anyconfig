96822bf93f527e0948220460b1060a3c5c11929a
Satoru Sato <satoru.satoh@gmail.com>
2026-02-11 23:41:38 +0900

    fix: fix several ruff errors for setup.py
    fix or add workarouds for several ruff errors like the followings in
    setup.py.
    
    - ANN001: missing-type-function-argument
    - ANN202: missing-type-argsmissing-type-args
    - C408: unnecessary-collection-call
    - DTZ005: call-datetime-now-without-tzinfo
    - I001: unsorted-imports
    - N801: invalid-class-name
    - Q000: bad-quotes-inline-string

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ import setuptools.command.bdist_rpm
 
 
 # It might throw IndexError and so on.
-VERSION = '0.1.0'
+VERSION = "0.1.0"
 VER_REG = re.compile(r"^__version__ = '([^']+)'")
 
-for fpath in pathlib.Path('src').glob('**/__init__.py'):
+for fpath in pathlib.Path("src").glob("**/__init__.py"):
     for line in fpath.open():
         match = VER_REG.match(line)
         if match:
@@ -21,11 +21,13 @@ for fpath in pathlib.Path('src').glob('**/__init__.py'):
 RELEASE = "1%{?dist}"
 if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
     import datetime
-    RELEASE = RELEASE.replace('1',
-                              datetime.datetime.now().strftime("%Y%m%d"))
+    RELEASE = RELEASE.replace(
+        "1",
+        datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y%m%d"),
+    )
 
 
-def _replace(line):
+def _replace(line: str) -> str:
     """Replace some strings in the RPM SPEC template."""
     if "@VERSION@" in line:
         return line.replace("@VERSION@", VERSION)
@@ -39,17 +41,18 @@ def _replace(line):
     return line
 
 
-class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
+class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):  # noqa: N801
     """Override the default content of the RPM SPEC."""
 
-    spec_tmpl = pathlib.Path('pkg/package.spec.in').resolve()
+    spec_tmpl = pathlib.Path("pkg/package.spec.in").resolve()
 
-    def _make_spec_file(self):
+    def _make_spec_file(self) -> list[str]:
         """Generate the RPM SPEC file."""
         return [_replace(line.rstrip()) for line in self.spec_tmpl.open()]
 
 
-setuptools.setup(version=VERSION, cmdclass=dict(bdist_rpm=bdist_rpm),
-                 data_files=[("share/man/man1", ["docs/anyconfig_cli.1"])])
-
-# vim:sw=4:ts=4:et:
+setuptools.setup(
+    version=VERSION,
+    cmdclass={"bdist_rpm": bdist_rpm},
+    data_files=[("share/man/man1", ["docs/anyconfig_cli.1"])],
+)

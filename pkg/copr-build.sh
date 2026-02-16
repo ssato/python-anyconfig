bace8241af6f3c4229698b1ba1011844da357615
Satoru Sato <satoru.satoh@gmail.com>
2026-02-16 22:38:54 +0900

    fix/change: [rpm] test build before uploading to copr

--- pkg/copr-build.sh
+++ pkg/copr-build.sh
@@ -11,10 +11,13 @@ curdir=${0%/*}
 topdir=${curdir}/../
 srpmdir=${topdir}/build
 
+check=$1
+
 # see also
 # - /etc/fedora-release
 # - https://copr.fedorainfracloud.org/coprs/ssato/python-anyconfig/
 dists="
+epel-10-x86_64
 fedora-42-x86_64
 fedora-rawhide-x86_64
 "
@@ -22,9 +25,12 @@ fedora-rawhide-x86_64
 copr_project=ssato/python-anyconfig
 srpm="$(ls -1 ${srpmdir:?}/*.src.rpm | sort -Vr | head -n 1)"  # FIXME
 
-for dist in ${dists:?}; do
-    mock -r ${dist:?} "${srpm:?}"
-done
-
-test -f ~/.config/copr
-copr-cli build ${copr_project:?} "${srpm:?}"
+[[ -z ${check} ]] && {
+    test -f ~/.config/copr
+    copr-cli build ${copr_project:?} "${srpm:?}"
+} || {
+    for dist in ${dists:?}; do
+        mock -r ${dist:?} "${srpm:?}" &
+    done
+    wait
+}
